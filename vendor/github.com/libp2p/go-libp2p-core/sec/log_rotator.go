f99087dd3fdffd036a9f8f6dae35285975426183
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-19 18:33:24 +0000

    Implement log rotation with size-based file splitting and compression

---
+++ vendor/github.com/libp2p/go-libp2p-core/sec/log_rotator.go
@@ -0,0 +1,173 @@
+
+package main
+
+import (
+    "compress/gzip"
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "strconv"
+    "strings"
+    "time"
+)
+
+const (
+    maxFileSize = 10 * 1024 * 1024 // 10MB
+    maxBackups  = 5
+)
+
+type LogRotator struct {
+    filename   string
+    current    *os.File
+    currentSize int64
+}
+
+func NewLogRotator(filename string) (*LogRotator, error) {
+    rotator := &LogRotator{filename: filename}
+    if err := rotator.openCurrent(); err != nil {
+        return nil, err
+    }
+    return rotator, nil
+}
+
+func (lr *LogRotator) Write(p []byte) (int, error) {
+    if lr.currentSize+int64(len(p)) > maxFileSize {
+        if err := lr.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := lr.current.Write(p)
+    if err == nil {
+        lr.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (lr *LogRotator) rotate() error {
+    if lr.current != nil {
+        lr.current.Close()
+    }
+
+    timestamp := time.Now().Format("20060102150405")
+    rotatedFile := fmt.Sprintf("%s.%s", lr.filename, timestamp)
+    if err := os.Rename(lr.filename, rotatedFile); err != nil {
+        return err
+    }
+
+    if err := lr.compressFile(rotatedFile); err != nil {
+        return err
+    }
+
+    if err := lr.cleanupOldBackups(); err != nil {
+        return err
+    }
+
+    return lr.openCurrent()
+}
+
+func (lr *LogRotator) compressFile(filename string) error {
+    src, err := os.Open(filename)
+    if err != nil {
+        return err
+    }
+    defer src.Close()
+
+    dst, err := os.Create(filename + ".gz")
+    if err != nil {
+        return err
+    }
+    defer dst.Close()
+
+    gz := gzip.NewWriter(dst)
+    defer gz.Close()
+
+    if _, err := io.Copy(gz, src); err != nil {
+        return err
+    }
+
+    os.Remove(filename)
+    return nil
+}
+
+func (lr *LogRotator) cleanupOldBackups() error {
+    pattern := lr.filename + ".*.gz"
+    matches, err := filepath.Glob(pattern)
+    if err != nil {
+        return err
+    }
+
+    if len(matches) <= maxBackups {
+        return nil
+    }
+
+    var timestamps []int64
+    for _, match := range matches {
+        parts := strings.Split(match, ".")
+        if len(parts) < 3 {
+            continue
+        }
+        ts, err := strconv.ParseInt(parts[len(parts)-2], 10, 64)
+        if err != nil {
+            continue
+        }
+        timestamps = append(timestamps, ts)
+    }
+
+    for i := 0; i < len(timestamps)-maxBackups; i++ {
+        oldestIdx := i
+        for j := i + 1; j < len(timestamps); j++ {
+            if timestamps[j] < timestamps[oldestIdx] {
+                oldestIdx = j
+            }
+        }
+        os.Remove(matches[oldestIdx])
+        timestamps[oldestIdx] = timestamps[i]
+        matches[oldestIdx] = matches[i]
+    }
+
+    return nil
+}
+
+func (lr *LogRotator) openCurrent() error {
+    file, err := os.OpenFile(lr.filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+    if err != nil {
+        return err
+    }
+
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return err
+    }
+
+    lr.current = file
+    lr.currentSize = info.Size()
+    return nil
+}
+
+func (lr *LogRotator) Close() error {
+    if lr.current != nil {
+        return lr.current.Close()
+    }
+    return nil
+}
+
+func main() {
+    rotator, err := NewLogRotator("application.log")
+    if err != nil {
+        fmt.Printf("Failed to create log rotator: %v\n", err)
+        return
+    }
+    defer rotator.Close()
+
+    for i := 0; i < 1000; i++ {
+        message := fmt.Sprintf("Log entry %d: %s\n", i, time.Now().Format(time.RFC3339))
+        if _, err := rotator.Write([]byte(message)); err != nil {
+            fmt.Printf("Write error: %v\n", err)
+            break
+        }
+        time.Sleep(10 * time.Millisecond)
+    }
+}
