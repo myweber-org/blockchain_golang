fd276443d434347448fb99ab2a51d4f9be7de306
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-20 21:19:47 +0000

    Add file encryption utility with AES-256-GCM and key derivation

---
+++ vendor/github.com/koron/file_encryption_utility.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string) []byte {
+	hash := sha256.Sum256([]byte(passphrase))
+	return hash[:]
+}
+
+func encryptData(plaintext []byte, passphrase string) ([]byte, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, passphrase string) ([]byte, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < gcm.NonceSize() {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:gcm.NonceSize()], ciphertext[gcm.NonceSize():]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func main() {
+	secretMessage := "Confidential data: Project details and API keys"
+	passphrase := "securePass123!"
+
+	fmt.Println("Original:", secretMessage)
+
+	encrypted, err := encryptData([]byte(secretMessage), passphrase)
+	if err != nil {
+		fmt.Println("Encryption error:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Encrypted (hex):", hex.EncodeToString(encrypted))
+
+	decrypted, err := decryptData(encrypted, passphrase)
+	if err != nil {
+		fmt.Println("Decryption error:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Decrypted:", string(decrypted))
+}
