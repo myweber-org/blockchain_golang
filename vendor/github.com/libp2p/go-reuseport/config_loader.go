ccaf3784cbacfb6435c75ee0c08e34f014c1c8aa
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-21 15:10:47 +0000

    Add configuration loader with YAML support and environment variable overrides

---
+++ vendor/github.com/libp2p/go-reuseport/config_loader.go
@@ -0,0 +1,86 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Output string `yaml:"output" env:"LOG_OUTPUT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(&cfg)
+	return &cfg, nil
+}
+
+func overrideFromEnv(cfg *Config) {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		cfg.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		if port, err := parseInt(val); err == nil {
+			cfg.Server.Port = port
+		}
+	}
+	if val := os.Getenv("DB_HOST"); val != "" {
+		cfg.Database.Host = val
+	}
+	if val := os.Getenv("DB_PORT"); val != "" {
+		if port, err := parseInt(val); err == nil {
+			cfg.Database.Port = port
+		}
+	}
+	if val := os.Getenv("DB_NAME"); val != "" {
+		cfg.Database.Name = val
+	}
+	if val := os.Getenv("DB_USER"); val != "" {
+		cfg.Database.User = val
+	}
+	if val := os.Getenv("DB_PASSWORD"); val != "" {
+		cfg.Database.Password = val
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		cfg.Logging.Level = val
+	}
+	if val := os.Getenv("LOG_OUTPUT"); val != "" {
+		cfg.Logging.Output = val
+	}
+}
+
+func parseInt(s string) (int, error) {
+	var n int
+	_, err := fmt.Sscanf(s, "%d", &n)
+	return n, err
+}
