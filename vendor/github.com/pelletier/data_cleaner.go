3a2d5b71323377ddd8520024feb5bb1b964923d9
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-23 09:51:21 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- vendor/github.com/pelletier/data_cleaner.go
+++ vendor/github.com/pelletier/data_cleaner.go
@@ -42,3 +42,26 @@ func RemoveDuplicatesSorted[T comparable](input []T) []T {
 
 	return result
 }
+package main
+
+import "fmt"
+
+func removeDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{4, 2, 8, 2, 4, 9, 8, 1}
+	cleaned := removeDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
