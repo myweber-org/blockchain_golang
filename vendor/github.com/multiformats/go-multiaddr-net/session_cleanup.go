1f215f89ba8498e3859157f7e8f1b079d8d0a6e7
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-26 15:50:37 +0000

    Add session cleanup cron job to remove expired sessions daily

---
+++ vendor/github.com/multiformats/go-multiaddr-net/session_cleanup.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/yourproject/db"
+)
+
+const cleanupInterval = 24 * time.Hour
+
+func main() {
+	ticker := time.NewTicker(cleanupInterval)
+	defer ticker.Stop()
+
+	ctx := context.Background()
+	for range ticker.C {
+		if err := cleanupExpiredSessions(ctx); err != nil {
+			log.Printf("session cleanup failed: %v", err)
+		} else {
+			log.Println("session cleanup completed successfully")
+		}
+	}
+}
+
+func cleanupExpiredSessions(ctx context.Context) error {
+	query := `DELETE FROM user_sessions WHERE expires_at < NOW()`
+	result, err := db.Conn.ExecContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	rows, _ := result.RowsAffected()
+	log.Printf("deleted %d expired sessions", rows)
+	return nil
+}
