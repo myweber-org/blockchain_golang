73938f4dd5ed51a27abc3383044c83c8c15c8d4c
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-26 23:07:17 +0000

    Add user activity logging middleware for API endpoints

--- vendor/go.opencensus.io/stats/user_activity_logger.go
+++ vendor/go.opencensus.io/stats/user_activity_logger.go
@@ -40,4 +40,49 @@ func (rw *responseWriter) WriteHeader(code int) {
 
 func wrapResponseWriter(w http.ResponseWriter) *responseWriter {
 	return &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	UserID    string
+	Endpoint  string
+	Method    string
+	Timestamp time.Time
+	IPAddress string
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		userID := "anonymous"
+		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
+			userID = extractUserID(authHeader)
+		}
+
+		activity := ActivityLog{
+			UserID:    userID,
+			Endpoint:  r.URL.Path,
+			Method:    r.Method,
+			Timestamp: start,
+			IPAddress: r.RemoteAddr,
+		}
+
+		log.Printf("Activity: %s %s by %s from %s", 
+			activity.Method, 
+			activity.Endpoint, 
+			activity.UserID, 
+			activity.IPAddress)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func extractUserID(token string) string {
+	return "user_" + token[:8]
 }
