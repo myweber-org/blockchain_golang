c72c5d63a66145736cff23f0391d799091a128cf
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-27 01:00:14 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- vendor/github.com/hashicorp/hcl/hcl/token/data_cleaner.go
+++ vendor/github.com/hashicorp/hcl/hcl/token/data_cleaner.go
@@ -66,4 +66,18 @@ func main() {
 			fmt.Printf("%s is invalid\n", email)
 		}
 	}
+}package datautils
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+
+	return result
 }
