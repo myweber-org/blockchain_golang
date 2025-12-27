9ce35898f78b8bce3c999665382c06ca34dd9662
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-27 22:51:58 +0000

    Add utility function to sanitize user input by removing extra whitespace

--- vendor/github.com/ipfs/go-log/tracer/wire/data_cleaner.go
+++ vendor/github.com/ipfs/go-log/tracer/wire/data_cleaner.go
@@ -21,3 +21,19 @@ func main() {
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
 }
+package utils
+
+import (
+	"regexp"
+	"strings"
+)
+
+// SanitizeInput removes leading/trailing whitespace and collapses multiple spaces
+func SanitizeInput(input string) string {
+	// Trim spaces from start and end
+	trimmed := strings.TrimSpace(input)
+	
+	// Replace multiple spaces with single space
+	re := regexp.MustCompile(`\s+`)
+	return re.ReplaceAllString(trimmed, " ")
+}
