0bfb1d8913ff73e612fd363ef428e112477a4169
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-29 21:09:19 +0000

    Add user activity logging middleware for API endpoints

---
+++ vendor/github.com/jbenet/goprocess/user_activity_logger.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := extractUserID(r)
+	ipAddress := r.RemoteAddr
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s from %s accessed %s %s - Duration: %v",
+		userID, ipAddress, r.Method, r.URL.Path, duration)
+}
+
+func extractUserID(r *http.Request) string {
+	if userID := r.Header.Get("X-User-ID"); userID != "" {
+		return userID
+	}
+	return "anonymous"
+}
