928dad621b82c6338ca7a11411366dcef2980f78
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-29 16:40:11 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/libp2p/go-addr-util/user_activity_logger.go
+++ vendor/github.com/libp2p/go-addr-util/user_activity_logger.go
@@ -47,4 +47,30 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userAgent := r.Header.Get("User-Agent")
+	ipAddress := r.RemoteAddr
+	requestPath := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("Activity: %s | %s | %s | %v", ipAddress, userAgent, requestPath, duration)
 }
