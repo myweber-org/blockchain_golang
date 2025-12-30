4eec09d6e7685d945e660f785d4580e6ef7bd041
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-30 23:37:32 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- vendor/github.com/hashicorp/hcl/json/scanner/data_cleaner.go
+++ vendor/github.com/hashicorp/hcl/json/scanner/data_cleaner.go
@@ -13,3 +13,26 @@ func RemoveDuplicates[T comparable](slice []T) []T {
 
 	return result
 }
+package main
+
+import "fmt"
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 6, 6, 7}
+	cleaned := RemoveDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
