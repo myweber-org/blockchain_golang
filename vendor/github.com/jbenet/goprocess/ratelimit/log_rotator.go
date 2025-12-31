6ea68034e3ff6013caf56bc474f6d226ecb24523
InkspillVerse <inkspill.verse@outlook.com>
2025-12-31 21:20:00 +0000

    Implement a simple log rotation mechanism with size-based file rotation

--- vendor/github.com/jbenet/goprocess/ratelimit/log_rotator.go
+++ vendor/github.com/jbenet/goprocess/ratelimit/log_rotator.go
@@ -122,4 +122,114 @@ func main() {
         writer.Write([]byte(message))
         time.Sleep(10 * time.Millisecond)
     }
+}package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type RotatingWriter struct {
+	mu          sync.Mutex
+	currentFile *os.File
+	filePath    string
+	maxSize     int64
+	currentSize int64
+	rotationNum int
+}
+
+func NewRotatingWriter(filePath string, maxSize int64) (*RotatingWriter, error) {
+	writer := &RotatingWriter{
+		filePath: filePath,
+		maxSize:  maxSize,
+	}
+
+	if err := writer.openCurrentFile(); err != nil {
+		return nil, err
+	}
+
+	return writer, nil
+}
+
+func (w *RotatingWriter) openCurrentFile() error {
+	dir := filepath.Dir(w.filePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(w.filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	w.currentFile = file
+	w.currentSize = stat.Size()
+	return nil
+}
+
+func (w *RotatingWriter) rotate() error {
+	w.currentFile.Close()
+
+	backupPath := fmt.Sprintf("%s.%d", w.filePath, w.rotationNum)
+	if err := os.Rename(w.filePath, backupPath); err != nil {
+		return err
+	}
+
+	w.rotationNum++
+	return w.openCurrentFile()
+}
+
+func (w *RotatingWriter) Write(p []byte) (int, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.currentSize+int64(len(p)) > w.maxSize {
+		if err := w.rotate(); err != nil {
+			return 0, err
+		}
+	}
+
+	n, err := w.currentFile.Write(p)
+	if err == nil {
+		w.currentSize += int64(n)
+	}
+	return n, err
+}
+
+func (w *RotatingWriter) Close() error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.currentFile != nil {
+		return w.currentFile.Close()
+	}
+	return nil
+}
+
+func main() {
+	writer, err := NewRotatingWriter("logs/app.log", 1024*10)
+	if err != nil {
+		fmt.Printf("Failed to create writer: %v\n", err)
+		return
+	}
+	defer writer.Close()
+
+	for i := 0; i < 100; i++ {
+		message := fmt.Sprintf("Log entry %d: This is a sample log message.\n", i)
+		if _, err := writer.Write([]byte(message)); err != nil {
+			fmt.Printf("Write error: %v\n", err)
+			break
+		}
+	}
+
+	fmt.Println("Log rotation test completed")
 }
