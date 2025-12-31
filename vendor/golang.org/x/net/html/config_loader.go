5429f308b397edc46cddff4d3ab5eed26de27e8c
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-31 23:07:59 +0000

    Add configuration loader with YAML parsing support

---
+++ vendor/golang.org/x/net/html/config_loader.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+}
+
+type ServerConfig struct {
+	Port         int    `yaml:"port"`
+	ReadTimeout  int    `yaml:"read_timeout"`
+	WriteTimeout int    `yaml:"write_timeout"`
+	DebugMode    bool   `yaml:"debug_mode"`
+	LogLevel     string `yaml:"log_level"`
+}
+
+type AppConfig struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+}
+
+func LoadConfig(filePath string) (*AppConfig, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Printf("Failed to read config file: %v", err)
+		return nil, err
+	}
+
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Printf("Failed to parse YAML config: %v", err)
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) error {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		return fmt.Errorf("invalid server port: %d", config.Server.Port)
+	}
+
+	if config.Database.Host == "" {
+		return fmt.Errorf("database host cannot be empty")
+	}
+
+	if config.Database.Port <= 0 || config.Database.Port > 65535 {
+		return fmt.Errorf("invalid database port: %d", config.Database.Port)
+	}
+
+	return nil
+}
