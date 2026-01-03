1d50458b088cbdfd3c9260589be6f3409d0024df
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-03 01:00:39 +0000

    Add file encryption utility with AES-GCM and key derivation

--- vendor/github.com/libp2p/go-libp2p/p2p/host/basic/file_encryptor.go
+++ vendor/github.com/libp2p/go-libp2p/p2p/host/basic/file_encryptor.go
@@ -108,4 +108,131 @@ func main() {
 		fmt.Println("Invalid mode. Use 'encrypt' or 'decrypt'")
 		os.Exit(1)
 	}
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encryptData(data []byte, passphrase string) ([]byte, error) {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return nil, err
+	}
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return append(salt, ciphertext...), nil
+}
+
+func decryptData(encrypted []byte, passphrase string) ([]byte, error) {
+	if len(encrypted) < 16 {
+		return nil, errors.New("invalid encrypted data")
+	}
+
+	salt := encrypted[:16]
+	ciphertext := encrypted[16:]
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("invalid ciphertext")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func processFile(inputPath, outputPath, passphrase string, encrypt bool) error {
+	inputFile, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+
+	inputData, err := io.ReadAll(inputFile)
+	if err != nil {
+		return err
+	}
+
+	var outputData []byte
+	if encrypt {
+		outputData, err = encryptData(inputData, passphrase)
+	} else {
+		outputData, err = decryptData(inputData, passphrase)
+	}
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, outputData, 0644)
+}
+
+func main() {
+	if len(os.Args) != 5 {
+		fmt.Println("Usage: file_encryptor <input> <output> <passphrase> <encrypt|decrypt>")
+		os.Exit(1)
+	}
+
+	inputPath := os.Args[1]
+	outputPath := os.Args[2]
+	passphrase := os.Args[3]
+	mode := os.Args[4]
+
+	var encrypt bool
+	switch mode {
+	case "encrypt":
+		encrypt = true
+	case "decrypt":
+		encrypt = false
+	default:
+		fmt.Println("Mode must be 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+
+	if err := processFile(inputPath, outputPath, passphrase, encrypt); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Operation completed successfully")
 }
