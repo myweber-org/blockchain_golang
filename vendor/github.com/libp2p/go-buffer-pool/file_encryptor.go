b5d952f80d3be8ef84e4b608d9ed79c02babd40f
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-03 21:04:55 +0000

    Add file encryption utility with AES-GCM and key derivation

---
+++ vendor/github.com/libp2p/go-buffer-pool/file_encryptor.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encryptFile(inputPath, outputPath, passphrase string) error {
+	input, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return err
+	}
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return err
+	}
+
+	ciphertext := gcm.Seal(nil, nonce, input, nil)
+	output := append(salt, nonce...)
+	output = append(output, ciphertext...)
+
+	return os.WriteFile(outputPath, output, 0644)
+}
+
+func decryptFile(inputPath, outputPath, passphrase string) error {
+	input, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	if len(input) < 48 {
+		return fmt.Errorf("file too short")
+	}
+
+	salt := input[:16]
+	nonce := input[16:32]
+	ciphertext := input[32:]
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
+
+func main() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: file_encryptor <encrypt|decrypt> <input> <output> <passphrase>")
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputPath := os.Args[2]
+	outputPath := os.Args[3]
+	passphrase := os.Args[4]
+
+	var err error
+	switch operation {
+	case "encrypt":
+		err = encryptFile(inputPath, outputPath, passphrase)
+	case "decrypt":
+		err = decryptFile(inputPath, outputPath, passphrase)
+	default:
+		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Operation completed successfully")
+}
