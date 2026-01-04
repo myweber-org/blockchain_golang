621592e162a9846e6423a67115801c4f2882afc9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-04 01:07:49 +0000

    Add configuration loader with YAML parsing support

---
+++ vendor/github.com/libp2p/go-libp2p-secio/config_loader.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+
+    "gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+    Host     string `yaml:"host"`
+    Port     int    `yaml:"port"`
+    Username string `yaml:"username"`
+    Password string `yaml:"password"`
+    Name     string `yaml:"name"`
+}
+
+type ServerConfig struct {
+    Port         int            `yaml:"port"`
+    ReadTimeout  int            `yaml:"read_timeout"`
+    WriteTimeout int            `yaml:"write_timeout"`
+    Database     DatabaseConfig `yaml:"database"`
+}
+
+func LoadConfig(configPath string) (*ServerConfig, error) {
+    if configPath == "" {
+        configPath = "config.yaml"
+    }
+
+    file, err := os.Open(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open config file: %w", err)
+    }
+    defer file.Close()
+
+    data, err := ioutil.ReadAll(file)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config ServerConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML config: %w", err)
+    }
+
+    if config.Port == 0 {
+        config.Port = 8080
+    }
+    if config.ReadTimeout == 0 {
+        config.ReadTimeout = 30
+    }
+    if config.WriteTimeout == 0 {
+        config.WriteTimeout = 30
+    }
+
+    return &config, nil
+}
+
+func ValidateConfig(config *ServerConfig) error {
+    if config.Database.Host == "" {
+        return fmt.Errorf("database host is required")
+    }
+    if config.Database.Port == 0 {
+        return fmt.Errorf("database port is required")
+    }
+    if config.Database.Name == "" {
+        return fmt.Errorf("database name is required")
+    }
+    return nil
+}
