9284e9781208316a4de4bf13c20cbba5e51043c1
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-04 23:17:29 +0000

    Add file encryption utility with AES-GCM and key derivation

---
+++ vendor/github.com/libp2p/go-stream-muxer-multistream/file_encryption_tool.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string) []byte {
+	hash := sha256.Sum256([]byte(passphrase))
+	return hash[:]
+}
+
+func encrypt(plaintext []byte, passphrase string) (string, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func decrypt(encodedCiphertext string, passphrase string) ([]byte, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(encodedCiphertext)
+	if err != nil {
+		return nil, err
+	}
+
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run file_encryption_tool.go <encrypt|decrypt> <input_file> <output_file>")
+		fmt.Print("Enter passphrase: ")
+		var passphrase string
+		fmt.Scanln(&passphrase)
+		os.Exit(1)
+	}
+
+	action := os.Args[1]
+	inputFile := os.Args[2]
+	outputFile := os.Args[3]
+
+	fmt.Print("Enter passphrase: ")
+	var passphrase string
+	fmt.Scanln(&passphrase)
+
+	inputData, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading input file: %v\n", err)
+		os.Exit(1)
+	}
+
+	switch action {
+	case "encrypt":
+		encrypted, err := encrypt(inputData, passphrase)
+		if err != nil {
+			fmt.Printf("Encryption error: %v\n", err)
+			os.Exit(1)
+		}
+		err = os.WriteFile(outputFile, []byte(encrypted), 0644)
+		if err != nil {
+			fmt.Printf("Error writing output file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("File encrypted successfully")
+
+	case "decrypt":
+		decrypted, err := decrypt(string(inputData), passphrase)
+		if err != nil {
+			fmt.Printf("Decryption error: %v\n", err)
+			os.Exit(1)
+		}
+		err = os.WriteFile(outputFile, decrypted, 0644)
+		if err != nil {
+			fmt.Printf("Error writing output file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("File decrypted successfully")
+
+	default:
+		fmt.Println("Invalid action. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+}
