a66076f22a01bd87e11b60e2874e64b0270779d1
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-05 21:26:35 +0000

    Add configuration loader with YAML parsing and environment variable support

--- vendor/github.com/libp2p/go-libp2p-swarm/config_loader.go
+++ vendor/github.com/libp2p/go-libp2p-swarm/config_loader.go
@@ -53,4 +53,76 @@ func validateConfig(config *ServerConfig) error {
     }
 
     return nil
+}package config
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+	} `yaml:"database"`
+	Server struct {
+		Port         int      `yaml:"port"`
+		ReadTimeout  int      `yaml:"read_timeout"`
+		WriteTimeout int      `yaml:"write_timeout"`
+		AllowedHosts []string `yaml:"allowed_hosts"`
+	} `yaml:"server"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	config := &Config{}
+	
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(config)
+	return config, nil
+}
+
+func overrideFromEnv(config *Config) {
+	if host := os.Getenv("DB_HOST"); host != "" {
+		config.Database.Host = host
+	}
+	if port := os.Getenv("DB_PORT"); port != "" {
+		config.Database.Port = atoi(port)
+	}
+	if user := os.Getenv("DB_USER"); user != "" {
+		config.Database.Username = user
+	}
+	if pass := os.Getenv("DB_PASS"); pass != "" {
+		config.Database.Password = pass
+	}
+	if level := os.Getenv("LOG_LEVEL"); level != "" {
+		config.Logging.Level = strings.ToUpper(level)
+	}
+}
+
+func atoi(s string) int {
+	var result int
+	for _, ch := range s {
+		if ch >= '0' && ch <= '9' {
+			result = result*10 + int(ch-'0')
+		}
+	}
+	return result
 }
