436ab81471f6994af1ee20973b0be4d335099583
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-08 16:55:36 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/libp2p/go-addr-util/user_activity_logger.go
+++ vendor/github.com/libp2p/go-addr-util/user_activity_logger.go
@@ -73,4 +73,54 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	duration := time.Since(start)
 	log.Printf("Activity: %s | %s | %s | %v", ipAddress, userAgent, requestPath, duration)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	UserID    string
+	Endpoint  string
+	Method    string
+	Timestamp time.Time
+	IPAddress string
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		userID := "anonymous"
+		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
+			userID = extractUserID(authHeader)
+		}
+
+		activity := ActivityLog{
+			UserID:    userID,
+			Endpoint:  r.URL.Path,
+			Method:    r.Method,
+			Timestamp: start,
+			IPAddress: r.RemoteAddr,
+		}
+
+		logActivity(activity)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func extractUserID(token string) string {
+	return "user_" + token[:8]
+}
+
+func logActivity(activity ActivityLog) {
+	log.Printf("ACTIVITY: User %s accessed %s %s from %s at %v",
+		activity.UserID,
+		activity.Method,
+		activity.Endpoint,
+		activity.IPAddress,
+		activity.Timestamp.Format(time.RFC3339))
 }
