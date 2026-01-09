8c372211a9545b9078caef255b04ac27d41bcb5a
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-09 20:41:09 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- vendor/golang.org/x/text/encoding/data_cleaner.go
+++ vendor/golang.org/x/text/encoding/data_cleaner.go
@@ -44,4 +44,16 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
+}package datautils
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
 }
