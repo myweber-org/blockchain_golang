3fde727b4f9bd38b84d62a922e89ca462e79ddc2
InkspillVerse <inkspill.verse@outlook.com>
2026-01-09 23:53:56 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- vendor/golang.org/x/crypto/ed25519/internal/data_cleaner.go
+++ vendor/golang.org/x/crypto/ed25519/internal/data_cleaner.go
@@ -42,4 +42,64 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Printf("Original: %v\n", data)
 	fmt.Printf("Cleaned: %v\n", cleaned)
+}package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: data_cleaner <input.csv>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := strings.TrimSuffix(inputFile, ".csv") + "_cleaned.csv"
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Printf("Error reading CSV: %v\n", err)
+		os.Exit(1)
+	}
+
+	uniqueMap := make(map[string]bool)
+	var uniqueRecords [][]string
+
+	for _, record := range records {
+		key := strings.Join(record, "|")
+		if !uniqueMap[key] {
+			uniqueMap[key] = true
+			uniqueRecords = append(uniqueRecords, record)
+		}
+	}
+
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+
+	writer := csv.NewWriter(outFile)
+	err = writer.WriteAll(uniqueRecords)
+	if err != nil {
+		fmt.Printf("Error writing CSV: %v\n", err)
+		os.Exit(1)
+	}
+
+	writer.Flush()
+	fmt.Printf("Cleaned data saved to: %s\n", outputFile)
+	fmt.Printf("Removed %d duplicate rows\n", len(records)-len(uniqueRecords))
 }
