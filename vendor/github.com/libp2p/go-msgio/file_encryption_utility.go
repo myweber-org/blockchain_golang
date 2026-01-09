2cf6a2c428ecc578d472ce1b22b27517f996899b
InkspillVerse <inkspill.verse@outlook.com>
2026-01-09 21:51:47 +0000

    Add AES-256 file encryption utility with key derivation

---
+++ vendor/github.com/libp2p/go-msgio/file_encryption_utility.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+const (
+	saltSize      = 16
+	nonceSize     = 12
+	keyIterations = 100000
+)
+
+func deriveKey(password string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hash.Write(salt)
+	for i := 0; i < keyIterations; i++ {
+		hash.Write(hash.Sum(nil))
+	}
+	return hash.Sum(nil)
+}
+
+func encryptFile(inputPath, outputPath, password string) error {
+	salt := make([]byte, saltSize)
+	if _, err := rand.Read(salt); err != nil {
+		return err
+	}
+
+	key := deriveKey(password, salt)
+
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, nonceSize)
+	if _, err := rand.Read(nonce); err != nil {
+		return err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+
+	outputData := make([]byte, 0, len(salt)+len(nonce)+len(ciphertext))
+	outputData = append(outputData, salt...)
+	outputData = append(outputData, nonce...)
+	outputData = append(outputData, ciphertext...)
+
+	return os.WriteFile(outputPath, outputData, 0644)
+}
+
+func decryptFile(inputPath, outputPath, password string) error {
+	encryptedData, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	if len(encryptedData) < saltSize+nonceSize {
+		return errors.New("file too short")
+	}
+
+	salt := encryptedData[:saltSize]
+	nonce := encryptedData[saltSize : saltSize+nonceSize]
+	ciphertext := encryptedData[saltSize+nonceSize:]
+
+	key := deriveKey(password, salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return errors.New("decryption failed - wrong password or corrupted file")
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
+
+func main() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: go run file_encryption_utility.go <encrypt|decrypt> <input> <output> <password>")
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputPath := os.Args[2]
+	outputPath := os.Args[3]
+	password := os.Args[4]
+
+	switch operation {
+	case "encrypt":
+		if err := encryptFile(inputPath, outputPath, password); err != nil {
+			fmt.Printf("Encryption failed: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("File encrypted successfully: %s\n", outputPath)
+	case "decrypt":
+		if err := decryptFile(inputPath, outputPath, password); err != nil {
+			fmt.Printf("Decryption failed: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("File decrypted successfully: %s\n", outputPath)
+	default:
+		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+}
