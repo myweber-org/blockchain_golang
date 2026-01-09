1b56345d2ff4e2870e3a11791109e57d5c9eddd9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-09 21:32:19 +0000

    Add user activity logging middleware with rate limiting

--- vendor/github.com/spf13/cast/user_activity_logger.go
+++ vendor/github.com/spf13/cast/user_activity_logger.go
@@ -3,27 +3,56 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
 type ActivityLogger struct {
-	handler http.Handler
+	mu          sync.RWMutex
+	userLimits  map[string]time.Time
+	rateLimit   time.Duration
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(limit time.Duration) *ActivityLogger {
+	return &ActivityLogger{
+		userLimits: make(map[string]time.Time),
+		rateLimit:  limit,
+	}
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
+func (al *ActivityLogger) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userID := r.Header.Get("X-User-ID")
+		if userID == "" {
+			userID = "anonymous"
+		}
 
-	log.Printf(
-		"Activity: %s %s from %s completed in %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+		if !al.checkRateLimit(userID) {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+
+		al.logActivity(userID, r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (al *ActivityLogger) checkRateLimit(userID string) bool {
+	al.mu.Lock()
+	defer al.mu.Unlock()
+
+	lastTime, exists := al.userLimits[userID]
+	now := time.Now()
+
+	if exists && now.Sub(lastTime) < al.rateLimit {
+		return false
+	}
+
+	al.userLimits[userID] = now
+	return true
+}
+
+func (al *ActivityLogger) logActivity(userID, method, path string) {
+	log.Printf("Activity: user=%s method=%s path=%s time=%s",
+		userID, method, path, time.Now().Format(time.RFC3339))
 }
