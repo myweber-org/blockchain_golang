0802a7c33f0ee0847562ec5e5eab2a8621af1c44
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-10 01:51:58 +0000

    Add user activity logging middleware to track API usage

--- vendor/github.com/opentracing/opentracing-go/ext/user_activity_logger.go
+++ vendor/github.com/opentracing/opentracing-go/ext/user_activity_logger.go
@@ -1,80 +1,4 @@
-package main
-
-import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "os"
-    "time"
-)
-
-type Activity struct {
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Timestamp time.Time `json:"timestamp"`
-    Details   string    `json:"details,omitempty"`
-}
-
-type ActivityLogger struct {
-    logFile *os.File
-}
-
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return nil, err
-    }
-    return &ActivityLogger{logFile: file}, nil
-}
-
-func (l *ActivityLogger) LogActivity(userID, action, details string) error {
-    activity := Activity{
-        UserID:    userID,
-        Action:    action,
-        Timestamp: time.Now().UTC(),
-        Details:   details,
-    }
-
-    data, err := json.Marshal(activity)
-    if err != nil {
-        return err
-    }
-
-    data = append(data, '\n')
-    _, err = l.logFile.Write(data)
-    return err
-}
-
-func (l *ActivityLogger) Close() error {
-    return l.logFile.Close()
-}
-
-func main() {
-    logger, err := NewActivityLogger("user_activities.jsonl")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer logger.Close()
-
-    activities := []struct {
-        userID  string
-        action  string
-        details string
-    }{
-        {"user_123", "login", "Successful authentication"},
-        {"user_456", "purchase", "Order ID: ORD-78910"},
-        {"user_123", "logout", "Session duration: 45m"},
-        {"user_789", "view_page", "/products/electronics"},
-    }
-
-    for _, act := range activities {
-        if err := logger.LogActivity(act.userID, act.action, act.details); err != nil {
-            log.Printf("Failed to log activity: %v", err)
-        }
-    }
-
-    fmt.Println("Activity logging completed")
-}package middleware
+package middleware
 
 import (
 	"log"
@@ -92,15 +16,30 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+
+	al.handler.ServeHTTP(recorder, r)
+
 	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
+	log.Printf(
+		"%s %s %d %s %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
+		recorder.statusCode,
 		duration,
+		r.RemoteAddr,
 	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
