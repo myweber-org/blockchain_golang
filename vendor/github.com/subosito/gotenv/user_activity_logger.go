defd23abeb6f0e76679efee31f0d173893b33faf
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-10 14:36:37 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/subosito/gotenv/user_activity_logger.go
+++ vendor/github.com/subosito/gotenv/user_activity_logger.go
@@ -45,4 +45,38 @@ func ActivityLogger(next http.Handler) http.Handler {
 
 func extractUserIDFromToken(token string) string {
 	return "user_" + token[:8]
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	writer := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+	
+	al.handler.ServeHTTP(writer, r)
+	
+	duration := time.Since(start)
+	log.Printf("%s %s %d %s", r.Method, r.URL.Path, writer.statusCode, duration)
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
