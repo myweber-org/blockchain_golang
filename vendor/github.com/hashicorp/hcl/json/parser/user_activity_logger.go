45a28c27804b5f1c2b08d3454e55240b157a81b1
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-10 19:52:11 +0000

    Add user activity logging with timestamp and action type

--- vendor/github.com/hashicorp/hcl/json/parser/user_activity_logger.go
+++ vendor/github.com/hashicorp/hcl/json/parser/user_activity_logger.go
@@ -1,36 +1,78 @@
-package middleware
+package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-	"net/http"
+	"os"
 	"time"
 )
 
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode int
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Resource  string    `json:"resource"`
 }
 
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
+type ActivityLogger struct {
+	logFile *os.File
 }
 
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
+}
 
-		next.ServeHTTP(rw, r)
+func (al *ActivityLogger) LogActivity(userID, action, resource string) error {
+	activity := ActivityLog{
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		Action:    action,
+		Resource:  resource,
+	}
 
-		duration := time.Since(start)
-		log.Printf(
-			"[%s] %s %s %d %v",
-			time.Now().Format(time.RFC3339),
-			r.Method,
-			r.URL.Path,
-			rw.statusCode,
-			duration,
-		)
-	})
+	data, err := json.Marshal(activity)
+	if err != nil {
+		return err
+	}
+
+	data = append(data, '\n')
+	_, err = al.logFile.Write(data)
+	return err
+}
+
+func (al *ActivityLogger) Close() error {
+	return al.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	activities := []struct {
+		userID   string
+		action   string
+		resource string
+	}{
+		{"user_001", "LOGIN", "auth_system"},
+		{"user_002", "CREATE", "document_123"},
+		{"user_001", "UPDATE", "profile_settings"},
+		{"user_003", "DELETE", "comment_456"},
+	}
+
+	for _, act := range activities {
+		err := logger.LogActivity(act.userID, act.action, act.resource)
+		if err != nil {
+			log.Printf("Failed to log activity: %v", err)
+		} else {
+			fmt.Printf("Logged: %s performed %s on %s\n", act.userID, act.action, act.resource)
+		}
+	}
 }
