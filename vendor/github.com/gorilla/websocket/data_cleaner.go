bbfb7c8a43b157c16c09471549e1b3562452a9a3
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-11 15:36:26 +0000

    Add data cleaning utility to remove duplicate entries from a slice of strings

--- vendor/github.com/gorilla/websocket/data_cleaner.go
+++ vendor/github.com/gorilla/websocket/data_cleaner.go
@@ -112,4 +112,18 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
+}package datautils
+
+func RemoveDuplicates(input []string) []string {
+	seen := make(map[string]bool)
+	result := []string{}
+
+	for _, item := range input {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
 }
