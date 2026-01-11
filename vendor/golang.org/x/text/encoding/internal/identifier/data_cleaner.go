5d5a7f2e5b4b7aa71774fbb49d4864537d02ee1b
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-11 23:03:20 +0000

    Add utility function to sanitize user input by removing extra whitespace

--- vendor/golang.org/x/text/encoding/internal/identifier/data_cleaner.go
+++ vendor/golang.org/x/text/encoding/internal/identifier/data_cleaner.go
@@ -1,24 +1,18 @@
 
-package main
+package utils
 
-import "fmt"
+import (
+	"regexp"
+	"strings"
+)
 
-func removeDuplicates(nums []int) []int {
-	seen := make(map[int]bool)
-	result := []int{}
-
-	for _, num := range nums {
-		if !seen[num] {
-			seen[num] = true
-			result = append(result, num)
-		}
-	}
-	return result
-}
-
-func main() {
-	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
-	cleaned := removeDuplicates(data)
-	fmt.Printf("Original: %v\n", data)
-	fmt.Printf("Cleaned: %v\n", cleaned)
+func SanitizeInput(input string) string {
+	// Trim leading and trailing whitespace
+	trimmed := strings.TrimSpace(input)
+	
+	// Replace multiple spaces with a single space
+	re := regexp.MustCompile(`\s+`)
+	cleaned := re.ReplaceAllString(trimmed, " ")
+	
+	return cleaned
 }
