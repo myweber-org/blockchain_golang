2ffcc8e9979afe7add2896793cc7c33e1b64fa5c
InkspillVerse <inkspill.verse@outlook.com>
2026-01-13 17:11:44 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/user_activity_logger.go
+++ vendor/github.com/user_activity_logger.go
@@ -39,4 +39,42 @@ func ActivityLogger(next http.Handler) http.Handler {
 
 func extractUserID(token string) string {
 	return "user_" + token[:8]
+}package main
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+type ActivityLogger struct {
+    handler http.Handler
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    start := time.Now()
+    
+    al.handler.ServeHTTP(w, r)
+    
+    duration := time.Since(start)
+    log.Printf("%s %s %s %v", r.RemoteAddr, r.Method, r.URL.Path, duration)
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+    return &ActivityLogger{handler: handler}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("API response"))
+}
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/api", apiHandler)
+    
+    loggedMux := NewActivityLogger(mux)
+    
+    log.Println("Server starting on :8080")
+    http.ListenAndServe(":8080", loggedMux)
 }
