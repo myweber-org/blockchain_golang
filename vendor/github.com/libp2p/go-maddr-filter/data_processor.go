d17805a9325df0a6ca367d6416302da6687c3631
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-13 23:46:26 +0000

    Add data processing utility for cleaning and validating input strings

--- vendor/github.com/libp2p/go-maddr-filter/data_processor.go
+++ vendor/github.com/libp2p/go-maddr-filter/data_processor.go
@@ -17,3 +17,26 @@ func ValidateEmail(email string) bool {
 	matched, _ := regexp.MatchString(pattern, email)
 	return matched
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func CleanInput(input string) string {
+	trimmed := strings.TrimSpace(input)
+	re := regexp.MustCompile(`\s+`)
+	return re.ReplaceAllString(trimmed, " ")
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func ContainsOnlyAlphanumeric(s string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+	return re.MatchString(s)
+}
