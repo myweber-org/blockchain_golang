983cea77c97900424db7bad883a5928f27b170b0
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-14 22:54:45 +0000

    Add file encryption utility with AES-256-GCM

---
+++ vendor/golang.org/x/crypto/file_encryptor.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encryptFile(inputPath, outputPath string, key []byte) error {
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return fmt.Errorf("read file error: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return fmt.Errorf("cipher creation error: %v", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return fmt.Errorf("gcm creation error: %v", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return fmt.Errorf("nonce generation error: %v", err)
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+
+	if err := os.WriteFile(outputPath, ciphertext, 0644); err != nil {
+		return fmt.Errorf("write file error: %v", err)
+	}
+
+	return nil
+}
+
+func decryptFile(inputPath, outputPath string, key []byte) error {
+	ciphertext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return fmt.Errorf("read file error: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return fmt.Errorf("cipher creation error: %v", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return fmt.Errorf("gcm creation error: %v", err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return fmt.Errorf("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return fmt.Errorf("decryption error: %v", err)
+	}
+
+	if err := os.WriteFile(outputPath, plaintext, 0644); err != nil {
+		return fmt.Errorf("write file error: %v", err)
+	}
+
+	return nil
+}
+
+func generateKey() ([]byte, error) {
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		return nil, fmt.Errorf("key generation error: %v", err)
+	}
+	return key, nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_encryptor <command> [arguments]")
+		fmt.Println("Commands:")
+		fmt.Println("  genkey                    Generate encryption key")
+		fmt.Println("  encrypt <file> <key>      Encrypt file")
+		fmt.Println("  decrypt <file> <key>      Decrypt file")
+		return
+	}
+
+	switch os.Args[1] {
+	case "genkey":
+		key, err := generateKey()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Generated key: %s\n", hex.EncodeToString(key))
+
+	case "encrypt":
+		if len(os.Args) != 4 {
+			fmt.Println("Usage: file_encryptor encrypt <input> <key_hex>")
+			os.Exit(1)
+		}
+		key, err := hex.DecodeString(os.Args[3])
+		if err != nil {
+			fmt.Printf("Invalid key: %v\n", err)
+			os.Exit(1)
+		}
+		output := os.Args[2] + ".enc"
+		if err := encryptFile(os.Args[2], output, key); err != nil {
+			fmt.Printf("Encryption failed: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("File encrypted: %s\n", output)
+
+	case "decrypt":
+		if len(os.Args) != 4 {
+			fmt.Println("Usage: file_encryptor decrypt <input> <key_hex>")
+			os.Exit(1)
+		}
+		key, err := hex.DecodeString(os.Args[3])
+		if err != nil {
+			fmt.Printf("Invalid key: %v\n", err)
+			os.Exit(1)
+		}
+		output := os.Args[2] + ".dec"
+		if err := decryptFile(os.Args[2], output, key); err != nil {
+			fmt.Printf("Decryption failed: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("File decrypted: %s\n", output)
+
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+}
