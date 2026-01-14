30a7b0fef649ba04aeed074fd7e4080d4bd556b8
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-14 20:02:58 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/libp2p/go-libp2p/p2p/discovery/user_activity_logger.go
+++ vendor/github.com/libp2p/go-libp2p/p2p/discovery/user_activity_logger.go
@@ -1,63 +1,4 @@
-
 package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLog struct {
-	Timestamp time.Time
-	Method    string
-	Path      string
-	UserAgent string
-	IPAddress string
-	Duration  time.Duration
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-
-		next.ServeHTTP(recorder, r)
-
-		duration := time.Since(start)
-
-		activity := ActivityLog{
-			Timestamp: time.Now().UTC(),
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			UserAgent: r.UserAgent(),
-			IPAddress: r.RemoteAddr,
-			Duration:  duration,
-		}
-
-		log.Printf("Activity: %s %s | IP: %s | Agent: %s | Duration: %v | Status: %d",
-			activity.Method,
-			activity.Path,
-			activity.IPAddress,
-			activity.UserAgent,
-			activity.Duration,
-			recorder.statusCode,
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (r *responseRecorder) WriteHeader(statusCode int) {
-	r.statusCode = statusCode
-	r.ResponseWriter.WriteHeader(statusCode)
-}package middleware
 
 import (
 	"log"
@@ -75,32 +16,13 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
+	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
-	
-	log.Printf(
-		"%s %s %d %s %s",
+
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
-		recorder.statusCode,
-		duration,
 		r.RemoteAddr,
+		duration,
 	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
 }
