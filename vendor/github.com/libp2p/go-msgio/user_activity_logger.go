f6183d634d9cad64190e40b2e98cac35b341d1c7
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-15 15:53:44 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/libp2p/go-msgio/user_activity_logger.go
+++ vendor/github.com/libp2p/go-msgio/user_activity_logger.go
@@ -1,49 +1,4 @@
-package main
-
-import (
-    "encoding/json"
-    "log"
-    "os"
-    "time"
-)
-
-type ActivityLog struct {
-    Timestamp time.Time `json:"timestamp"`
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Details   string    `json:"details,omitempty"`
-}
-
-func logActivity(userID, action, details string) error {
-    logEntry := ActivityLog{
-        Timestamp: time.Now().UTC(),
-        UserID:    userID,
-        Action:    action,
-        Details:   details,
-    }
-
-    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    encoder := json.NewEncoder(file)
-    encoder.SetIndent("", "  ")
-    if err := encoder.Encode(logEntry); err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func main() {
-    err := logActivity("user123", "LOGIN", "User logged in from IP 192.168.1.100")
-    if err != nil {
-        log.Fatal("Failed to log activity:", err)
-    }
-    log.Println("Activity logged successfully")
-}package middleware
+package middleware
 
 import (
 	"log"
@@ -52,44 +7,22 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
+	handler http.Handler
 }
 
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
