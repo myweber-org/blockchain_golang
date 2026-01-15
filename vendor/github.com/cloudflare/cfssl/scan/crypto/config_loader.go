610a837b3726c8c637b951890bc3b51bdc3b3b31
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-15 20:23:57 +0000

    Add configuration loader with YAML support and environment variable overrides

--- vendor/github.com/cloudflare/cfssl/scan/crypto/config_loader.go
+++ vendor/github.com/cloudflare/cfssl/scan/crypto/config_loader.go
@@ -58,4 +58,67 @@ func validateConfig(c *Config) error {
         c.LogLevel = "info"
     }
     return nil
+}package config
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		URL      string `yaml:"url"`
+		PoolSize int    `yaml:"pool_size"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	overrideWithEnv(&cfg)
+	return &cfg, nil
+}
+
+func overrideWithEnv(cfg *Config) {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		cfg.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		cfg.Server.Port = atoi(val)
+	}
+	if val := os.Getenv("DATABASE_URL"); val != "" {
+		cfg.Database.URL = val
+	}
+	if val := os.Getenv("DATABASE_POOL_SIZE"); val != "" {
+		cfg.Database.PoolSize = atoi(val)
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		cfg.LogLevel = strings.ToUpper(val)
+	}
+}
+
+func atoi(s string) int {
+	var n int
+	for _, ch := range s {
+		if ch < '0' || ch > '9' {
+			break
+		}
+		n = n*10 + int(ch-'0')
+	}
+	return n
 }
