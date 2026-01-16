1e939052f03f7b0da1f320220082be857d1dbd5d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-16 01:14:00 +0000

    Add file encryption utility with AES-256-GCM and key derivation

---
+++ vendor/github.com/libp2p/go-libp2p/p2p/host/basic/file_encryption_tool.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string) []byte {
+	hash := sha256.Sum256([]byte(passphrase))
+	return hash[:]
+}
+
+func encryptData(plaintext []byte, passphrase string) (string, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func decryptData(encrypted string, passphrase string) ([]byte, error) {
+	key := deriveKey(passphrase)
+	data, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run file_encryption_tool.go <encrypt|decrypt> <input_file> <passphrase>")
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	filename := os.Args[2]
+	passphrase := os.Args[3]
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	switch operation {
+	case "encrypt":
+		encrypted, err := encryptData(data, passphrase)
+		if err != nil {
+			fmt.Printf("Encryption failed: %v\n", err)
+			os.Exit(1)
+		}
+		outputFile := filename + ".enc"
+		if err := os.WriteFile(outputFile, []byte(encrypted), 0644); err != nil {
+			fmt.Printf("Error writing encrypted file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Encrypted data written to %s\n", outputFile)
+
+	case "decrypt":
+		decrypted, err := decryptData(string(data), passphrase)
+		if err != nil {
+			fmt.Printf("Decryption failed: %v\n", err)
+			os.Exit(1)
+		}
+		outputFile := filename + ".dec"
+		if err := os.WriteFile(outputFile, decrypted, 0644); err != nil {
+			fmt.Printf("Error writing decrypted file: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Decrypted data written to %s\n", outputFile)
+
+	default:
+		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+}
