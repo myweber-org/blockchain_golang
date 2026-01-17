25efbce8417708b801448f609bd6607c8638f3f7
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-17 21:31:40 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/btcsuite/user_activity_logger.go
+++ vendor/github.com/btcsuite/user_activity_logger.go
@@ -26,4 +26,34 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf("[%s] %s %s - %s - Duration: %v",
+		time.Now().Format(time.RFC3339),
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
