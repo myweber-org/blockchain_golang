2f0d92123806d0674d07ad5e0d24f32ac878cb15
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-17 14:25:03 +0000

    Add user activity logging middleware for tracking API requests

--- vendor/github.com/hashicorp/hcl/hcl/strconv/user_activity_logger.go
+++ vendor/github.com/hashicorp/hcl/hcl/strconv/user_activity_logger.go
@@ -54,4 +54,58 @@ func logActivity(activity UserActivity) {
 func GetActivityFromContext(ctx context.Context) (UserActivity, bool) {
 	activity, ok := ctx.Value(UserActivityKey).(UserActivity)
 	return activity, ok
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp  time.Time
+	Method     string
+	Path       string
+	RemoteAddr string
+	UserAgent  string
+	StatusCode int
+	Duration   time.Duration
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		lrw := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		
+		next.ServeHTTP(lrw, r)
+		
+		duration := time.Since(start)
+		activity := ActivityLog{
+			Timestamp:  start,
+			Method:     r.Method,
+			Path:       r.URL.Path,
+			RemoteAddr: r.RemoteAddr,
+			UserAgent:  r.UserAgent(),
+			StatusCode: lrw.statusCode,
+			Duration:   duration,
+		}
+		
+		log.Printf("ACTIVITY: %s %s %d %s %s",
+			activity.Method,
+			activity.Path,
+			activity.StatusCode,
+			activity.Duration,
+			activity.RemoteAddr,
+		)
+	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
