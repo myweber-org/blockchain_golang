18a06d4f751a25af90bf1c76f141aa7a4eb019e8
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-17 15:49:52 +0000

    Add user activity logging middleware with request details and timestamp

---
+++ vendor/github.com/ipfs/go-log/tracer/user_activity_logger.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(rw, r)
+
+		duration := time.Since(start)
+		log.Printf(
+			"[%s] %s %s %d %v",
+			time.Now().Format("2006-01-02 15:04:05"),
+			r.Method,
+			r.URL.Path,
+			rw.statusCode,
+			duration,
+		)
+	})
+}
