2fd4c307c60838dd45c7e7ff236b3dfc4f4776af
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-19 20:54:04 +0000

    Add user activity logging with timestamp and action type

--- vendor/github.com/huin/user_activity_logger.go
+++ vendor/github.com/huin/user_activity_logger.go
@@ -27,4 +27,73 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type Activity struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Details   string    `json:"details"`
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, action, details string) error {
+	activity := Activity{
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		Action:    action,
+		Details:   details,
+	}
+
+	data, err := json.Marshal(activity)
+	if err != nil {
+		return err
+	}
+
+	data = append(data, '\n')
+	_, err = al.logFile.Write(data)
+	return err
+}
+
+func (al *ActivityLogger) Close() error {
+	return al.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("user_activities.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	err = logger.LogActivity("user123", "login", "Successful authentication")
+	if err != nil {
+		log.Printf("Failed to log activity: %v", err)
+	}
+
+	err = logger.LogActivity("user123", "file_upload", "Uploaded document.pdf")
+	if err != nil {
+		log.Printf("Failed to log activity: %v", err)
+	}
+
+	fmt.Println("Activities logged successfully")
 }
