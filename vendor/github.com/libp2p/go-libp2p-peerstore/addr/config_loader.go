f6ceb53c68a4563b343e91b45cabfd8b480608d5
InkspillVerse <inkspill.verse@outlook.com>
2026-01-19 22:18:22 +0000

    Add configuration loader with YAML support and environment variable overrides

--- vendor/github.com/libp2p/go-libp2p-peerstore/addr/config_loader.go
+++ vendor/github.com/libp2p/go-libp2p-peerstore/addr/config_loader.go
@@ -57,4 +57,70 @@ func ValidateConfig(config *ServerConfig) error {
         return fmt.Errorf("server port must be between 1 and 65535")
     }
     return nil
+}package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		URL      string `yaml:"url" env:"DB_URL"`
+		PoolSize int    `yaml:"pool_size" env:"DB_POOL_SIZE"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(&cfg)
+	return &cfg, nil
+}
+
+func overrideFromEnv(cfg *Config) {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		cfg.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		port := 0
+		fmt.Sscanf(val, "%d", &port)
+		if port > 0 {
+			cfg.Server.Port = port
+		}
+	}
+	if val := os.Getenv("DB_URL"); val != "" {
+		cfg.Database.URL = val
+	}
+	if val := os.Getenv("DB_POOL_SIZE"); val != "" {
+		size := 0
+		fmt.Sscanf(val, "%d", &size)
+		if size > 0 {
+			cfg.Database.PoolSize = size
+		}
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		cfg.LogLevel = val
+	}
+}
+
+func DefaultConfigPath() string {
+	exe, _ := os.Executable()
+	dir := filepath.Dir(exe)
+	return filepath.Join(dir, "config.yaml")
 }
