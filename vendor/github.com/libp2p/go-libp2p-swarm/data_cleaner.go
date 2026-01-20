8e09670da5398448673b306d5df63d3224c4785a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-20 23:50:48 +0000

    Add utility function to remove duplicate entries from a slice of integers

--- vendor/github.com/libp2p/go-libp2p-swarm/data_cleaner.go
+++ vendor/github.com/libp2p/go-libp2p-swarm/data_cleaner.go
@@ -22,3 +22,18 @@ func main() {
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
 }
+package utils
+
+func RemoveDuplicates(nums []int) []int {
+    seen := make(map[int]bool)
+    result := []int{}
+    
+    for _, num := range nums {
+        if !seen[num] {
+            seen[num] = true
+            result = append(result, num)
+        }
+    }
+    
+    return result
+}
