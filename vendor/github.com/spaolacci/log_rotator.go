0edcb26a995facd29ab3244328b88bdceadbdf5b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-22 19:12:55 +0000

    Add log rotation with size-based file splitting and cleanup of old logs

---
+++ vendor/github.com/spaolacci/log_rotator.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	maxLogSize    = 10 * 1024 * 1024 // 10MB
+	maxBackupLogs = 5
+	logFileName   = "app.log"
+)
+
+type RotatingLogger struct {
+	currentFile *os.File
+	currentSize int64
+	basePath    string
+}
+
+func NewRotatingLogger(path string) (*RotatingLogger, error) {
+	rl := &RotatingLogger{basePath: path}
+	if err := rl.openCurrentLog(); err != nil {
+		return nil, err
+	}
+	return rl, nil
+}
+
+func (rl *RotatingLogger) openCurrentLog() error {
+	fullPath := filepath.Join(rl.basePath, logFileName)
+	file, err := os.OpenFile(fullPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	rl.currentFile = file
+	rl.currentSize = info.Size()
+	return nil
+}
+
+func (rl *RotatingLogger) Write(p []byte) (n int, err error) {
+	if rl.currentSize+int64(len(p)) > maxLogSize {
+		if err := rl.rotate(); err != nil {
+			log.Printf("Failed to rotate log: %v", err)
+		}
+	}
+
+	n, err = rl.currentFile.Write(p)
+	if err == nil {
+		rl.currentSize += int64(n)
+	}
+	return n, err
+}
+
+func (rl *RotatingLogger) rotate() error {
+	if err := rl.currentFile.Close(); err != nil {
+		return err
+	}
+
+	timestamp := time.Now().Format("20060102_150405")
+	backupName := fmt.Sprintf("%s.%s", logFileName, timestamp)
+	oldPath := filepath.Join(rl.basePath, logFileName)
+	newPath := filepath.Join(rl.basePath, backupName)
+
+	if err := os.Rename(oldPath, newPath); err != nil {
+		return err
+	}
+
+	if err := rl.openCurrentLog(); err != nil {
+		return err
+	}
+
+	rl.cleanupOldLogs()
+	return nil
+}
+
+func (rl *RotatingLogger) cleanupOldLogs() {
+	pattern := filepath.Join(rl.basePath, logFileName+".*")
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return
+	}
+
+	if len(matches) <= maxBackupLogs {
+		return
+	}
+
+	for i := 0; i < len(matches)-maxBackupLogs; i++ {
+		os.Remove(matches[i])
+	}
+}
+
+func (rl *RotatingLogger) Close() error {
+	if rl.currentFile != nil {
+		return rl.currentFile.Close()
+	}
+	return nil
+}
+
+func main() {
+	logger, err := NewRotatingLogger(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	log.SetOutput(io.MultiWriter(os.Stdout, logger))
+
+	for i := 0; i < 100; i++ {
+		log.Printf("Log entry %d: %s", i, time.Now().Format(time.RFC3339))
+		time.Sleep(100 * time.Millisecond)
+	}
+}
