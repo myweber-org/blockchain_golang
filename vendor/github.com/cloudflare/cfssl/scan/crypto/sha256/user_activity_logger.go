8e09f44c58ccbd299913d26a9dce3a2fe5a10f76
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-22 23:37:03 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/cloudflare/cfssl/scan/crypto/sha256/user_activity_logger.go
+++ vendor/github.com/cloudflare/cfssl/scan/crypto/sha256/user_activity_logger.go
@@ -7,56 +7,43 @@ import (
 )
 
 type ActivityLogger struct {
-	handler http.Handler
+	Logger *log.Logger
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	userAgent := r.UserAgent()
-	ipAddress := r.RemoteAddr
-	requestPath := r.URL.Path
-
-	al.handler.ServeHTTP(w, r)
-
-	duration := time.Since(start)
-	log.Printf("Activity: %s | %s | %s | %v", ipAddress, userAgent, requestPath, duration)
-}package middleware
-
-import (
-    "log"
-    "net/http"
-    "time"
-)
-
-type responseWriter struct {
-    http.ResponseWriter
-    statusCode int
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		al.Logger.Printf(
+			"%s %s %d %s %s",
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration,
+			r.RemoteAddr,
+		)
+	})
 }
 
-func (rw *responseWriter) WriteHeader(code int) {
-    rw.statusCode = code
-    rw.ResponseWriter.WriteHeader(code)
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func ActivityLogger(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        start := time.Now()
-        rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-
-        next.ServeHTTP(rw, r)
-
-        duration := time.Since(start)
-        log.Printf(
-            "%s %s %d %s %s",
-            r.Method,
-            r.URL.Path,
-            rw.statusCode,
-            duration,
-            r.RemoteAddr,
-        )
-    })
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
