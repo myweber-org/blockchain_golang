d3c110c4ecb638bc0d54be7a5b73d414277fe160
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-22 14:32:33 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/subosito/user_activity_logger.go
+++ vendor/github.com/subosito/user_activity_logger.go
@@ -7,44 +7,22 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
+	handler http.Handler
 }
 
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userAgent := r.UserAgent()
+	ipAddress := r.RemoteAddr
+	requestPath := r.URL.Path
 
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
+	al.handler.ServeHTTP(w, r)
 
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+	duration := time.Since(start)
+	log.Printf("UserActivity - IP: %s | Path: %s | Agent: %s | Duration: %v",
+		ipAddress, requestPath, userAgent, duration)
 }
