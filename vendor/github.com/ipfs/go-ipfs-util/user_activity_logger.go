ce72cc3e04782e5409c8f05f9b544c013b9ed33b
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-22 19:57:59 +0000

    Add user activity logging with timestamps and event types

---
+++ vendor/github.com/ipfs/go-ipfs-util/user_activity_logger.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityEvent struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	EventType string    `json:"event_type"`
+	Details   string    `json:"details"`
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, eventType, details string) error {
+	event := ActivityEvent{
+		Timestamp: time.Now(),
+		UserID:    userID,
+		EventType: eventType,
+		Details:   details,
+	}
+
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	eventJSON = append(eventJSON, '\n')
+	_, err = al.logFile.Write(eventJSON)
+	return err
+}
+
+func (al *ActivityLogger) Close() error {
+	return al.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("user_activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	err = logger.LogActivity("user123", "LOGIN", "User logged in from IP 192.168.1.100")
+	if err != nil {
+		log.Printf("Failed to log activity: %v", err)
+	}
+
+	err = logger.LogActivity("user123", "VIEW_PAGE", "Accessed dashboard page")
+	if err != nil {
+		log.Printf("Failed to log activity: %v", err)
+	}
+
+	fmt.Println("Activity logging completed")
+}
