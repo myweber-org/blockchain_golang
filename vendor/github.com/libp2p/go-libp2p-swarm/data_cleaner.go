a8cf7e6640280289e225aadd5ac3ecfb92f6a02d
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-22 15:51:03 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- vendor/github.com/libp2p/go-libp2p-swarm/data_cleaner.go
+++ vendor/github.com/libp2p/go-libp2p-swarm/data_cleaner.go
@@ -3,37 +3,21 @@ package main
 
 import "fmt"
 
-func removeDuplicates(input []int) []int {
+func RemoveDuplicates(nums []int) []int {
 	seen := make(map[int]bool)
 	result := []int{}
-
-	for _, value := range input {
-		if !seen[value] {
-			seen[value] = true
-			result = append(result, value)
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
 		}
 	}
 	return result
 }
 
 func main() {
-	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
-	cleaned := removeDuplicates(data)
-	fmt.Println("Original:", data)
-	fmt.Println("Cleaned:", cleaned)
-}
-package utils
-
-func RemoveDuplicates(nums []int) []int {
-    seen := make(map[int]bool)
-    result := []int{}
-    
-    for _, num := range nums {
-        if !seen[num] {
-            seen[num] = true
-            result = append(result, num)
-        }
-    }
-    
-    return result
+	input := []int{1, 2, 2, 3, 4, 4, 5, 6, 6}
+	output := RemoveDuplicates(input)
+	fmt.Printf("Original: %v\n", input)
+	fmt.Printf("Cleaned: %v\n", output)
 }
