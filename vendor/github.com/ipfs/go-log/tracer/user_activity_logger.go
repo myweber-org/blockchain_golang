7d6cbead95eee86d3bc2a892d1518b1c001bdd1e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-23 23:55:21 +0000

    Add user activity logging middleware for tracking API requests

--- vendor/github.com/ipfs/go-log/tracer/user_activity_logger.go
+++ vendor/github.com/ipfs/go-log/tracer/user_activity_logger.go
@@ -6,31 +6,48 @@ import (
 	"time"
 )
 
-type responseWriter struct {
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		clientIP := r.RemoteAddr
+		method := r.Method
+		path := r.URL.Path
+
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(recorder, r)
+
+		duration := time.Since(start)
+		status := recorder.statusCode
+
+		al.Logger.Printf(
+			"IP: %s | Method: %s | Path: %s | Status: %d | Duration: %v",
+			clientIP,
+			method,
+			path,
+			status,
+			duration,
+		)
+	})
+}
+
+type responseRecorder struct {
 	http.ResponseWriter
 	statusCode int
 }
 
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-
-		next.ServeHTTP(rw, r)
-
-		duration := time.Since(start)
-		log.Printf(
-			"[%s] %s %s %d %v",
-			time.Now().Format("2006-01-02 15:04:05"),
-			r.Method,
-			r.URL.Path,
-			rw.statusCode,
-			duration,
-		)
-	})
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
