b92eb969fb57d82fb53c00d7e1e77405cd803a15
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-26 01:38:12 +0000

    Add user activity logging with timestamps and action types

--- vendor/github.com/libp2p/go-msgio/user_activity_logger.go
+++ vendor/github.com/libp2p/go-msgio/user_activity_logger.go
@@ -25,4 +25,85 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityType string
+
+const (
+    Login    ActivityType = "LOGIN"
+    Logout   ActivityType = "LOGOUT"
+    Purchase ActivityType = "PURCHASE"
+    View     ActivityType = "VIEW"
+)
+
+type UserActivity struct {
+    UserID    string       `json:"user_id"`
+    Action    ActivityType `json:"action"`
+    Timestamp time.Time    `json:"timestamp"`
+    Details   string       `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+    encoder *json.Encoder
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{
+        logFile: file,
+        encoder: json.NewEncoder(file),
+    }, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID string, action ActivityType, details string) error {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+    return l.encoder.Encode(activity)
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activities.jsonl")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    activities := []struct {
+        userID string
+        action ActivityType
+        details string
+    }{
+        {"user_123", Login, "Successful authentication"},
+        {"user_456", View, "Product page: laptop"},
+        {"user_123", Purchase, "Order ID: ORD-789"},
+        {"user_789", Logout, "Session terminated"},
+    }
+
+    for _, act := range activities {
+        if err := logger.LogActivity(act.userID, act.action, act.details); err != nil {
+            log.Printf("Failed to log activity: %v", err)
+        }
+    }
+
+    fmt.Println("Activity logging completed")
 }
