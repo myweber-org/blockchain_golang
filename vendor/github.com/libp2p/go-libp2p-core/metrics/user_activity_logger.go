b4465c99e9dc0badcb1c7e3042a0c757b19c41e2
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-26 23:52:44 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/libp2p/go-libp2p-core/metrics/user_activity_logger.go
+++ vendor/github.com/libp2p/go-libp2p-core/metrics/user_activity_logger.go
@@ -6,6 +6,38 @@ import (
 	"time"
 )
 
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		al.Logger.Printf(
+			"%s %s %d %s %s",
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration,
+			r.RemoteAddr,
+		)
+	})
+}
+
 type responseRecorder struct {
 	http.ResponseWriter
 	statusCode int
@@ -14,23 +46,4 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		recorder := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
-
-		next.ServeHTTP(recorder, r)
-
-		duration := time.Since(start)
-		log.Printf(
-			"[%s] %s %s %d %v",
-			time.Now().Format(time.RFC3339),
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-		)
-	})
 }
