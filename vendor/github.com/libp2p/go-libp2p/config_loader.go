e6d24e519e0c1dd25432162bdbd05261ddc997a9
InkspillVerse <inkspill.verse@outlook.com>
2026-01-26 20:28:11 +0000

    Add configuration loader with environment variable support

--- vendor/github.com/libp2p/go-libp2p/config_loader.go
+++ vendor/github.com/libp2p/go-libp2p/config_loader.go
@@ -1,94 +1,43 @@
 package config
 
 import (
-	"errors"
-	"io/ioutil"
-	"os"
-
-	"gopkg.in/yaml.v2"
+    "os"
+    "strconv"
+    "strings"
 )
 
-type DatabaseConfig struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
-	Database string `yaml:"database"`
+type Config struct {
+    ServerPort int
+    DatabaseURL string
+    DebugMode bool
+    AllowedHosts []string
 }
 
-type ServerConfig struct {
-	Port         int    `yaml:"port"`
-	ReadTimeout  int    `yaml:"read_timeout"`
-	WriteTimeout int    `yaml:"write_timeout"`
-	DebugMode    bool   `yaml:"debug_mode"`
-	LogLevel     string `yaml:"log_level"`
+func Load() (*Config, error) {
+    cfg := &Config{}
+    
+    portStr := getEnv("SERVER_PORT", "8080")
+    port, err := strconv.Atoi(portStr)
+    if err != nil {
+        return nil, err
+    }
+    cfg.ServerPort = port
+    
+    cfg.DatabaseURL = getEnv("DATABASE_URL", "postgres://localhost:5432/app")
+    
+    debugStr := getEnv("DEBUG_MODE", "false")
+    cfg.DebugMode = strings.ToLower(debugStr) == "true"
+    
+    hostsStr := getEnv("ALLOWED_HOSTS", "localhost,127.0.0.1")
+    cfg.AllowedHosts = strings.Split(hostsStr, ",")
+    
+    return cfg, nil
 }
 
-type AppConfig struct {
-	Server   ServerConfig   `yaml:"server"`
-	Database DatabaseConfig `yaml:"database"`
-}
-
-func LoadConfig(configPath string) (*AppConfig, error) {
-	if configPath == "" {
-		return nil, errors.New("config path cannot be empty")
-	}
-
-	file, err := os.Open(configPath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	content, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-
-	var config AppConfig
-	err = yaml.Unmarshal(content, &config)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := validateConfig(&config); err != nil {
-		return nil, err
-	}
-
-	return &config, nil
-}
-
-func validateConfig(config *AppConfig) error {
-	if config.Server.Port <= 0 || config.Server.Port > 65535 {
-		return errors.New("server port must be between 1 and 65535")
-	}
-
-	if config.Server.ReadTimeout < 0 {
-		return errors.New("read timeout cannot be negative")
-	}
-
-	if config.Server.WriteTimeout < 0 {
-		return errors.New("write timeout cannot be negative")
-	}
-
-	if config.Database.Host == "" {
-		return errors.New("database host cannot be empty")
-	}
-
-	if config.Database.Port <= 0 || config.Database.Port > 65535 {
-		return errors.New("database port must be between 1 and 65535")
-	}
-
-	validLogLevels := map[string]bool{
-		"debug": true,
-		"info":  true,
-		"warn":  true,
-		"error": true,
-	}
-
-	if !validLogLevels[config.Server.LogLevel] {
-		return errors.New("invalid log level specified")
-	}
-
-	return nil
+func getEnv(key, defaultValue string) string {
+    value := os.Getenv(key)
+    if value == "" {
+        return defaultValue
+    }
+    return value
 }
