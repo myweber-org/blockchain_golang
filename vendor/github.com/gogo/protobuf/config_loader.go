75e56f1d42a924b8a88f730bda6b9735df1be74a
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-27 14:31:51 +0000

    Add configuration file loader with YAML parsing support

---
+++ vendor/github.com/gogo/protobuf/config_loader.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+    LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(filePath string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    if config.Server.Host == "" {
+        config.Server.Host = "localhost"
+    }
+    if config.Server.Port == 0 {
+        config.Server.Port = 8080
+    }
+    if config.LogLevel == "" {
+        config.LogLevel = "info"
+    }
+
+    return &config, nil
+}
+
+func (c *AppConfig) Validate() error {
+    if c.Database.Host == "" {
+        return fmt.Errorf("database host is required")
+    }
+    if c.Database.Username == "" {
+        return fmt.Errorf("database username is required")
+    }
+    return nil
+}
