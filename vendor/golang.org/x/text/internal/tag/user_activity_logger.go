9dd119abebdfa000cc928a4fb52f63342cab3645
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-27 20:09:59 +0000

    Add user activity logging middleware for API endpoints

--- vendor/golang.org/x/text/internal/tag/user_activity_logger.go
+++ vendor/golang.org/x/text/internal/tag/user_activity_logger.go
@@ -19,62 +19,11 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
 
-	log.Printf("Activity: %s %s from %s took %v",
+	log.Printf(
+		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
 		duration,
+		time.Now().Format(time.RFC3339),
 	)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		userAgent := r.UserAgent()
-		clientIP := r.RemoteAddr
-		method := r.Method
-		path := r.URL.Path
-
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-
-		next.ServeHTTP(recorder, r)
-
-		duration := time.Since(start)
-		al.Logger.Printf(
-			"Activity: %s %s | Status: %d | Duration: %v | IP: %s | Agent: %s",
-			method,
-			path,
-			recorder.statusCode,
-			duration,
-			clientIP,
-			userAgent,
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
 }
