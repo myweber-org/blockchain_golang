13431a46ff67faa10d2143cb3dc18541eed7d850
InkspillVerse <inkspill.verse@outlook.com>
2026-01-27 20:51:55 +0000

    Add configuration loader with YAML support and environment variable overrides

--- vendor/github.com/libp2p/go-libp2p-core/network/config_loader.go
+++ vendor/github.com/libp2p/go-libp2p-core/network/config_loader.go
@@ -54,4 +54,87 @@ func overrideFromEnv(config *AppConfig) {
     // and override values from environment variables
     // using reflection based on 'env' struct tags
     // Implementation omitted for brevity
+}package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Output string `yaml:"output" env:"LOG_OUTPUT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	if configPath == "" {
+		return nil, errors.New("config path cannot be empty")
+	}
+
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	cfg.overrideFromEnv()
+
+	return &cfg, nil
+}
+
+func (c *Config) overrideFromEnv() {
+	c.Server.Host = getEnvOrDefault("SERVER_HOST", c.Server.Host)
+	c.Server.Port = getEnvIntOrDefault("SERVER_PORT", c.Server.Port)
+
+	c.Database.Host = getEnvOrDefault("DB_HOST", c.Database.Host)
+	c.Database.Port = getEnvIntOrDefault("DB_PORT", c.Database.Port)
+	c.Database.Name = getEnvOrDefault("DB_NAME", c.Database.Name)
+	c.Database.User = getEnvOrDefault("DB_USER", c.Database.User)
+	c.Database.Password = getEnvOrDefault("DB_PASSWORD", c.Database.Password)
+
+	c.Logging.Level = getEnvOrDefault("LOG_LEVEL", c.Logging.Level)
+	c.Logging.Output = getEnvOrDefault("LOG_OUTPUT", c.Logging.Output)
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvIntOrDefault(key string, defaultValue int) int {
+	if value := os.Getenv(key); value != "" {
+		var result int
+		if _, err := fmt.Sscanf(value, "%d", &result); err == nil {
+			return result
+		}
+	}
+	return defaultValue
 }
