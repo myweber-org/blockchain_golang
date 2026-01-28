ec73bac8478f444e77321617f7e7c8ebea8602ab
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-28 15:14:33 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/mr-tron/base58/base58/user_activity_logger.go
+++ vendor/github.com/mr-tron/base58/base58/user_activity_logger.go
@@ -1,56 +1,45 @@
 package middleware
 
 import (
-	"context"
 	"log"
 	"net/http"
 	"time"
 )
 
-type ActivityKey string
-
-const UserActivityKey ActivityKey = "user_activity"
-
-type UserActivity struct {
-	UserID    string
-	Action    string
-	Timestamp time.Time
-	IPAddress string
+type ActivityLogger struct {
+	handler http.Handler
 }
 
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		activity := UserActivity{
-			UserID:    extractUserID(r),
-			Action:    r.Method + " " + r.URL.Path,
-			Timestamp: time.Now().UTC(),
-			IPAddress: r.RemoteAddr,
-		}
-
-		ctx := context.WithValue(r.Context(), UserActivityKey, activity)
-		next.ServeHTTP(w, r.WithContext(ctx))
-
-		logActivity(activity)
-	})
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func extractUserID(r *http.Request) string {
-	if auth := r.Header.Get("Authorization"); auth != "" {
-		return "authenticated_user"
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
 	}
-	return "anonymous"
-}
 
-func logActivity(activity UserActivity) {
-	log.Printf("Activity: User=%s Action=%s IP=%s Time=%s",
-		activity.UserID,
-		activity.Action,
-		activity.IPAddress,
-		activity.Timestamp.Format(time.RFC3339),
+	al.handler.ServeHTTP(recorder, r)
+
+	duration := time.Since(start)
+	log.Printf(
+		"%s %s %d %s %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		duration,
+		r.RemoteAddr,
 	)
 }
 
-func GetActivityFromContext(ctx context.Context) (UserActivity, bool) {
-	activity, ok := ctx.Value(UserActivityKey).(UserActivity)
-	return activity, ok
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
