3c0e699cbaf9e8f5a2175c0da9f9b2570d6e1870
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-28 20:56:03 +0000

    Add file encryption utility with AES-256-GCM support

--- vendor/github.com/gorilla/file_encryptor.go
+++ vendor/github.com/gorilla/file_encryptor.go
@@ -268,4 +268,123 @@ func main() {
 		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
 		os.Exit(1)
 	}
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encryptFile(inputPath, outputPath string, key []byte) error {
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return fmt.Errorf("read file error: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+
+	if err := os.WriteFile(outputPath, ciphertext, 0644); err != nil {
+		return fmt.Errorf("write file error: %v", err)
+	}
+
+	return nil
+}
+
+func decryptFile(inputPath, outputPath string, key []byte) error {
+	ciphertext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return fmt.Errorf("read file error: %v", err)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(outputPath, plaintext, 0644); err != nil {
+		return fmt.Errorf("write file error: %v", err)
+	}
+
+	return nil
+}
+
+func main() {
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		fmt.Printf("Key generation error: %v\n", err)
+		return
+	}
+
+	testData := []byte("Sensitive information requiring encryption")
+	testFile := "test_data.txt"
+	encryptedFile := "encrypted.dat"
+	decryptedFile := "decrypted.txt"
+
+	if err := os.WriteFile(testFile, testData, 0644); err != nil {
+		fmt.Printf("Write test file error: %v\n", err)
+		return
+	}
+	defer os.Remove(testFile)
+	defer os.Remove(encryptedFile)
+	defer os.Remove(decryptedFile)
+
+	fmt.Println("Testing file encryption...")
+	if err := encryptFile(testFile, encryptedFile, key); err != nil {
+		fmt.Printf("Encryption error: %v\n", err)
+		return
+	}
+
+	fmt.Println("Testing file decryption...")
+	if err := decryptFile(encryptedFile, decryptedFile, key); err != nil {
+		fmt.Printf("Decryption error: %v\n", err)
+		return
+	}
+
+	decryptedData, err := os.ReadFile(decryptedFile)
+	if err != nil {
+		fmt.Printf("Read decrypted file error: %v\n", err)
+		return
+	}
+
+	if string(decryptedData) == string(testData) {
+		fmt.Println("Encryption/decryption successful")
+	} else {
+		fmt.Println("Encryption/decryption failed")
+	}
 }
