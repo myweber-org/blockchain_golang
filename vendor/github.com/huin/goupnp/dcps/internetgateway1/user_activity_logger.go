be500840abd162f73255c7380cb01f7d374178dd
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-29 01:05:22 +0000

    Add user activity logging with timestamps and action types

--- vendor/github.com/huin/goupnp/dcps/internetgateway1/user_activity_logger.go
+++ vendor/github.com/huin/goupnp/dcps/internetgateway1/user_activity_logger.go
@@ -77,4 +77,92 @@ func ActivityLogger(next http.Handler) http.Handler {
 			r.RemoteAddr,
 		)
 	})
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityType string
+
+const (
+	Login    ActivityType = "LOGIN"
+	Logout   ActivityType = "LOGOUT"
+	Purchase ActivityType = "PURCHASE"
+	View     ActivityType = "VIEW"
+	Search   ActivityType = "SEARCH"
+)
+
+type UserActivity struct {
+	UserID    string       `json:"user_id"`
+	Action    ActivityType `json:"action"`
+	Timestamp time.Time    `json:"timestamp"`
+	Details   string       `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID string, action ActivityType, details string) error {
+	activity := UserActivity{
+		UserID:    userID,
+		Action:    action,
+		Timestamp: time.Now().UTC(),
+		Details:   details,
+	}
+
+	data, err := json.Marshal(activity)
+	if err != nil {
+		return err
+	}
+
+	data = append(data, '\n')
+	_, err = l.logFile.Write(data)
+	return err
+}
+
+func (l *ActivityLogger) Close() error {
+	return l.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("user_activities.jsonl")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	activities := []struct {
+		userID string
+		action ActivityType
+		details string
+	}{
+		{"user_123", Login, "Successful login from Chrome browser"},
+		{"user_456", View, "Viewed product page: laptop-x1"},
+		{"user_123", Search, "Searched for: wireless headphones"},
+		{"user_789", Purchase, "Order ID: ORD-789456"},
+		{"user_123", Logout, "Session duration: 45m"},
+	}
+
+	for _, act := range activities {
+		err := logger.LogActivity(act.userID, act.action, act.details)
+		if err != nil {
+			log.Printf("Failed to log activity: %v", err)
+		}
+	}
+
+	fmt.Println("Activity logging completed")
 }
