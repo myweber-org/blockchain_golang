8fb3d91eba5e8fed34a0312dce1e12132e8ef06f
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-29 01:00:11 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/jbenet/goprocess/context/user_activity_logger.go
+++ vendor/github.com/jbenet/goprocess/context/user_activity_logger.go
@@ -1,75 +1,24 @@
-package main
+package middleware
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"time"
 )
 
-type ActivityType string
-
-const (
-	Login    ActivityType = "LOGIN"
-	Logout   ActivityType = "LOGOUT"
-	Purchase ActivityType = "PURCHASE"
-	View     ActivityType = "VIEW"
-)
-
-type UserActivity struct {
-	UserID    string       `json:"user_id"`
-	Action    ActivityType `json:"action"`
-	Timestamp time.Time    `json:"timestamp"`
-	Details   string       `json:"details,omitempty"`
-}
-
 type ActivityLogger struct {
-	logFile *os.File
+	handler http.Handler
 }
 
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, err
-	}
-	return &ActivityLogger{logFile: file}, nil
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (l *ActivityLogger) LogActivity(activity UserActivity) error {
-	activity.Timestamp = time.Now().UTC()
-	data, err := json.Marshal(activity)
-	if err != nil {
-		return err
-	}
-	data = append(data, '\n')
-	_, err = l.logFile.Write(data)
-	return err
-}
-
-func (l *ActivityLogger) Close() error {
-	return l.logFile.Close()
-}
-
-func main() {
-	logger, err := NewActivityLogger("user_activities.jsonl")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logger.Close()
-
-	activities := []UserActivity{
-		{UserID: "user123", Action: Login, Details: "Successful login from Chrome"},
-		{UserID: "user123", Action: View, Details: "Viewed product page: laptop"},
-		{UserID: "user123", Action: Purchase, Details: "Purchased item: laptop"},
-		{UserID: "user456", Action: Login, Details: "Mobile app login"},
-	}
-
-	for _, activity := range activities {
-		if err := logger.LogActivity(activity); err != nil {
-			log.Printf("Failed to log activity: %v", err)
-		}
-	}
-
-	fmt.Println("Activities logged successfully")
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(start)
+	log.Printf("Activity: %s %s from %s took %v", r.Method, r.URL.Path, r.RemoteAddr, duration)
 }
