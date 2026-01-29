6dde3496f5a7624c1ae55f94c13098a09d31cf17
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-29 00:16:11 +0000

    Add configuration loader with YAML support and environment variable overrides

--- vendor/github.com/libp2p/go-libp2p-core/transport/config_loader.go
+++ vendor/github.com/libp2p/go-libp2p-core/transport/config_loader.go
@@ -48,4 +48,82 @@ func validateConfig(c *Config) error {
         return fmt.Errorf("database driver cannot be empty")
     }
     return nil
+}package config
+
+import (
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	config := &Config{}
+
+	file, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(file, config)
+	if err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(config)
+
+	return config, nil
+}
+
+func overrideFromEnv(config *Config) {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		config.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		config.Server.Port = atoi(val)
+	}
+	if val := os.Getenv("DB_HOST"); val != "" {
+		config.Database.Host = val
+	}
+	if val := os.Getenv("DB_PORT"); val != "" {
+		config.Database.Port = atoi(val)
+	}
+	if val := os.Getenv("DB_NAME"); val != "" {
+		config.Database.Name = val
+	}
+	if val := os.Getenv("DB_USER"); val != "" {
+		config.Database.User = val
+	}
+	if val := os.Getenv("DB_PASSWORD"); val != "" {
+		config.Database.Password = val
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		config.LogLevel = val
+	}
+}
+
+func atoi(s string) int {
+	var result int
+	for _, ch := range s {
+		if ch < '0' || ch > '9' {
+			return 0
+		}
+		result = result*10 + int(ch-'0')
+	}
+	return result
 }
