aea9658e8eff55d5c98ff10e50de4d08f6e6bb57
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-30 01:36:48 +0000

    Add log rotation functionality with size-based file splitting

--- vendor/golang.org/x/crypto/blake2s/log_rotator.go
+++ vendor/golang.org/x/crypto/blake2s/log_rotator.go
@@ -139,3 +139,117 @@ func main() {
 
     fmt.Println("Log rotation test completed")
 }
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+type LogRotator struct {
+	filePath    string
+	maxSize     int64
+	currentSize int64
+	file        *os.File
+	mu          sync.Mutex
+}
+
+func NewLogRotator(path string, maxSizeMB int) (*LogRotator, error) {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+
+	rotator := &LogRotator{
+		filePath: absPath,
+		maxSize:  int64(maxSizeMB) * 1024 * 1024,
+	}
+
+	err = rotator.openFile()
+	return rotator, err
+}
+
+func (lr *LogRotator) openFile() error {
+	file, err := os.OpenFile(lr.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	lr.file = file
+	lr.currentSize = info.Size()
+	return nil
+}
+
+func (lr *LogRotator) rotateIfNeeded() error {
+	if lr.currentSize < lr.maxSize {
+		return nil
+	}
+
+	lr.file.Close()
+
+	timestamp := time.Now().Format("20060102_150405")
+	backupPath := fmt.Sprintf("%s.%s", lr.filePath, timestamp)
+	err := os.Rename(lr.filePath, backupPath)
+	if err != nil {
+		return err
+	}
+
+	return lr.openFile()
+}
+
+func (lr *LogRotator) Write(p []byte) (int, error) {
+	lr.mu.Lock()
+	defer lr.mu.Unlock()
+
+	err := lr.rotateIfNeeded()
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := lr.file.Write(p)
+	if err == nil {
+		lr.currentSize += int64(n)
+	}
+	return n, err
+}
+
+func (lr *LogRotator) Close() error {
+	lr.mu.Lock()
+	defer lr.mu.Unlock()
+
+	if lr.file != nil {
+		return lr.file.Close()
+	}
+	return nil
+}
+
+func main() {
+	rotator, err := NewLogRotator("app.log", 10)
+	if err != nil {
+		fmt.Printf("Failed to create log rotator: %v\n", err)
+		return
+	}
+	defer rotator.Close()
+
+	for i := 0; i < 1000; i++ {
+		logEntry := fmt.Sprintf("[%s] Log entry %d: Some sample log data here\n",
+			time.Now().Format(time.RFC3339), i)
+		_, err := rotator.Write([]byte(logEntry))
+		if err != nil {
+			fmt.Printf("Failed to write log: %v\n", err)
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	fmt.Println("Log rotation test completed")
+}
