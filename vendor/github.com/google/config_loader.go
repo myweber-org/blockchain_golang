25ddec2d37e8f5ed94f8f84cce6f8d124918f977
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-30 21:27:15 +0000

    Add configuration file loader with YAML parsing support

--- vendor/github.com/google/config_loader.go
+++ vendor/github.com/google/config_loader.go
@@ -1,95 +1,53 @@
 package config
 
 import (
-    "fmt"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
-    Server struct {
-        Host string `yaml:"host"`
-        Port int    `yaml:"port"`
-    } `yaml:"server"`
-    Database struct {
-        Username string `yaml:"username"`
-        Password string `yaml:"password"`
-        Name     string `yaml:"name"`
-    } `yaml:"database"`
+type AppConfig struct {
+	Server struct {
+		Port    int    `yaml:"port"`
+		Host    string `yaml:"host"`
+		Timeout int    `yaml:"timeout"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
 }
 
-func LoadConfig(path string) (*Config, error) {
-    data, err := ioutil.ReadFile(path)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read config file: %w", err)
-    }
+func LoadConfig(filename string) (*AppConfig, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
 
-    var config Config
-    if err := yaml.Unmarshal(data, &config); err != nil {
-        return nil, fmt.Errorf("failed to parse YAML: %w", err)
-    }
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
 
-    if config.Server.Host == "" {
-        config.Server.Host = "localhost"
-    }
-    if config.Server.Port == 0 {
-        config.Server.Port = 8080
-    }
-
-    if config.Database.Name == "" {
-        return nil, fmt.Errorf("database name is required")
-    }
-
-    return &config, nil
-}package config
-
-import (
-    "fmt"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
-)
-
-type Config struct {
-    Server struct {
-        Host string `yaml:"host"`
-        Port int    `yaml:"port"`
-    } `yaml:"server"`
-    Database struct {
-        URL      string `yaml:"url"`
-        MaxConns int    `yaml:"max_connections"`
-    } `yaml:"database"`
+	log.Printf("Configuration loaded from %s", filename)
+	return &config, nil
 }
 
-func LoadConfig(path string) (*Config, error) {
-    data, err := ioutil.ReadFile(path)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read config file: %w", err)
-    }
-
-    var cfg Config
-    if err := yaml.Unmarshal(data, &cfg); err != nil {
-        return nil, fmt.Errorf("failed to parse YAML: %w", err)
-    }
-
-    if err := validateConfig(&cfg); err != nil {
-        return nil, fmt.Errorf("config validation failed: %w", err)
-    }
-
-    return &cfg, nil
-}
-
-func validateConfig(cfg *Config) error {
-    if cfg.Server.Host == "" {
-        return fmt.Errorf("server host cannot be empty")
-    }
-    if cfg.Server.Port <= 0 || cfg.Server.Port > 65535 {
-        return fmt.Errorf("invalid server port: %d", cfg.Server.Port)
-    }
-    if cfg.Database.URL == "" {
-        return fmt.Errorf("database URL cannot be empty")
-    }
-    if cfg.Database.MaxConns < 1 {
-        return fmt.Errorf("max connections must be positive")
-    }
-    return nil
+func (c *AppConfig) Validate() error {
+	if c.Server.Port <= 0 || c.Server.Port > 65535 {
+		return fmt.Errorf("invalid server port: %d", c.Server.Port)
+	}
+	if c.Database.Host == "" {
+		return fmt.Errorf("database host cannot be empty")
+	}
+	return nil
 }
