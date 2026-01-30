285b3ce823582aacd4946c914bb25d808bbde887
InkspillVerse <inkspill.verse@outlook.com>
2026-01-30 17:01:22 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/hashicorp/hcl/json/parser/user_activity_logger.go
+++ vendor/github.com/hashicorp/hcl/json/parser/user_activity_logger.go
@@ -1,78 +1,28 @@
-package main
+package middleware
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"time"
 )
 
-type ActivityLog struct {
-	Timestamp time.Time `json:"timestamp"`
-	UserID    string    `json:"user_id"`
-	Action    string    `json:"action"`
-	Resource  string    `json:"resource"`
-}
-
 type ActivityLogger struct {
-	logFile *os.File
+	handler http.Handler
 }
 
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, err
-	}
-	return &ActivityLogger{logFile: file}, nil
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLogger) LogActivity(userID, action, resource string) error {
-	activity := ActivityLog{
-		Timestamp: time.Now().UTC(),
-		UserID:    userID,
-		Action:    action,
-		Resource:  resource,
-	}
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-	data, err := json.Marshal(activity)
-	if err != nil {
-		return err
-	}
-
-	data = append(data, '\n')
-	_, err = al.logFile.Write(data)
-	return err
-}
-
-func (al *ActivityLogger) Close() error {
-	return al.logFile.Close()
-}
-
-func main() {
-	logger, err := NewActivityLogger("activity.log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logger.Close()
-
-	activities := []struct {
-		userID   string
-		action   string
-		resource string
-	}{
-		{"user_001", "LOGIN", "auth_system"},
-		{"user_002", "CREATE", "document_123"},
-		{"user_001", "UPDATE", "profile_settings"},
-		{"user_003", "DELETE", "comment_456"},
-	}
-
-	for _, act := range activities {
-		err := logger.LogActivity(act.userID, act.action, act.resource)
-		if err != nil {
-			log.Printf("Failed to log activity: %v", err)
-		} else {
-			fmt.Printf("Logged: %s performed %s on %s\n", act.userID, act.action, act.resource)
-		}
-	}
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
