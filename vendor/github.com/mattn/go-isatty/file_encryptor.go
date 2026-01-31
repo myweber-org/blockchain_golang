d8ea157fa4bdfa33a6d502fea8b8d5418628eb38
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-31 18:37:08 +0000

    Add file encryption utility with AES-256-GCM

--- vendor/github.com/mattn/go-isatty/file_encryptor.go
+++ vendor/github.com/mattn/go-isatty/file_encryptor.go
@@ -103,4 +103,107 @@ func main() {
 	}
 
 	fmt.Printf("Operation completed successfully\n")
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encryptFile(inputPath, outputPath string, key []byte) error {
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return os.WriteFile(outputPath, ciphertext, 0644)
+}
+
+func decryptFile(inputPath, outputPath string, key []byte) error {
+	ciphertext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return fmt.Errorf("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run file_encryptor.go <encrypt|decrypt> <input> <output>")
+		fmt.Println("Key must be 32 bytes for AES-256, provided as hex string")
+		return
+	}
+
+	action := os.Args[1]
+	inputFile := os.Args[2]
+	outputFile := os.Args[3]
+
+	fmt.Print("Enter encryption key (64 hex characters): ")
+	var keyHex string
+	fmt.Scanln(&keyHex)
+
+	key, err := hex.DecodeString(keyHex)
+	if err != nil || len(key) != 32 {
+		fmt.Println("Invalid key: must be 64 hex characters (32 bytes)")
+		return
+	}
+
+	switch action {
+	case "encrypt":
+		err = encryptFile(inputFile, outputFile, key)
+	case "decrypt":
+		err = decryptFile(inputFile, outputFile, key)
+	default:
+		fmt.Println("Action must be 'encrypt' or 'decrypt'")
+		return
+	}
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	} else {
+		fmt.Printf("Operation completed successfully\n")
+	}
 }
