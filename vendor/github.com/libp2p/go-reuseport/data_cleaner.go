9e53ce4d49bed354d50f8c6ce3051eaad73ce019
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-31 20:59:07 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

---
+++ vendor/github.com/libp2p/go-reuseport/data_cleaner.go
@@ -0,0 +1,24 @@
+
+package main
+
+import "fmt"
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{5, 2, 8, 2, 5, 9, 8, 1}
+	cleaned := RemoveDuplicates(data)
+	fmt.Printf("Original: %v\n", data)
+	fmt.Printf("Cleaned: %v\n", cleaned)
+}
