0425262229c9bee2b1c8576222671642bcb24535
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-31 20:27:09 +0000

    Add configuration loader with environment variable support and validation

--- cli/config_loader.go
+++ cli/config_loader.go
@@ -1,46 +1,98 @@
+
 package config
 
 import (
-    "fmt"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 type Config struct {
-    Server struct {
-        Host string `yaml:"host"`
-        Port int    `yaml:"port"`
-    } `yaml:"server"`
-    Database struct {
-        Username string `yaml:"username"`
-        Password string `yaml:"password"`
-        Name     string `yaml:"name"`
-    } `yaml:"database"`
+	ServerPort int    `env:"SERVER_PORT" default:"8080"`
+	DBHost     string `env:"DB_HOST" default:"localhost"`
+	DBPort     int    `env:"DB_PORT" default:"5432"`
+	DBName     string `env:"DB_NAME" default:"appdb"`
+	DebugMode  bool   `env:"DEBUG_MODE" default:"false"`
+	APIKey     string `env:"API_KEY" required:"true"`
 }
 
-func LoadConfig(filename string) (*Config, error) {
-    data, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read config file: %v", err)
-    }
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+	
+	v := reflect.ValueOf(cfg).Elem()
+	t := v.Type()
 
-    var config Config
-    if err := yaml.Unmarshal(data, &config); err != nil {
-        return nil, fmt.Errorf("failed to parse YAML: %v", err)
-    }
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		structField := t.Field(i)
+		
+		envTag := structField.Tag.Get("env")
+		if envTag == "" {
+			continue
+		}
+		
+		envValue := os.Getenv(envTag)
+		defaultValue := structField.Tag.Get("default")
+		required := structField.Tag.Get("required") == "true"
+		
+		if envValue == "" {
+			if required {
+				return nil, fmt.Errorf("required environment variable %s is not set", envTag)
+			}
+			envValue = defaultValue
+		}
+		
+		if err := setFieldValue(field, envValue); err != nil {
+			return nil, fmt.Errorf("failed to set field %s: %w", structField.Name, err)
+		}
+	}
+	
+	return cfg, nil
+}
 
-    return &config, nil
+func setFieldValue(field reflect.Value, value string) error {
+	switch field.Kind() {
+	case reflect.String:
+		field.SetString(value)
+	case reflect.Int:
+		intVal, err := strconv.Atoi(value)
+		if err != nil {
+			return err
+		}
+		field.SetInt(int64(intVal))
+	case reflect.Bool:
+		boolVal, err := strconv.ParseBool(strings.ToLower(value))
+		if err != nil {
+			return err
+		}
+		field.SetBool(boolVal)
+	default:
+		return errors.New("unsupported field type")
+	}
+	return nil
 }
 
 func (c *Config) Validate() error {
-    if c.Server.Host == "" {
-        return fmt.Errorf("server host cannot be empty")
-    }
-    if c.Server.Port <= 0 || c.Server.Port > 65535 {
-        return fmt.Errorf("invalid server port: %d", c.Server.Port)
-    }
-    if c.Database.Name == "" {
-        return fmt.Errorf("database name cannot be empty")
-    }
-    return nil
+	if c.ServerPort < 1 || c.ServerPort > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if c.DBPort < 1 || c.DBPort > 65535 {
+		return errors.New("database port must be between 1 and 65535")
+	}
+	if strings.TrimSpace(c.DBHost) == "" {
+		return errors.New("database host cannot be empty")
+	}
+	return nil
+}
+
+func (c *Config) ToJSON() (string, error) {
+	bytes, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
 }
