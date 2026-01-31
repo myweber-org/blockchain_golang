48a389d94f6b6881608e6f97be6c2bcba62b7027
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-31 18:18:55 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- vendor/github.com/jackpal/data_cleaner.go
+++ vendor/github.com/jackpal/data_cleaner.go
@@ -65,3 +65,26 @@ func main() {
 
 	fmt.Printf("Data cleaned successfully. Output written to %s\n", outputFile)
 }
+package main
+
+import "fmt"
+
+func removeDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{4, 2, 8, 2, 4, 9, 8, 1}
+	cleaned := removeDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
