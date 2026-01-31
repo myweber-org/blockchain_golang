5ce52ea6ed12cf1eb5990ef5151d857367eb8a61
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-31 19:56:30 +0000

    Add AES-256 file encryption utility with secure key derivation

---
+++ vendor/github.com/mitchellh/file_encryption_utility.go
@@ -0,0 +1,158 @@
+
+package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "crypto/sha256"
+    "encoding/hex"
+    "errors"
+    "fmt"
+    "io"
+    "os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+    hash := sha256.New()
+    hash.Write([]byte(passphrase))
+    hash.Write(salt)
+    return hash.Sum(nil)
+}
+
+func encryptFile(inputPath, outputPath, passphrase string) error {
+    inputFile, err := os.Open(inputPath)
+    if err != nil {
+        return err
+    }
+    defer inputFile.Close()
+
+    plaintext, err := io.ReadAll(inputFile)
+    if err != nil {
+        return err
+    }
+
+    salt := make([]byte, 16)
+    if _, err := rand.Read(salt); err != nil {
+        return err
+    }
+
+    key := deriveKey(passphrase, salt)
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return err
+    }
+
+    nonce := make([]byte, gcm.NonceSize())
+    if _, err := rand.Read(nonce); err != nil {
+        return err
+    }
+
+    ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+
+    outputFile, err := os.Create(outputPath)
+    if err != nil {
+        return err
+    }
+    defer outputFile.Close()
+
+    if _, err := outputFile.Write(salt); err != nil {
+        return err
+    }
+    if _, err := outputFile.Write(ciphertext); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func decryptFile(inputPath, outputPath, passphrase string) error {
+    inputFile, err := os.Open(inputPath)
+    if err != nil {
+        return err
+    }
+    defer inputFile.Close()
+
+    data, err := io.ReadAll(inputFile)
+    if err != nil {
+        return err
+    }
+
+    if len(data) < 16 {
+        return errors.New("file too short")
+    }
+
+    salt := data[:16]
+    ciphertext := data[16:]
+
+    key := deriveKey(passphrase, salt)
+
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return err
+    }
+
+    nonceSize := gcm.NonceSize()
+    if len(ciphertext) < nonceSize {
+        return errors.New("ciphertext too short")
+    }
+
+    nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+    plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+    if err != nil {
+        return err
+    }
+
+    outputFile, err := os.Create(outputPath)
+    if err != nil {
+        return err
+    }
+    defer outputFile.Close()
+
+    if _, err := outputFile.Write(plaintext); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func main() {
+    if len(os.Args) < 5 {
+        fmt.Println("Usage: go run file_encryption_utility.go <encrypt|decrypt> <input> <output> <passphrase>")
+        os.Exit(1)
+    }
+
+    mode := os.Args[1]
+    inputPath := os.Args[2]
+    outputPath := os.Args[3]
+    passphrase := os.Args[4]
+
+    switch mode {
+    case "encrypt":
+        if err := encryptFile(inputPath, outputPath, passphrase); err != nil {
+            fmt.Printf("Encryption failed: %v\n", err)
+            os.Exit(1)
+        }
+        fmt.Printf("File encrypted successfully. Salt: %s\n", hex.EncodeToString([]byte{}))
+    case "decrypt":
+        if err := decryptFile(inputPath, outputPath, passphrase); err != nil {
+            fmt.Printf("Decryption failed: %v\n", err)
+            os.Exit(1)
+        }
+        fmt.Println("File decrypted successfully")
+    default:
+        fmt.Println("Invalid mode. Use 'encrypt' or 'decrypt'")
+        os.Exit(1)
+    }
+}
