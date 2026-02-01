18f55ec08a33bab48a9174934bd75c51625e49ce
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-01 15:36:51 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/hashicorp/hcl/hcl/printer/user_activity_logger.go
+++ vendor/github.com/hashicorp/hcl/hcl/printer/user_activity_logger.go
@@ -46,4 +46,55 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		userID := r.Header.Get("X-User-ID")
+		if userID == "" {
+			userID = "anonymous"
+		}
+
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(recorder, r)
+
+		duration := time.Since(start)
+		al.Logger.Printf(
+			"User %s | Method %s | Path %s | Status %d | Duration %v",
+			userID,
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration,
+		)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
