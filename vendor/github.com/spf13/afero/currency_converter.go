c13364fb3fa3f32bbb6204cf2745b35657781350
heyQuim <hey.quim@hotmail.com>
2026-02-01 23:57:02 +0000

    Add currency conversion utility with USD to EUR conversion

--- vendor/github.com/spf13/afero/currency_converter.go
+++ vendor/github.com/spf13/afero/currency_converter.go
@@ -15,4 +15,38 @@ func main() {
 	usdAmount := 100.0
 	eurAmount := ConvertUSDToEUR(usdAmount)
 	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
+}package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const exchangeRate = 0.85
+
+func convertUSDToEUR(amount float64) float64 {
+	return amount * exchangeRate
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run currency_converter.go <amount_in_usd>")
+		return
+	}
+
+	amountStr := os.Args[1]
+	amount, err := strconv.ParseFloat(amountStr, 64)
+	if err != nil {
+		fmt.Printf("Invalid amount: %s\n", amountStr)
+		return
+	}
+
+	if amount < 0 {
+		fmt.Println("Amount cannot be negative")
+		return
+	}
+
+	converted := convertUSDToEUR(amount)
+	fmt.Printf("%.2f USD = %.2f EUR (Rate: %.2f)\n", amount, converted, exchangeRate)
 }
