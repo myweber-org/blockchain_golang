488d31b82a876116f0c06e677ed1bcf6c8f3f3fe
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-01 00:56:39 +0000

    Add session cleanup job to remove expired sessions from the database

--- vendor/github.com/corgi-kx/logcustom/session_cleanup.go
+++ vendor/github.com/corgi-kx/logcustom/session_cleanup.go
@@ -50,4 +50,70 @@ func (sc *SessionCleaner) cleanExpiredSessions() {
     if rows > 0 {
         log.Printf("Cleaned %d expired sessions", rows)
     }
+}package main
+
+import (
+    "context"
+    "database/sql"
+    "log"
+    "time"
+)
+
+const (
+    cleanupInterval = 1 * time.Hour
+    sessionTTL      = 24 * time.Hour
+)
+
+type SessionCleaner struct {
+    db *sql.DB
+}
+
+func NewSessionCleaner(db *sql.DB) *SessionCleaner {
+    return &SessionCleaner{db: db}
+}
+
+func (sc *SessionCleaner) Run(ctx context.Context) {
+    ticker := time.NewTicker(cleanupInterval)
+    defer ticker.Stop()
+
+    for {
+        select {
+        case <-ctx.Done():
+            log.Println("Session cleaner stopped")
+            return
+        case <-ticker.C:
+            sc.cleanupExpiredSessions()
+        }
+    }
+}
+
+func (sc *SessionCleaner) cleanupExpiredSessions() {
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    defer cancel()
+
+    cutoffTime := time.Now().Add(-sessionTTL)
+    query := `DELETE FROM user_sessions WHERE last_activity < $1`
+
+    result, err := sc.db.ExecContext(ctx, query, cutoffTime)
+    if err != nil {
+        log.Printf("Failed to clean expired sessions: %v", err)
+        return
+    }
+
+    rowsAffected, _ := result.RowsAffected()
+    if rowsAffected > 0 {
+        log.Printf("Cleaned %d expired sessions", rowsAffected)
+    }
+}
+
+func main() {
+    db, err := sql.Open("postgres", "postgresql://localhost/sessions")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    cleaner := NewSessionCleaner(db)
+    ctx := context.Background()
+    cleaner.Run(ctx)
 }
