61d4ca449ff3f6fd62dc5402c4cf7ab6b111042b
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-01 15:38:45 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

---
+++ vendor/golang.org/x/sys/cpu/data_cleaner.go
@@ -0,0 +1,15 @@
+package utils
+
+func RemoveDuplicates(nums []int) []int {
+    seen := make(map[int]bool)
+    result := []int{}
+    
+    for _, num := range nums {
+        if !seen[num] {
+            seen[num] = true
+            result = append(result, num)
+        }
+    }
+    
+    return result
+}
