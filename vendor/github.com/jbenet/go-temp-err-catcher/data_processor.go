1ef584dc85e503aba986e2dccb58a3bce437da59
FelixMakes <felix.makes@hotmail.com>
2026-02-01 14:27:09 +0000

    Add utility function to filter and sort numeric slices

---
+++ vendor/github.com/jbenet/go-temp-err-catcher/data_processor.go
@@ -0,0 +1,24 @@
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func FilterAndSort(numbers []int, threshold int) []int {
+	var filtered []int
+	for _, num := range numbers {
+		if num > threshold {
+			filtered = append(filtered, num)
+		}
+	}
+	sort.Ints(filtered)
+	return filtered
+}
+
+func main() {
+	data := []int{45, 12, 89, 3, 67, 34, 9, 21}
+	result := FilterAndSort(data, 20)
+	fmt.Println("Filtered and sorted:", result)
+}
