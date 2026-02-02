d7cd7f8cd6681239778d442fb0087745e87b67e1
InkspillVerse <inkspill.verse@outlook.com>
2026-02-02 20:02:02 +0000

    Add data validation and sanitization functions for user input processing

---
+++ vendor/golang.org/x/text/internal/language/compact/data_processor.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeUsername(input string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+	sanitized := re.ReplaceAllString(input, "")
+	return strings.TrimSpace(sanitized)
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func TruncateString(input string, maxLength int) string {
+	if len(input) <= maxLength {
+		return input
+	}
+	return input[:maxLength]
+}
