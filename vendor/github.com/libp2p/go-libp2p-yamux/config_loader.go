42b2dc859b34dcc84a3c5a4e674e26d93cf8bfe4
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-02 19:00:30 +0000

    Add configuration file loader with YAML parsing support

---
+++ vendor/github.com/libp2p/go-libp2p-yamux/config_loader.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(filename string) (*AppConfig, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) bool {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		log.Printf("Invalid server port: %d", config.Server.Port)
+		return false
+	}
+
+	if config.Database.Host == "" {
+		log.Print("Database host cannot be empty")
+		return false
+	}
+
+	return true
+}
