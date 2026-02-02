49e6551a8b134cb789b8a22fcae9fae1d5bf1d38
VioletInquiry <violet.inquiry@outlook.com>
2026-02-02 23:55:50 +0000

    Add user activity logging with timestamps and action types

--- vendor/go.opencensus.io/stats/user_activity_logger.go
+++ vendor/go.opencensus.io/stats/user_activity_logger.go
@@ -85,4 +85,87 @@ func ActivityLogger(next http.Handler) http.Handler {
 
 func extractUserID(token string) string {
 	return "user_" + token[:8]
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityType string
+
+const (
+    Login    ActivityType = "LOGIN"
+    Logout   ActivityType = "LOGOUT"
+    Purchase ActivityType = "PURCHASE"
+    View     ActivityType = "VIEW"
+    Search   ActivityType = "SEARCH"
+)
+
+type UserActivity struct {
+    UserID    string       `json:"user_id"`
+    Action    ActivityType `json:"action"`
+    Timestamp time.Time    `json:"timestamp"`
+    Details   string       `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+    encoder *json.Encoder
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{
+        logFile: file,
+        encoder: json.NewEncoder(file),
+    }, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID string, action ActivityType, details string) error {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+    return l.encoder.Encode(activity)
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activities.jsonl")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    activities := []struct {
+        userID  string
+        action  ActivityType
+        details string
+    }{
+        {"user_123", Login, "Successful authentication"},
+        {"user_123", Search, "Query: 'golang tutorials'"},
+        {"user_456", View, "Product ID: 789"},
+        {"user_123", Purchase, "Order ID: 555, Amount: $49.99"},
+        {"user_456", Logout, "Session duration: 25m"},
+    }
+
+    for _, a := range activities {
+        if err := logger.LogActivity(a.userID, a.action, a.details); err != nil {
+            fmt.Printf("Failed to log activity: %v\n", err)
+        }
+    }
+
+    fmt.Println("Activity logging completed")
 }
