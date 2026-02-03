4f737df4fbae22d01dfd08c8942816f45688b195
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-03 00:25:24 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/hashicorp/hcl/hcl/strconv/user_activity_logger.go
+++ vendor/github.com/hashicorp/hcl/hcl/strconv/user_activity_logger.go
@@ -108,4 +108,49 @@ type loggingResponseWriter struct {
 func (lrw *loggingResponseWriter) WriteHeader(code int) {
 	lrw.statusCode = code
 	lrw.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+type ActivityLogger struct {
+    handler http.Handler
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    start := time.Now()
+    al.handler.ServeHTTP(w, r)
+    duration := time.Since(start)
+
+    log.Printf("[%s] %s %s %s %v",
+        time.Now().Format(time.RFC3339),
+        r.Method,
+        r.URL.Path,
+        r.RemoteAddr,
+        duration)
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+    return &ActivityLogger{handler: handler}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(`{"status": "ok"}`))
+}
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/api/data", apiHandler)
+
+    wrappedMux := NewActivityLogger(mux)
+
+    log.Println("Server starting on :8080")
+    if err := http.ListenAndServe(":8080", wrappedMux); err != nil {
+        log.Fatal(err)
+    }
 }
