9c01c423782b037ac768f013ed2a572ffc94e95e
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-03 16:00:55 +0000

    Add configuration loader with YAML support and environment variable overrides

--- vendor/github.com/libp2p/go-libp2p/p2p/protocol/ping/config_loader.go
+++ vendor/github.com/libp2p/go-libp2p/p2p/protocol/ping/config_loader.go
@@ -61,4 +61,61 @@ func ValidateConfig(config *AppConfig) bool {
 		return false
 	}
 	return true
+}package config
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		URL      string `yaml:"url" env:"DB_URL"`
+		PoolSize int    `yaml:"pool_size" env:"DB_POOL_SIZE"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(&cfg)
+	return &cfg, nil
+}
+
+func overrideFromEnv(cfg *Config) {
+	overrideString(&cfg.Server.Host, "SERVER_HOST")
+	overrideInt(&cfg.Server.Port, "SERVER_PORT")
+	overrideString(&cfg.Database.URL, "DB_URL")
+	overrideInt(&cfg.Database.PoolSize, "DB_POOL_SIZE")
+	overrideString(&cfg.LogLevel, "LOG_LEVEL")
+}
+
+func overrideString(field *string, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		*field = val
+	}
+}
+
+func overrideInt(field *int, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		var intVal int
+		if _, err := fmt.Sscanf(val, "%d", &intVal); err == nil {
+			*field = intVal
+		}
+	}
 }
