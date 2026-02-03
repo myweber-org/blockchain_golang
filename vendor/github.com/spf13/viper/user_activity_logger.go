691987e1597b1bc0ed6a37fc489dd89d1cb3bd82
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-03 20:06:45 +0000

    Add user activity logging with timestamps and action types

--- vendor/github.com/spf13/viper/user_activity_logger.go
+++ vendor/github.com/spf13/viper/user_activity_logger.go
@@ -32,4 +32,51 @@ func extractUserID(r *http.Request) string {
 		return user
 	}
 	return "anonymous"
+}package main
+
+import (
+    "encoding/json"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityLog struct {
+    Timestamp time.Time `json:"timestamp"`
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Details   string    `json:"details"`
+}
+
+func logActivity(userID, action, details string) error {
+    logEntry := ActivityLog{
+        Timestamp: time.Now(),
+        UserID:    userID,
+        Action:    action,
+        Details:   details,
+    }
+
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    encoder := json.NewEncoder(file)
+    encoder.SetIndent("", "  ")
+    return encoder.Encode(logEntry)
+}
+
+func main() {
+    err := logActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    err = logActivity("user123", "upload", "File: report.pdf")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    log.Println("Activity logging completed")
 }
