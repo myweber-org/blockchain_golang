c4316c5dc962cb7f76b8acd63ce3926d6b14f270
FelixMakes <felix.makes@hotmail.com>
2026-02-03 22:02:07 +0000

    Add data validation and sanitization for user input

--- vendor/go.opencensus.io/internal/tagencoding/data_processor.go
+++ vendor/go.opencensus.io/internal/tagencoding/data_processor.go
@@ -54,4 +54,54 @@ type InvalidUsernameError struct {
 
 func (e *InvalidUsernameError) Error() string {
 	return "Username must be at least 3 characters: " + e.Username
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type UserData struct {
+	Username string
+	Email    string
+	Comments string
+}
+
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+
+func SanitizeInput(input string) string {
+	input = strings.TrimSpace(input)
+	re := regexp.MustCompile(`[<>"'&]`)
+	return re.ReplaceAllString(input, "")
+}
+
+func ValidateUserData(data UserData) (bool, []string) {
+	var errors []string
+
+	if len(data.Username) < 3 || len(data.Username) > 20 {
+		errors = append(errors, "Username must be between 3 and 20 characters")
+	}
+
+	if !emailRegex.MatchString(data.Email) {
+		errors = append(errors, "Invalid email format")
+	}
+
+	if len(data.Comments) > 500 {
+		errors = append(errors, "Comments must not exceed 500 characters")
+	}
+
+	data.Username = SanitizeInput(data.Username)
+	data.Email = SanitizeInput(data.Email)
+	data.Comments = SanitizeInput(data.Comments)
+
+	return len(errors) == 0, errors
+}
+
+func ProcessUserData(data UserData) (UserData, []string) {
+	isValid, validationErrors := ValidateUserData(data)
+	if !isValid {
+		return data, validationErrors
+	}
+
+	return data, nil
 }
