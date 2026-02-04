15ebb4b10af9e862bc3a165654e16a9c203fc418
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-04 21:10:22 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/libp2p/go-libp2p-peerstore/user_activity_logger.go
+++ vendor/github.com/libp2p/go-libp2p-peerstore/user_activity_logger.go
@@ -27,4 +27,32 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userAgent := r.UserAgent()
+	clientIP := r.RemoteAddr
+	method := r.Method
+	path := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("Activity: %s %s | IP: %s | Agent: %s | Duration: %v", 
+		method, path, clientIP, userAgent, duration)
 }
