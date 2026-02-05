948e9fcf0331adda5b7152706a0699aceeea4147
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-05 19:43:18 +0000

    Implement log rotation with size-based and time-based triggers

--- vendor/github.com/libp2p/go-libp2p-circuit/pb/log_rotator.go
+++ vendor/github.com/libp2p/go-libp2p-circuit/pb/log_rotator.go
@@ -152,4 +152,110 @@ func main() {
         rotator.Write([]byte(msg))
         time.Sleep(100 * time.Millisecond)
     }
+}package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sync"
+    "time"
+)
+
+type Rotator struct {
+    mu          sync.Mutex
+    file        *os.File
+    basePath    string
+    maxSize     int64
+    maxAge      time.Duration
+    currentSize int64
+    createdAt   time.Time
+}
+
+func NewRotator(basePath string, maxSize int64, maxAge time.Duration) (*Rotator, error) {
+    r := &Rotator{
+        basePath:  basePath,
+        maxSize:   maxSize,
+        maxAge:    maxAge,
+        createdAt: time.Now(),
+    }
+    if err := r.openFile(); err != nil {
+        return nil, err
+    }
+    return r, nil
+}
+
+func (r *Rotator) openFile() error {
+    dir := filepath.Dir(r.basePath)
+    if err := os.MkdirAll(dir, 0755); err != nil {
+        return err
+    }
+    f, err := os.OpenFile(r.basePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    info, err := f.Stat()
+    if err != nil {
+        f.Close()
+        return err
+    }
+    r.file = f
+    r.currentSize = info.Size()
+    return nil
+}
+
+func (r *Rotator) rotate() error {
+    if r.file != nil {
+        r.file.Close()
+    }
+    timestamp := time.Now().Format("20060102_150405")
+    backupPath := fmt.Sprintf("%s.%s", r.basePath, timestamp)
+    if err := os.Rename(r.basePath, backupPath); err != nil {
+        return err
+    }
+    r.createdAt = time.Now()
+    return r.openFile()
+}
+
+func (r *Rotator) Write(p []byte) (int, error) {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+
+    if r.currentSize+int64(len(p)) > r.maxSize || time.Since(r.createdAt) > r.maxAge {
+        if err := r.rotate(); err != nil {
+            return 0, err
+        }
+    }
+    n, err := r.file.Write(p)
+    if err == nil {
+        r.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (r *Rotator) Close() error {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+    if r.file != nil {
+        return r.file.Close()
+    }
+    return nil
+}
+
+func main() {
+    rotator, err := NewRotator("./logs/app.log", 1024*1024, 24*time.Hour)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Failed to create rotator: %v\n", err)
+        os.Exit(1)
+    }
+    defer rotator.Close()
+
+    for i := 0; i < 100; i++ {
+        msg := fmt.Sprintf("Log entry %d at %s\n", i, time.Now().Format(time.RFC3339))
+        if _, err := rotator.Write([]byte(msg)); err != nil {
+            fmt.Fprintf(os.Stderr, "Write error: %v\n", err)
+        }
+        time.Sleep(100 * time.Millisecond)
+    }
 }
