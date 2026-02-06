0a58d93742dc49922fa0e4c28d02c45358709499
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-06 21:22:13 +0000

    Add file encryption utility with AES-256-GCM and key derivation

--- vendor/github.com/hashicorp/hcl/json/scanner/file_encryption_utility.go
+++ vendor/github.com/hashicorp/hcl/json/scanner/file_encryption_utility.go
@@ -102,3 +102,97 @@ func main() {
 	}
 	fmt.Printf("Decrypted: %s\n", decrypted)
 }
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write([]byte(passphrase))
+	hash.Write(salt)
+	return hash.Sum(nil)
+}
+
+func encryptData(data []byte, passphrase string) ([]byte, error) {
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return nil, err
+	}
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return append(salt, ciphertext...), nil
+}
+
+func decryptData(encryptedData []byte, passphrase string) ([]byte, error) {
+	if len(encryptedData) < 16 {
+		return nil, errors.New("invalid encrypted data")
+	}
+
+	salt := encryptedData[:16]
+	ciphertext := encryptedData[16:]
+
+	key := deriveKey(passphrase, salt)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("invalid ciphertext")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func main() {
+	originalText := "Sensitive data that needs protection"
+	passphrase := "secure-passphrase-123"
+
+	fmt.Println("Original:", originalText)
+
+	encrypted, err := encryptData([]byte(originalText), passphrase)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Encryption error: %v\n", err)
+		return
+	}
+	fmt.Printf("Encrypted: %x\n", encrypted)
+
+	decrypted, err := decryptData(encrypted, passphrase)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Decryption error: %v\n", err)
+		return
+	}
+	fmt.Println("Decrypted:", string(decrypted))
+}
