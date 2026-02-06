f37b477394ffa89008e1bdb688f55760202b8363
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-06 22:17:13 +0000

    Add configuration file loader with YAML parsing support

--- vendor/github.com/cloudflare/cfssl/scan/crypto/config_loader.go
+++ vendor/github.com/cloudflare/cfssl/scan/crypto/config_loader.go
@@ -3,8 +3,6 @@ package config
 import (
     "fmt"
     "io/ioutil"
-    "os"
-
     "gopkg.in/yaml.v2"
 )
 
@@ -14,111 +12,35 @@ type Config struct {
         Port int    `yaml:"port"`
     } `yaml:"server"`
     Database struct {
-        Host     string `yaml:"host"`
         Username string `yaml:"username"`
         Password string `yaml:"password"`
         Name     string `yaml:"name"`
     } `yaml:"database"`
-    LogLevel string `yaml:"log_level"`
 }
 
 func LoadConfig(path string) (*Config, error) {
-    if _, err := os.Stat(path); os.IsNotExist(err) {
-        return nil, fmt.Errorf("config file does not exist: %s", path)
-    }
-
     data, err := ioutil.ReadFile(path)
     if err != nil {
-        return nil, fmt.Errorf("failed to read config file: %v", err)
+        return nil, fmt.Errorf("failed to read config file: %w", err)
     }
 
     var config Config
     if err := yaml.Unmarshal(data, &config); err != nil {
-        return nil, fmt.Errorf("failed to parse YAML: %v", err)
-    }
-
-    if err := validateConfig(&config); err != nil {
-        return nil, fmt.Errorf("config validation failed: %v", err)
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
     }
 
     return &config, nil
 }
 
-func validateConfig(c *Config) error {
+func (c *Config) Validate() error {
     if c.Server.Host == "" {
         return fmt.Errorf("server host cannot be empty")
     }
     if c.Server.Port <= 0 || c.Server.Port > 65535 {
         return fmt.Errorf("invalid server port: %d", c.Server.Port)
     }
-    if c.Database.Host == "" {
-        return fmt.Errorf("database host cannot be empty")
-    }
-    if c.LogLevel == "" {
-        c.LogLevel = "info"
+    if c.Database.Name == "" {
+        return fmt.Errorf("database name cannot be empty")
     }
     return nil
-}package config
-
-import (
-	"os"
-	"strings"
-
-	"gopkg.in/yaml.v3"
-)
-
-type Config struct {
-	Server struct {
-		Host string `yaml:"host"`
-		Port int    `yaml:"port"`
-	} `yaml:"server"`
-	Database struct {
-		URL      string `yaml:"url"`
-		PoolSize int    `yaml:"pool_size"`
-	} `yaml:"database"`
-	LogLevel string `yaml:"log_level"`
-}
-
-func LoadConfig(path string) (*Config, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	var cfg Config
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		return nil, err
-	}
-
-	overrideWithEnv(&cfg)
-	return &cfg, nil
-}
-
-func overrideWithEnv(cfg *Config) {
-	if val := os.Getenv("SERVER_HOST"); val != "" {
-		cfg.Server.Host = val
-	}
-	if val := os.Getenv("SERVER_PORT"); val != "" {
-		cfg.Server.Port = atoi(val)
-	}
-	if val := os.Getenv("DATABASE_URL"); val != "" {
-		cfg.Database.URL = val
-	}
-	if val := os.Getenv("DATABASE_POOL_SIZE"); val != "" {
-		cfg.Database.PoolSize = atoi(val)
-	}
-	if val := os.Getenv("LOG_LEVEL"); val != "" {
-		cfg.LogLevel = strings.ToUpper(val)
-	}
-}
-
-func atoi(s string) int {
-	var n int
-	for _, ch := range s {
-		if ch < '0' || ch > '9' {
-			break
-		}
-		n = n*10 + int(ch-'0')
-	}
-	return n
 }
