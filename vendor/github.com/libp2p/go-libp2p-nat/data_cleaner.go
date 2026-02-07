85f79cf492a84e378bedab83591c7a5ab6060ece
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-07 00:47:21 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- vendor/github.com/libp2p/go-libp2p-nat/data_cleaner.go
+++ vendor/github.com/libp2p/go-libp2p-nat/data_cleaner.go
@@ -24,4 +24,75 @@ func main() {
 	uniqueStrings := RemoveDuplicates(strings)
 	fmt.Println("Original:", strings)
 	fmt.Println("Unique:", uniqueStrings)
+}package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: data_cleaner <input.csv>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := strings.TrimSuffix(inputFile, ".csv") + "_cleaned.csv"
+
+	inFile, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening input file: %v\n", err)
+		os.Exit(1)
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+
+	reader := csv.NewReader(inFile)
+	writer := csv.NewWriter(outFile)
+	defer writer.Flush()
+
+	seen := make(map[string]bool)
+	headers, err := reader.Read()
+	if err != nil {
+		fmt.Printf("Error reading headers: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = writer.Write(headers)
+	if err != nil {
+		fmt.Printf("Error writing headers: %v\n", err)
+		os.Exit(1)
+	}
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Error reading record: %v\n", err)
+			continue
+		}
+
+		key := strings.Join(record, "|")
+		if !seen[key] {
+			seen[key] = true
+			err = writer.Write(record)
+			if err != nil {
+				fmt.Printf("Error writing record: %v\n", err)
+			}
+		}
+	}
+
+	fmt.Printf("Cleaned data written to %s\n", outputFile)
 }
