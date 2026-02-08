db329f76ceea4c3d310af4e058623ed1614cbb6b
FelixMakes <felix.makes@hotmail.com>
2026-02-08 23:54:52 +0000

    Add data cleaning utility with duplicate removal and trimming functions

--- vendor/github.com/libp2p/go-ws-transport/data_cleaner.go
+++ vendor/github.com/libp2p/go-ws-transport/data_cleaner.go
@@ -13,3 +13,34 @@ func RemoveDuplicates[T comparable](slice []T) []T {
 
 	return result
 }
+package main
+
+import (
+	"strings"
+)
+
+func RemoveDuplicates(input []string) []string {
+	seen := make(map[string]bool)
+	result := []string{}
+	for _, item := range input {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func TrimSpaces(input []string) []string {
+	result := make([]string, len(input))
+	for i, item := range input {
+		result[i] = strings.TrimSpace(item)
+	}
+	return result
+}
+
+func CleanData(data []string) []string {
+	trimmed := TrimSpaces(data)
+	cleaned := RemoveDuplicates(trimmed)
+	return cleaned
+}
