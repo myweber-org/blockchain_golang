1d4682b23a8a0e16713b12b8053c0238f43c8fba
FelixMakes <felix.makes@hotmail.com>
2026-02-08 00:08:02 +0000

    Add user activity logging middleware for tracking API requests

--- vendor/github.com/hashicorp/hcl/hcl/user_activity_logger.go
+++ vendor/github.com/hashicorp/hcl/hcl/user_activity_logger.go
@@ -34,3 +34,75 @@ func ActivityLogger(next http.Handler) http.Handler {
 		)
 	})
 }
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time
+	Method    string
+	Path      string
+	UserAgent string
+	IPAddress string
+	Duration  time.Duration
+	Status    int
+}
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+
+	al.handler.ServeHTTP(recorder, r)
+
+	duration := time.Since(startTime)
+
+	activity := ActivityLog{
+		Timestamp: startTime,
+		Method:    r.Method,
+		Path:      r.URL.Path,
+		UserAgent: r.UserAgent(),
+		IPAddress: r.RemoteAddr,
+		Duration:  duration,
+		Status:    recorder.statusCode,
+	}
+
+	al.logActivity(activity)
+}
+
+func (al *ActivityLogger) logActivity(activity ActivityLog) {
+	log.Printf("ACTIVITY: %s | %s %s | %s | %s | %d | %v",
+		activity.Timestamp.Format(time.RFC3339),
+		activity.Method,
+		activity.Path,
+		activity.IPAddress,
+		activity.UserAgent,
+		activity.Status,
+		activity.Duration,
+	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
+}
