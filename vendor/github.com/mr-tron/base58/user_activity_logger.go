b9e40cd24c91bec766de5fb1f45cc8eb1730605d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-08 19:39:05 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/mr-tron/base58/user_activity_logger.go
+++ vendor/github.com/mr-tron/base58/user_activity_logger.go
@@ -1,67 +1,46 @@
-package main
+package middleware
 
 import (
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"time"
 )
 
-type ActivityEvent struct {
-	Timestamp time.Time
-	UserID    string
-	EventType string
-	Details   string
-}
-
 type ActivityLogger struct {
-	logFile *os.File
+	handler http.Handler
 }
 
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, err
-	}
-	return &ActivityLogger{logFile: file}, nil
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLogger) LogActivity(userID, eventType, details string) {
-	event := ActivityEvent{
-		Timestamp: time.Now(),
-		UserID:    userID,
-		EventType: eventType,
-		Details:   details,
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
 	}
-
-	logEntry := fmt.Sprintf("%s | User: %s | Event: %s | Details: %s\n",
-		event.Timestamp.Format("2006-01-02 15:04:05"),
-		event.UserID,
-		event.EventType,
-		event.Details)
-
-	if _, err := al.logFile.WriteString(logEntry); err != nil {
-		log.Printf("Failed to write activity log: %v", err)
-	}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf("%s %s %d %v %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		duration,
+		r.RemoteAddr,
+	)
 }
 
-func (al *ActivityLogger) Close() {
-	if al.logFile != nil {
-		al.logFile.Close()
-	}
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func main() {
-	logger, err := NewActivityLogger("user_activity.log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logger.Close()
-
-	logger.LogActivity("user123", "LOGIN", "Successful authentication")
-	logger.LogActivity("user123", "VIEW_PAGE", "Accessed dashboard")
-	logger.LogActivity("user456", "UPLOAD", "File: report.pdf")
-	logger.LogActivity("user123", "LOGOUT", "Session ended")
-
-	fmt.Println("Activity logging completed. Check user_activity.log for details.")
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
