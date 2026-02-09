eecffa88ae65dd717c40c5f1df7ecb6b545b2fcd
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-09 15:10:06 +0000

    Add data cleaning utility to remove duplicate entries from slices

--- vendor/golang.org/x/text/encoding/internal/identifier/data_cleaner.go
+++ vendor/golang.org/x/text/encoding/internal/identifier/data_cleaner.go
@@ -60,3 +60,16 @@ func main() {
 
     fmt.Println("CSV cleaning completed successfully")
 }
+package data
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
+}
