d9206b2b8d4ca13e3c5a9243bd256d46ac6b0a61
ElaraWrites <elara.writes@outlook.com>
2026-02-10 21:49:41 +0000

    Add user activity logging with timestamp and action type

--- vendor/github.com/libp2p/go-libp2p-secio/pb/user_activity_logger.go
+++ vendor/github.com/libp2p/go-libp2p-secio/pb/user_activity_logger.go
@@ -24,4 +24,48 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	duration := time.Since(start)
 	log.Printf("Activity: %s | %s | %s | %v", clientIP, userAgent, requestPath, duration)
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Details   string    `json:"details"`
+}
+
+func logActivity(userID, action, details string) {
+	activity := ActivityLog{
+		Timestamp: time.Now(),
+		UserID:    userID,
+		Action:    action,
+		Details:   details,
+	}
+
+	file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("Failed to open log file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(activity); err != nil {
+		log.Printf("Failed to write log entry: %v", err)
+	}
+}
+
+func main() {
+	logActivity("user123", "LOGIN", "User logged in from web browser")
+	logActivity("user456", "UPDATE_PROFILE", "Changed email address")
+	logActivity("user123", "LOGOUT", "Session expired after 30 minutes")
+
+	fmt.Println("Activity logging completed. Check activity.log for details.")
 }
