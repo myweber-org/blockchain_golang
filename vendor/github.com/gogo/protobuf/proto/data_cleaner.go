434b1a66741a1613cd07e2d1f1801db680661fb8
VioletInquiry <violet.inquiry@outlook.com>
2026-02-10 23:13:22 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- vendor/github.com/gogo/protobuf/proto/data_cleaner.go
+++ vendor/github.com/gogo/protobuf/proto/data_cleaner.go
@@ -12,3 +12,26 @@ func RemoveDuplicates[T comparable](slice []T) []T {
 	}
 	return result
 }
+package main
+
+import "fmt"
+
+func removeDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := removeDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}
