2f9d14f6afebd99a205167bfe44fbc219324fedd
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-10 20:31:59 +0000

    Add currency conversion utility with USD to EUR rate

---
+++ vendor/github.com/whyrusleeping/mafmt/currency_converter.go
@@ -0,0 +1,36 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const usdToEurRate = 0.85
+
+func convertUSDToEUR(amount float64) float64 {
+	return amount * usdToEurRate
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run currency_converter.go <amount_in_usd>")
+		os.Exit(1)
+	}
+
+	amountStr := os.Args[1]
+	amount, err := strconv.ParseFloat(amountStr, 64)
+	if err != nil {
+		fmt.Printf("Invalid amount: %s\n", amountStr)
+		os.Exit(1)
+	}
+
+	if amount < 0 {
+		fmt.Println("Amount cannot be negative")
+		os.Exit(1)
+	}
+
+	converted := convertUSDToEUR(amount)
+	fmt.Printf("%.2f USD = %.2f EUR\n", amount, converted)
+}
