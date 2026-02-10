3c514aabcc3f6f0bd7d640527b105ebcb4f6a72a
ElaraWrites <elara.writes@outlook.com>
2026-02-10 00:14:01 +0000

    Add data cleaning utility for removing duplicate entries

--- vendor/github.com/opentracing/data_cleaner.go
+++ vendor/github.com/opentracing/data_cleaner.go
@@ -42,4 +42,33 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
+}package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func RemoveDuplicates(input []int) []int {
+	if len(input) == 0 {
+		return input
+	}
+
+	sort.Ints(input)
+
+	unique := input[:1]
+	for i := 1; i < len(input); i++ {
+		if input[i] != input[i-1] {
+			unique = append(unique, input[i])
+		}
+	}
+
+	return unique
+}
+
+func main() {
+	data := []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}
+	cleaned := RemoveDuplicates(data)
+	fmt.Printf("Original: %v\n", data)
+	fmt.Printf("Cleaned: %v\n", cleaned)
 }
