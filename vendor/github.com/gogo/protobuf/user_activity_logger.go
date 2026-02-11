2979065fa0f6a70e69a9632cf5e7f0b955252b11
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-11 20:21:41 +0000

    Add user activity logging with timestamp and action type

--- vendor/github.com/gogo/protobuf/user_activity_logger.go
+++ vendor/github.com/gogo/protobuf/user_activity_logger.go
@@ -46,4 +46,60 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type UserActivity struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+func logActivity(userID, action, details string) error {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    encoder := json.NewEncoder(file)
+    encoder.SetIndent("", "  ")
+    if err := encoder.Encode(activity); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func main() {
+    activities := []struct {
+        userID, action, details string
+    }{
+        {"user_001", "login", "Successful authentication"},
+        {"user_002", "purchase", "Order #789 completed"},
+        {"user_001", "logout", "Session terminated"},
+    }
+
+    for _, act := range activities {
+        if err := logActivity(act.userID, act.action, act.details); err != nil {
+            log.Printf("Failed to log activity: %v", err)
+        } else {
+            fmt.Printf("Logged: %s - %s\n", act.userID, act.action)
+        }
+    }
 }
