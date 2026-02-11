48d4c690ca86aadd067ea5a0eed0a0609c9c7fe8
InkspillVerse <inkspill.verse@outlook.com>
2026-02-11 20:41:19 +0000

    Add user activity logging with timestamps and action types

--- vendor/golang.org/x/net/bpf/user_activity_logger.go
+++ vendor/golang.org/x/net/bpf/user_activity_logger.go
@@ -53,4 +53,73 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityLog struct {
+    Timestamp time.Time `json:"timestamp"`
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Details   string    `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, action, details string) error {
+    activity := ActivityLog{
+        Timestamp: time.Now(),
+        UserID:    userID,
+        Action:    action,
+        Details:   details,
+    }
+
+    data, err := json.Marshal(activity)
+    if err != nil {
+        return err
+    }
+
+    data = append(data, '\n')
+    _, err = al.logFile.Write(data)
+    return err
+}
+
+func (al *ActivityLogger) Close() error {
+    return al.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("activity.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    err = logger.LogActivity("user123", "file_upload", "uploaded profile.jpg")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    fmt.Println("Activity logging completed")
 }
