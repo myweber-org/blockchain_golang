7a7bc08ff2929b60bd3f912a47dac68f0f5ae08e
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-11 15:34:22 +0000

    Add user activity logging middleware with request details and timestamp

--- vendor/github.com/libp2p/go-libp2p/p2p/protocol/ping/user_activity_logger.go
+++ vendor/github.com/libp2p/go-libp2p/p2p/protocol/ping/user_activity_logger.go
@@ -69,4 +69,32 @@ func main() {
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf("[%s] %s %s %s %v",
+		time.Now().Format(time.RFC3339),
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
