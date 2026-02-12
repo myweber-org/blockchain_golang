dbb4f247c5403491f5bb570aff86fbde43b91245
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-12 19:21:24 +0000

    Add file encryption utility with AES-256-GCM support

--- vendor/github.com/hashicorp/file_encryption.go
+++ vendor/github.com/hashicorp/file_encryption.go
@@ -75,3 +75,96 @@ func main() {
 
 	fmt.Printf("Decrypted: %s\n", string(decrypted))
 }
+package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/hex"
+    "errors"
+    "fmt"
+    "io"
+    "os"
+)
+
+func generateKey() ([]byte, error) {
+    key := make([]byte, 32)
+    _, err := rand.Read(key)
+    if err != nil {
+        return nil, err
+    }
+    return key, nil
+}
+
+func encryptData(plaintext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return nil, err
+    }
+
+    nonce := make([]byte, gcm.NonceSize())
+    if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+        return nil, err
+    }
+
+    ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+    return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return nil, err
+    }
+
+    nonceSize := gcm.NonceSize()
+    if len(ciphertext) < nonceSize {
+        return nil, errors.New("ciphertext too short")
+    }
+
+    nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+    plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+    if err != nil {
+        return nil, err
+    }
+
+    return plaintext, nil
+}
+
+func main() {
+    key, err := generateKey()
+    if err != nil {
+        fmt.Printf("Key generation failed: %v\n", err)
+        os.Exit(1)
+    }
+
+    fmt.Printf("Generated key: %s\n", hex.EncodeToString(key))
+
+    secretMessage := []byte("This is a confidential message")
+    fmt.Printf("Original: %s\n", secretMessage)
+
+    encrypted, err := encryptData(secretMessage, key)
+    if err != nil {
+        fmt.Printf("Encryption failed: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Printf("Encrypted: %s\n", hex.EncodeToString(encrypted))
+
+    decrypted, err := decryptData(encrypted, key)
+    if err != nil {
+        fmt.Printf("Decryption failed: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Printf("Decrypted: %s\n", decrypted)
+}
