eff37367c28d8e824526ccc3c67e7f0a896b1844
InkspillVerse <inkspill.verse@outlook.com>
2026-02-13 23:47:49 +0000

    Add user activity logging middleware with rate limiting and error handling

--- vendor/golang.org/x/net/internal/iana/user_activity_logger.go
+++ vendor/golang.org/x/net/internal/iana/user_activity_logger.go
@@ -4,26 +4,55 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"golang.org/x/time/rate"
 )
 
 type ActivityLogger struct {
-	handler http.Handler
+	limiter *rate.Limiter
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(rps int) *ActivityLogger {
+	return &ActivityLogger{
+		limiter: rate.NewLimiter(rate.Limit(rps), rps*2),
+	}
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
+func (al *ActivityLogger) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !al.limiter.Allow() {
+			http.Error(w, "rate limit exceeded", http.StatusTooManyRequests)
+			return
+		}
 
-	log.Printf("[%s] %s %s - %s - Duration: %v",
-		time.Now().Format(time.RFC3339),
+		start := time.Now()
+		recorder := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
+
+		defer func() {
+			duration := time.Since(start)
+			al.logActivity(r, recorder.statusCode, duration)
+		}()
+
+		next.ServeHTTP(recorder, r)
+	})
+}
+
+func (al *ActivityLogger) logActivity(r *http.Request, status int, duration time.Duration) {
+	log.Printf("ACTIVITY: %s %s %d %s %s",
 		r.Method,
 		r.URL.Path,
+		status,
+		duration.Round(time.Millisecond),
 		r.RemoteAddr,
-		duration,
 	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
