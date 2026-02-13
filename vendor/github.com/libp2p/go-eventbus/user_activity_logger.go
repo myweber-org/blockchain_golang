ee3c6a499174568ac60dc12b628cd62d1c87b209
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-13 20:30:03 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/libp2p/go-eventbus/user_activity_logger.go
+++ vendor/github.com/libp2p/go-eventbus/user_activity_logger.go
@@ -6,43 +6,41 @@ import (
 	"time"
 )
 
-type ActivityLogger struct {
-	handler http.Handler
+type ActivityLog struct {
+	UserID    string
+	Endpoint  string
+	Method    string
+	Timestamp time.Time
+	IPAddress string
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		userID := "anonymous"
+		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
+			userID = extractUserID(authHeader)
+		}
+
+		activity := ActivityLog{
+			UserID:    userID,
+			Endpoint:  r.URL.Path,
+			Method:    r.Method,
+			Timestamp: start,
+			IPAddress: r.RemoteAddr,
+		}
+
+		log.Printf("Activity: %s %s by %s from %s", 
+			activity.Method, 
+			activity.Endpoint, 
+			activity.UserID, 
+			activity.IPAddress)
+
+		next.ServeHTTP(w, r)
+	})
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf(
-		"Method: %s | Path: %s | Status: %d | Duration: %v | RemoteAddr: %s | UserAgent: %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.RemoteAddr,
-		r.UserAgent(),
-	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+func extractUserID(token string) string {
+	return "user_" + token[:8]
 }
