9a285c9a4ca9ff753b39837fbeb6ffa8d40bbdfa
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-14 20:21:10 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- vendor/github.com/libp2p/go-libp2p-mplex/data_cleaner.go
+++ vendor/github.com/libp2p/go-libp2p-mplex/data_cleaner.go
@@ -201,3 +201,75 @@ func main() {
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
 }
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func removeDuplicates(inputFile, outputFile string) error {
+	in, err := os.Open(inputFile)
+	if err != nil {
+		return fmt.Errorf("failed to open input file: %w", err)
+	}
+	defer in.Close()
+
+	out, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer out.Close()
+
+	reader := csv.NewReader(in)
+	writer := csv.NewWriter(out)
+	defer writer.Flush()
+
+	seen := make(map[string]bool)
+	headers, err := reader.Read()
+	if err != nil {
+		return fmt.Errorf("failed to read headers: %w", err)
+	}
+	if err := writer.Write(headers); err != nil {
+		return fmt.Errorf("failed to write headers: %w", err)
+	}
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("failed to read record: %w", err)
+		}
+
+		key := fmt.Sprintf("%v", record)
+		if !seen[key] {
+			seen[key] = true
+			if err := writer.Write(record); err != nil {
+				return fmt.Errorf("failed to write record: %w", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	if err := removeDuplicates(inputFile, outputFile); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully cleaned data. Output written to %s\n", outputFile)
+}
