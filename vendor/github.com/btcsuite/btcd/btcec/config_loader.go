c5f9aa7445dddb0a3f4b0e672eee397f72cfdf91
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-14 14:50:32 +0000

    Add configuration file loader with YAML parsing support

--- vendor/github.com/btcsuite/btcd/btcec/config_loader.go
+++ vendor/github.com/btcsuite/btcd/btcec/config_loader.go
@@ -63,4 +63,36 @@ func validateConfig(config *AppConfig) error {
     }
 
     return nil
+}package config
+
+import (
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    var config Config
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        return nil, err
+    }
+
+    return &config, nil
 }
