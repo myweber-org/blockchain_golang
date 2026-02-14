062eb128397a9f73ab187707c69ff985f0762e88
heyQuim <hey.quim@hotmail.com>
2026-02-14 23:25:30 +0000

    Add data processing utility for cleaning and normalizing input strings

--- vendor/github.com/libp2p/go-libp2p-secio/pb/data_processor.go
+++ vendor/github.com/libp2p/go-libp2p-secio/pb/data_processor.go
@@ -35,3 +35,44 @@ func Tokenize(input string) []string {
 	
 	return strings.Split(normalized, " ")
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	normalized := dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+	return normalized
+}
+
+func (dp *DataProcessor) NormalizeCase(input string, toUpper bool) string {
+	cleaned := dp.CleanString(input)
+	if toUpper {
+		return strings.ToUpper(cleaned)
+	}
+	return strings.ToLower(cleaned)
+}
+
+func (dp *DataProcessor) ExtractAlphanumeric(input string) string {
+	alnumRegex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	cleaned := dp.CleanString(input)
+	return alnumRegex.ReplaceAllString(cleaned, "")
+}
+
+func (dp *DataProcessor) ValidateEmail(input string) bool {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(strings.TrimSpace(input))
+}
