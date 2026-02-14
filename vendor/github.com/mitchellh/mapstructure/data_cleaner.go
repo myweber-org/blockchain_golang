457fe9202910700c4c535e8f162f5b2ef5bb7841
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-14 19:59:27 +0000

    Add data cleaning utility to remove duplicate entries from slices

--- vendor/github.com/mitchellh/mapstructure/data_cleaner.go
+++ vendor/github.com/mitchellh/mapstructure/data_cleaner.go
@@ -37,4 +37,26 @@ func main() {
 	fmt.Println("After trimming:", trimmed)
 	deduped := cleaner.RemoveDuplicates(trimmed)
 	fmt.Println("After deduplication:", deduped)
+}package main
+
+import "fmt"
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	slice := []int{1, 2, 2, 3, 4, 4, 5}
+	cleaned := RemoveDuplicates(slice)
+	fmt.Println("Original:", slice)
+	fmt.Println("Cleaned:", cleaned)
 }
