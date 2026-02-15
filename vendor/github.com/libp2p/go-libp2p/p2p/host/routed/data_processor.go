bfab3b8abc79e4d62c12febe586af9cb9a7aec29
FelixMakes <felix.makes@hotmail.com>
2026-02-15 22:10:41 +0000

    Add data validation and sanitization logic for user input

--- vendor/github.com/libp2p/go-libp2p/p2p/host/routed/data_processor.go
+++ vendor/github.com/libp2p/go-libp2p/p2p/host/routed/data_processor.go
@@ -144,4 +144,40 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	emailRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		emailRegex: regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`),
+	}
+}
+
+func (dp *DataProcessor) SanitizeString(input string) string {
+	return strings.TrimSpace(input)
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) ProcessUserData(name, email string) (string, bool) {
+	sanitizedName := dp.SanitizeString(name)
+	if sanitizedName == "" {
+		return "", false
+	}
+
+	if !dp.ValidateEmail(email) {
+		return "", false
+	}
+
+	return sanitizedName, true
 }
