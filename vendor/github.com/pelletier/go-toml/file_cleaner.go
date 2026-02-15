f7bbcc19e96612b7fb706a6d920776056dcf856b
InkspillVerse <inkspill.verse@outlook.com>
2026-02-15 14:46:06 +0000

    Add utility to remove temporary files older than 7 days

--- vendor/github.com/pelletier/go-toml/file_cleaner.go
+++ vendor/github.com/pelletier/go-toml/file_cleaner.go
@@ -2,25 +2,53 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"time"
 )
 
+const (
+	tempDir      = "/tmp/app_cache"
+	maxAge       = 7 * 24 * time.Hour
+	fileModePerm = 0755
+)
+
 func main() {
-	tempDir := os.TempDir()
-	cutoff := time.Now().AddDate(0, 0, -7)
-	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+	if err := cleanOldFiles(); err != nil {
+		fmt.Printf("Cleanup failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles() error {
+	if _, err := os.Stat(tempDir); os.IsNotExist(err) {
+		return nil
+	}
+
+	cutoffTime := time.Now().Add(-maxAge)
+
+	return filepath.WalkDir(tempDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
 			return nil
 		}
-		if info.ModTime().Before(cutoff) && !info.IsDir() {
-			fmt.Printf("Removing old file: %s\n", path)
-			os.Remove(path)
+
+		info, err := d.Info()
+		if err != nil {
+			return err
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			if err := os.Remove(path); err != nil {
+				return fmt.Errorf("failed to remove %s: %w", path, err)
+			}
+			fmt.Printf("Removed: %s\n", path)
 		}
 		return nil
 	})
-	if err != nil {
-		fmt.Printf("Error cleaning temp files: %v\n", err)
-	}
 }
