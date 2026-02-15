06c26b64b2e46b93d57f6453dce22f746dea8c22
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-15 22:55:40 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/libp2p/go-libp2p-core/crypto/pb/user_activity_logger.go
+++ vendor/github.com/libp2p/go-libp2p-core/crypto/pb/user_activity_logger.go
@@ -70,4 +70,42 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf("[%s] %s %s - %v", r.RemoteAddr, r.Method, r.URL.Path, duration)
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status": "ok"}`))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/data", apiHandler)
+
+	loggedMux := NewActivityLogger(mux)
+
+	log.Println("Server starting on :8080")
+	http.ListenAndServe(":8080", loggedMux)
 }
