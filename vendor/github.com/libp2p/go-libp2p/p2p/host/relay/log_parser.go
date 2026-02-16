8f92b96a2803f38984dd329e22c0ff0a76404849
heyQuim <hey.quim@hotmail.com>
2026-02-16 23:37:20 +0000

    Add log parsing utility to extract error messages from application logs

---
+++ vendor/github.com/libp2p/go-libp2p/p2p/host/relay/log_parser.go
@@ -0,0 +1,86 @@
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type LogEntry struct {
+	Timestamp string
+	Level     string
+	Message   string
+}
+
+func parseLogLine(line string) (*LogEntry, error) {
+	pattern := `^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) \[(\w+)\] (.+)$`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindStringSubmatch(line)
+
+	if matches == nil {
+		return nil, fmt.Errorf("invalid log format")
+	}
+
+	return &LogEntry{
+		Timestamp: matches[1],
+		Level:     matches[2],
+		Message:   matches[3],
+	}, nil
+}
+
+func filterLogsByLevel(entries []LogEntry, level string) []LogEntry {
+	var filtered []LogEntry
+	for _, entry := range entries {
+		if strings.EqualFold(entry.Level, level) {
+			filtered = append(filtered, entry)
+		}
+	}
+	return filtered
+}
+
+func readLogFile(filename string) ([]LogEntry, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var entries []LogEntry
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		entry, err := parseLogLine(scanner.Text())
+		if err != nil {
+			continue
+		}
+		entries = append(entries, *entry)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return entries, nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: log_parser <logfile>")
+		os.Exit(1)
+	}
+
+	entries, err := readLogFile(os.Args[1])
+	if err != nil {
+		fmt.Printf("Error reading log file: %v\n", err)
+		os.Exit(1)
+	}
+
+	errorLogs := filterLogsByLevel(entries, "ERROR")
+	fmt.Printf("Found %d error logs:\n", len(errorLogs))
+	for _, entry := range errorLogs {
+		fmt.Printf("[%s] %s\n", entry.Timestamp, entry.Message)
+	}
+}
