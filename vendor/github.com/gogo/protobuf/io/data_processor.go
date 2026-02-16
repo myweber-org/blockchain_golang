961c55b59c7473df023612207613bc04faac44f3
FelixMakes <felix.makes@hotmail.com>
2026-02-16 23:52:52 +0000

    Add data validation and sanitization logic for user input

--- vendor/github.com/gogo/protobuf/io/data_processor.go
+++ vendor/github.com/gogo/protobuf/io/data_processor.go
@@ -143,4 +143,58 @@ func CalculateStatistics(records []Record) (float64, float64, int) {
 
 	averageScore := sum / float64(len(records))
 	return averageScore, maxScore, activeCount
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type UserData struct {
+	Username string
+	Email    string
+	Comments string
+}
+
+func SanitizeInput(input string) string {
+	input = strings.TrimSpace(input)
+	re := regexp.MustCompile(`<.*?>`)
+	return re.ReplaceAllString(input, "")
+}
+
+func ValidateEmail(email string) bool {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(email)
+}
+
+func ProcessUserData(data UserData) (UserData, error) {
+	data.Username = SanitizeInput(data.Username)
+	data.Email = SanitizeInput(data.Email)
+	data.Comments = SanitizeInput(data.Comments)
+
+	if !ValidateEmail(data.Email) {
+		return data, &InvalidEmailError{Email: data.Email}
+	}
+
+	if len(data.Username) < 3 || len(data.Username) > 20 {
+		return data, &InvalidUsernameError{Username: data.Username}
+	}
+
+	return data, nil
+}
+
+type InvalidEmailError struct {
+	Email string
+}
+
+func (e *InvalidEmailError) Error() string {
+	return "Invalid email format: " + e.Email
+}
+
+type InvalidUsernameError struct {
+	Username string
+}
+
+func (e *InvalidUsernameError) Error() string {
+	return "Username must be between 3 and 20 characters: " + e.Username
 }
