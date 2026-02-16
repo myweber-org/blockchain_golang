3a85004b9a6425f7603f957077fb4f7977233037
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-16 15:00:43 +0000

    Add user activity logging middleware for API endpoints

--- vendor/golang.org/x/text/encoding/htmlindex/user_activity_logger.go
+++ vendor/golang.org/x/text/encoding/htmlindex/user_activity_logger.go
@@ -114,4 +114,38 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := extractUserID(r)
+	ip := r.RemoteAddr
+	method := r.Method
+	path := r.URL.Path
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s from %s %s %s completed in %v", userID, ip, method, path, duration)
+}
+
+func extractUserID(r *http.Request) string {
+	if user := r.Header.Get("X-User-ID"); user != "" {
+		return user
+	}
+	return "anonymous"
 }
