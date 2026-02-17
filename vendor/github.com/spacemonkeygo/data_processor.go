04d9dfd7d1d3c449280ea187b6875c99079c3369
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-17 18:43:25 +0000

    Add data processing utility for cleaning and validating input strings

--- vendor/github.com/spacemonkeygo/data_processor.go
+++ vendor/github.com/spacemonkeygo/data_processor.go
@@ -29,3 +29,53 @@ func (dp *DataProcessor) ProcessUserData(name, email string) (string, string, bo
 	isValidEmail := dp.ValidateEmail(sanitizedEmail)
 	return sanitizedName, sanitizedEmail, isValidEmail
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+	emailRegex      *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+		emailRegex:      regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) NormalizeEmail(email string) (string, bool) {
+	cleaned := dp.CleanString(email)
+	lowerEmail := strings.ToLower(cleaned)
+	return lowerEmail, dp.ValidateEmail(lowerEmail)
+}
+
+func (dp *DataProcessor) ProcessInputs(names []string, emails []string) ([]string, []string) {
+	var cleanNames []string
+	var validEmails []string
+
+	for _, name := range names {
+		cleanNames = append(cleanNames, dp.CleanString(name))
+	}
+
+	for _, email := range emails {
+		if normalized, valid := dp.NormalizeEmail(email); valid {
+			validEmails = append(validEmails, normalized)
+		}
+	}
+
+	return cleanNames, validEmails
+}
