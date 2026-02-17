41699ea3e22ebd1f7b157105fc98fb42df85c25d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-17 18:54:01 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- vendor/golang.org/x/net/html/atom/data_cleaner.go
+++ vendor/golang.org/x/net/html/atom/data_cleaner.go
@@ -20,4 +20,51 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
+}package datautils
+
+import "sort"
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	seen := make(map[T]bool)
+	result := make([]T, 0, len(slice))
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
+
+func RemoveDuplicatesSorted[T comparable](slice []T) []T {
+	if len(slice) < 2 {
+		return slice
+	}
+
+	sort.Slice(slice, func(i, j int) bool {
+		switch v := any(slice[i]).(type) {
+		case int:
+			return v < any(slice[j]).(int)
+		case string:
+			return v < any(slice[j]).(string)
+		default:
+			return false
+		}
+	})
+
+	j := 0
+	for i := 1; i < len(slice); i++ {
+		if slice[j] != slice[i] {
+			j++
+			slice[j] = slice[i]
+		}
+	}
+
+	return slice[:j+1]
 }
