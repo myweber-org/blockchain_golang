c23d800efe9caa917a0ad5732314b40a256e0581
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-18 23:45:41 +0000

    Add log parsing utility to extract error messages from application logs

---
+++ vendor/github.com/multiformats/go-multiaddr/log_parser.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type LogEntry struct {
+	Timestamp string
+	Level     string
+	Message   string
+}
+
+func parseLogLine(line string) (*LogEntry, error) {
+	pattern := `^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) \[(\w+)\] (.+)$`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindStringSubmatch(line)
+
+	if len(matches) != 4 {
+		return nil, fmt.Errorf("invalid log format")
+	}
+
+	return &LogEntry{
+		Timestamp: matches[1],
+		Level:     matches[2],
+		Message:   matches[3],
+	}, nil
+}
+
+func filterErrors(entries []LogEntry) []LogEntry {
+	var errorEntries []LogEntry
+	for _, entry := range entries {
+		if strings.ToUpper(entry.Level) == "ERROR" {
+			errorEntries = append(errorEntries, entry)
+		}
+	}
+	return errorEntries
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: log_parser <logfile>")
+		os.Exit(1)
+	}
+
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var entries []LogEntry
+	scanner := bufio.NewScanner(file)
+	lineNumber := 1
+
+	for scanner.Scan() {
+		entry, err := parseLogLine(scanner.Text())
+		if err != nil {
+			fmt.Printf("Warning: Line %d - %v\n", lineNumber, err)
+		} else {
+			entries = append(entries, *entry)
+		}
+		lineNumber++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	errorEntries := filterErrors(entries)
+	fmt.Printf("Found %d error entries:\n", len(errorEntries))
+	for _, entry := range errorEntries {
+		fmt.Printf("[%s] %s\n", entry.Timestamp, entry.Message)
+	}
+}
