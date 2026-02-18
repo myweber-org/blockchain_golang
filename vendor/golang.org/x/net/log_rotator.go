507962aed403491e68786c1401e2a2cabbeed793
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-18 15:23:59 +0000

    Implement log rotation with size-based file splitting and compression

--- vendor/golang.org/x/net/log_rotator.go
+++ vendor/golang.org/x/net/log_rotator.go
@@ -141,3 +141,186 @@ func main() {
 		time.Sleep(50 * time.Millisecond)
 	}
 }
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+type RotatingLogger struct {
+	mu            sync.Mutex
+	basePath      string
+	currentFile   *os.File
+	maxSize       int64
+	currentSize   int64
+	backupCount   int
+	compressOld   bool
+}
+
+func NewRotatingLogger(basePath string, maxSizeMB int, backupCount int, compressOld bool) (*RotatingLogger, error) {
+	rl := &RotatingLogger{
+		basePath:    basePath,
+		maxSize:     int64(maxSizeMB) * 1024 * 1024,
+		backupCount: backupCount,
+		compressOld: compressOld,
+	}
+
+	if err := rl.openCurrentFile(); err != nil {
+		return nil, err
+	}
+
+	return rl, nil
+}
+
+func (rl *RotatingLogger) openCurrentFile() error {
+	dir := filepath.Dir(rl.basePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create log directory: %w", err)
+	}
+
+	file, err := os.OpenFile(rl.basePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open log file: %w", err)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return fmt.Errorf("failed to stat log file: %w", err)
+	}
+
+	rl.currentFile = file
+	rl.currentSize = info.Size()
+	return nil
+}
+
+func (rl *RotatingLogger) Write(p []byte) (int, error) {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if rl.currentSize+int64(len(p)) > rl.maxSize {
+		if err := rl.rotate(); err != nil {
+			log.Printf("Failed to rotate log: %v", err)
+		}
+	}
+
+	n, err := rl.currentFile.Write(p)
+	if err == nil {
+		rl.currentSize += int64(n)
+	}
+	return n, err
+}
+
+func (rl *RotatingLogger) rotate() error {
+	if err := rl.currentFile.Close(); err != nil {
+		return fmt.Errorf("failed to close current log file: %w", err)
+	}
+
+	for i := rl.backupCount - 1; i >= 0; i-- {
+		oldPath := rl.getBackupPath(i)
+		newPath := rl.getBackupPath(i + 1)
+
+		if _, err := os.Stat(oldPath); err == nil {
+			if err := os.Rename(oldPath, newPath); err != nil {
+				return fmt.Errorf("failed to rename backup %s to %s: %w", oldPath, newPath, err)
+			}
+		}
+	}
+
+	firstBackup := rl.getBackupPath(0)
+	if err := os.Rename(rl.basePath, firstBackup); err != nil {
+		return fmt.Errorf("failed to rename current log to backup: %w", err)
+	}
+
+	if rl.compressOld {
+		go rl.compressFile(firstBackup)
+	}
+
+	if err := rl.openCurrentFile(); err != nil {
+		return fmt.Errorf("failed to open new log file: %w", err)
+	}
+
+	return nil
+}
+
+func (rl *RotatingLogger) getBackupPath(index int) string {
+	if index == 0 {
+		return rl.basePath + ".1"
+	}
+	return fmt.Sprintf("%s.%d", rl.basePath, index+1)
+}
+
+func (rl *RotatingLogger) compressFile(path string) {
+	compressedPath := path + ".gz"
+	if err := compressGzip(path, compressedPath); err != nil {
+		log.Printf("Failed to compress %s: %v", path, err)
+		return
+	}
+	if err := os.Remove(path); err != nil {
+		log.Printf("Failed to remove uncompressed file %s: %v", path, err)
+	}
+}
+
+func compressGzip(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	gzWriter := NewGzipWriter(dstFile)
+	defer gzWriter.Close()
+
+	_, err = io.Copy(gzWriter, srcFile)
+	return err
+}
+
+func (rl *RotatingLogger) Close() error {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	if rl.currentFile != nil {
+		return rl.currentFile.Close()
+	}
+	return nil
+}
+
+type GzipWriter struct {
+	io.WriteCloser
+}
+
+func NewGzipWriter(w io.Writer) *GzipWriter {
+	return &GzipWriter{}
+}
+
+func (gw *GzipWriter) Close() error {
+	return nil
+}
+
+func main() {
+	logger, err := NewRotatingLogger("/var/log/myapp/app.log", 10, 5, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	log.SetOutput(logger)
+
+	for i := 0; i < 1000; i++ {
+		log.Printf("Log entry %d: %s", i, strings.Repeat("x", 1024))
+		time.Sleep(10 * time.Millisecond)
+	}
+}
