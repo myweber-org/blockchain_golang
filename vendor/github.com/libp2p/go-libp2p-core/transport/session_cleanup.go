f97bcc528beff83420d51d41180bbabaaff0e924
heyQuim <hey.quim@hotmail.com>
2026-02-19 23:46:47 +0000

    Add session cleanup cron job to remove expired sessions daily

--- vendor/github.com/libp2p/go-libp2p-core/transport/session_cleanup.go
+++ vendor/github.com/libp2p/go-libp2p-core/transport/session_cleanup.go
@@ -53,3 +53,41 @@ func main() {
 
 	<-ctx.Done()
 }
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"yourproject/internal/db"
+)
+
+const cleanupInterval = 24 * time.Hour
+
+func main() {
+	ticker := time.NewTicker(cleanupInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			cleanupExpiredSessions()
+		}
+	}
+}
+
+func cleanupExpiredSessions() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	query := `DELETE FROM user_sessions WHERE expires_at < NOW()`
+	result, err := db.Conn.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Failed to clean up sessions: %v", err)
+		return
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	log.Printf("Cleaned up %d expired sessions", rowsAffected)
+}
