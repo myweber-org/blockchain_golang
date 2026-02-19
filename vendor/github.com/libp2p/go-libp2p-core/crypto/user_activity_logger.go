841d59e50115e3f64eade795bfedf3841f6e5607
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-19 00:27:56 +0000

    Add user activity logging with timestamp and action type

---
+++ vendor/github.com/libp2p/go-libp2p-core/crypto/user_activity_logger.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+    "os"
+    "time"
+)
+
+type UserActivity struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+    encoder *json.Encoder
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{
+        logFile: file,
+        encoder: json.NewEncoder(file),
+    }, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, action, details string) error {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+    return l.encoder.Encode(activity)
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activity.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    err = logger.LogActivity("user123", "file_upload", "uploaded profile.jpg")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    err = logger.LogActivity("user456", "logout", "")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+}
