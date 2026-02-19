9b138bd4d73db01831a6e849f571bfa4dcf4835e
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-19 20:44:23 +0000

    Add user activity logging middleware for tracking API requests

--- vendor/github.com/libp2p/go-libp2p-secio/pb/user_activity_logger.go
+++ vendor/github.com/libp2p/go-libp2p-secio/pb/user_activity_logger.go
@@ -103,4 +103,70 @@ func main() {
 	wrappedMux := NewActivityLogger(mux)
 	log.Println("Server starting on :8080")
 	http.ListenAndServe(":8080", wrappedMux)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time
+	Method    string
+	Path      string
+	UserAgent string
+	IPAddress string
+	Duration  time.Duration
+}
+
+type ActivityLogger struct {
+	activities chan ActivityLog
+}
+
+func NewActivityLogger(bufferSize int) *ActivityLogger {
+	al := &ActivityLogger{
+		activities: make(chan ActivityLog, bufferSize),
+	}
+	go al.processLogs()
+	return al
+}
+
+func (al *ActivityLogger) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		next.ServeHTTP(w, r)
+		
+		activity := ActivityLog{
+			Timestamp: time.Now(),
+			Method:    r.Method,
+			Path:      r.URL.Path,
+			UserAgent: r.UserAgent(),
+			IPAddress: r.RemoteAddr,
+			Duration:  time.Since(start),
+		}
+		
+		select {
+		case al.activities <- activity:
+		default:
+			log.Println("Activity log buffer full, dropping entry")
+		}
+	})
+}
+
+func (al *ActivityLogger) processLogs() {
+	for activity := range al.activities {
+		log.Printf("Activity: %s %s from %s (UA: %s) took %v",
+			activity.Method,
+			activity.Path,
+			activity.IPAddress,
+			activity.UserAgent,
+			activity.Duration,
+		)
+	}
+}
+
+func (al *ActivityLogger) Close() {
+	close(al.activities)
 }
