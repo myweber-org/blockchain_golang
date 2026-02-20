6636c838050aa30ecc638fbd02b6c3c4e4af50c1
InkspillVerse <inkspill.verse@outlook.com>
2026-02-20 15:48:43 +0000

    Add configuration file loader with YAML parsing support

--- vendor/golang.org/x/text/language/config_loader.go
+++ vendor/golang.org/x/text/language/config_loader.go
@@ -40,4 +40,67 @@ func getEnv(key, defaultValue string) string {
         return defaultValue
     }
     return value
+}package config
+
+import (
+	"errors"
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		Driver   string `yaml:"driver"`
+		Host     string `yaml:"host"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(filePath string) (*Config, error) {
+	if filePath == "" {
+		return nil, errors.New("config file path cannot be empty")
+	}
+
+	absPath, err := filepath.Abs(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func (c *Config) Validate() error {
+	if c.Server.Host == "" {
+		return errors.New("server host cannot be empty")
+	}
+	if c.Server.Port <= 0 || c.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if c.Database.Driver == "" {
+		return errors.New("database driver cannot be empty")
+	}
+	return nil
 }
