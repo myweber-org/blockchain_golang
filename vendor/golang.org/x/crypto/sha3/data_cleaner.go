9d1471851849e91b4318300bd614e9af09337069
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-21 18:11:16 +0000

    Add data cleaning utility with duplicate removal and whitespace trimming

--- vendor/golang.org/x/crypto/sha3/data_cleaner.go
+++ vendor/golang.org/x/crypto/sha3/data_cleaner.go
@@ -66,4 +66,83 @@ func main() {
 	
 	cleaned := processRecords(records)
 	fmt.Printf("Cleaned records: %d\n", len(cleaned))
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanData(input []string) []string {
+	seen := make(map[string]bool)
+	var result []string
+
+	for _, line := range input {
+		trimmed := strings.TrimSpace(line)
+		if trimmed == "" {
+			continue
+		}
+		if !seen[trimmed] {
+			seen[trimmed] = true
+			result = append(result, trimmed)
+		}
+	}
+	return result
+}
+
+func readLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func writeLines(filename string, lines []string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(writer, line)
+	}
+	return writer.Flush()
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input_file> <output_file>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	lines, err := readLines(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	cleaned := cleanData(lines)
+
+	err = writeLines(outputFile, cleaned)
+	if err != nil {
+		fmt.Printf("Error writing file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Cleaned %d lines to %d unique lines\n", len(lines), len(cleaned))
 }
