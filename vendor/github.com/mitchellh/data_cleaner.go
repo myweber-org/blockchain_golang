2454ef6a0753cae293e29166ac7a4d23a0e8e907
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-22 01:46:16 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- vendor/github.com/mitchellh/data_cleaner.go
+++ vendor/github.com/mitchellh/data_cleaner.go
@@ -6,4 +6,26 @@ import "strings"
 func SanitizeInput(input string) string {
     trimmed := strings.TrimSpace(input)
     return strings.Join(strings.Fields(trimmed), " ")
+}package main
+
+import "fmt"
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
 }
