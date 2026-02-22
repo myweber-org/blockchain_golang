a72bb30ce7d9f8420a0c57fe81016997a60b268a
InkspillVerse <inkspill.verse@outlook.com>
2026-02-22 19:59:08 +0000

    Add configuration loader with YAML parsing support

--- vendor/github.com/spf13/cast/config_loader.go
+++ vendor/github.com/spf13/cast/config_loader.go
@@ -82,4 +82,63 @@ func validateConfig(config *AppConfig) error {
 	}
 
 	return nil
+}package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+}
+
+type ServerConfig struct {
+	Port int    `yaml:"port"`
+	Mode string `yaml:"mode"`
+}
+
+type AppConfig struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+}
+
+func LoadConfig(filePath string) (*AppConfig, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) bool {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		log.Printf("Invalid server port: %d", config.Server.Port)
+		return false
+	}
+
+	if config.Database.Host == "" {
+		log.Print("Database host cannot be empty")
+		return false
+	}
+
+	if config.Database.Port <= 0 || config.Database.Port > 65535 {
+		log.Printf("Invalid database port: %d", config.Database.Port)
+		return false
+	}
+
+	return true
 }
