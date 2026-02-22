3dcc5b257c2c5a115d6a01ac56558cfc38c1c344
ElaraWrites <elara.writes@outlook.com>
2026-02-22 01:07:45 +0000

    Add user activity logging with timestamp and action type

--- vendor/github.com/jbenet/goprocess/user_activity_logger.go
+++ vendor/github.com/jbenet/goprocess/user_activity_logger.go
@@ -1,151 +1,50 @@
-package middleware
+package main
 
 import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	userID := extractUserID(r)
-	ipAddress := r.RemoteAddr
-
-	al.handler.ServeHTTP(w, r)
-
-	duration := time.Since(start)
-	log.Printf("User %s from %s accessed %s %s - Duration: %v",
-		userID, ipAddress, r.Method, r.URL.Path, duration)
-}
-
-func extractUserID(r *http.Request) string {
-	if userID := r.Header.Get("X-User-ID"); userID != "" {
-		return userID
-	}
-	return "anonymous"
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf(
-		"%s %s %d %s %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.RemoteAddr,
-	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package main
-
-import (
-	"log"
-	"net/http"
-	"time"
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
 )
 
 type ActivityLog struct {
-	UserID    string
-	Endpoint  string
-	Method    string
-	Timestamp time.Time
-	IPAddress string
+    Timestamp time.Time `json:"timestamp"`
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Details   string    `json:"details,omitempty"`
 }
 
-var activityChannel = make(chan ActivityLog, 100)
+func logActivity(userID, action, details string) {
+    logEntry := ActivityLog{
+        Timestamp: time.Now().UTC(),
+        UserID:    userID,
+        Action:    action,
+        Details:   details,
+    }
 
-func activityLoggerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(w, r)
+    logData, err := json.MarshalIndent(logEntry, "", "  ")
+    if err != nil {
+        log.Printf("Failed to marshal log entry: %v", err)
+        return
+    }
 
-		activity := ActivityLog{
-			UserID:    extractUserID(r),
-			Endpoint:  r.URL.Path,
-			Method:    r.Method,
-			Timestamp: start,
-			IPAddress: r.RemoteAddr,
-		}
+    fmt.Println(string(logData))
 
-		select {
-		case activityChannel <- activity:
-		default:
-			log.Println("Activity channel full, dropping log entry")
-		}
-	})
-}
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Printf("Failed to open log file: %v", err)
+        return
+    }
+    defer file.Close()
 
-func extractUserID(r *http.Request) string {
-	if auth := r.Header.Get("Authorization"); auth != "" {
-		return auth[:8]
-	}
-	return "anonymous"
-}
-
-func processActivityLogs() {
-	for activity := range activityChannel {
-		log.Printf("Activity: %s %s by %s from %s at %s",
-			activity.Method,
-			activity.Endpoint,
-			activity.UserID,
-			activity.IPAddress,
-			activity.Timestamp.Format(time.RFC3339))
-	}
+    if _, err := file.Write(append(logData, '\n')); err != nil {
+        log.Printf("Failed to write log entry: %v", err)
+    }
 }
 
 func main() {
-	go processActivityLogs()
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/users", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Users endpoint"))
-	})
-	mux.HandleFunc("/api/orders", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Orders endpoint"))
-	})
-
-	handler := activityLoggerMiddleware(mux)
-	log.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+    logActivity("user123", "LOGIN", "User logged in from web browser")
+    logActivity("user456", "UPDATE_PROFILE", "Changed email address")
+    logActivity("user789", "LOGOUT", "")
 }
