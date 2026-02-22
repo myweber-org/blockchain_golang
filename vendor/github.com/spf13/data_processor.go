3bbda9b83f274cb4f13e5d647be80ab2c803a15e
ElaraWrites <elara.writes@outlook.com>
2026-02-22 16:16:52 +0000

    Add data validation and sanitization for user input

--- vendor/github.com/spf13/data_processor.go
+++ vendor/github.com/spf13/data_processor.go
@@ -106,4 +106,33 @@ func GenerateReport(records []DataRecord) {
 			}
 		}
 	}
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	emailRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	regex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return &DataProcessor{emailRegex: regex}
+}
+
+func (dp *DataProcessor) SanitizeInput(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return strings.ToLower(trimmed)
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) ProcessUserData(rawEmail string) (string, bool) {
+	cleanEmail := dp.SanitizeInput(rawEmail)
+	isValid := dp.ValidateEmail(cleanEmail)
+	return cleanEmail, isValid
 }
