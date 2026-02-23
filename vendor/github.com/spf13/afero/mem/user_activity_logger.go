689d9dad2034b23b200ca6381ca0236a05cecac5
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-23 16:27:00 +0000

    Add user activity logging with timestamp and action type

--- vendor/github.com/spf13/afero/mem/user_activity_logger.go
+++ vendor/github.com/spf13/afero/mem/user_activity_logger.go
@@ -50,4 +50,48 @@ func (al *ActivityLogger) Cleanup() {
 			}
 		}
 	}
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Details   string    `json:"details"`
+}
+
+func logActivity(userID, action, details string) {
+	activity := ActivityLog{
+		Timestamp: time.Now(),
+		UserID:    userID,
+		Action:    action,
+		Details:   details,
+	}
+
+	file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("Failed to open log file: %v", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(activity); err != nil {
+		log.Printf("Failed to write log entry: %v", err)
+	}
+}
+
+func main() {
+	logActivity("user123", "login", "User logged in from web browser")
+	logActivity("user456", "purchase", "Purchased item ID: 789")
+	logActivity("user123", "logout", "Session ended after 30 minutes")
+
+	fmt.Println("Activity logging completed. Check activity.log for details.")
 }
