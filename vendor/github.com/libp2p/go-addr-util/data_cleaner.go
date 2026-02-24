65ff65fdf8c832c465c99528c2eb1cb7563b0fb9
ElaraWrites <elara.writes@outlook.com>
2026-02-24 22:03:56 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- vendor/github.com/libp2p/go-addr-util/data_cleaner.go
+++ vendor/github.com/libp2p/go-addr-util/data_cleaner.go
@@ -21,3 +21,62 @@ func main() {
 	fmt.Printf("Original: %v\n", data)
 	fmt.Printf("Cleaned: %v\n", cleaned)
 }
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func removeDuplicates(inputFile, outputFile string) error {
+	in, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	seen := make(map[string]bool)
+	reader := csv.NewReader(in)
+	var records [][]string
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		key := fmt.Sprintf("%v", record)
+		if !seen[key] {
+			seen[key] = true
+			records = append(records, record)
+		}
+	}
+
+	out, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	writer := csv.NewWriter(out)
+	return writer.WriteAll(records)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	err := removeDuplicates(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Duplicate removal completed successfully")
+}
