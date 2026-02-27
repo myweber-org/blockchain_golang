0512af873c3d31ef9b10fccb83446c8223eb321a
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-27 19:12:19 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/golang.org/x/net/html/charset/user_activity_logger.go
+++ vendor/golang.org/x/net/html/charset/user_activity_logger.go
@@ -26,4 +26,32 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		duration,
 		time.Now().Format(time.RFC3339),
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf("[%s] %s %s completed in %v", 
+		time.Now().Format(time.RFC3339),
+		r.Method,
+		r.URL.Path,
+		duration)
 }
