5d5c7191bd3bc9c1b978b0058526e2a21dfe7dd6
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-27 21:03:17 +0000

    Add user activity logging with timestamp and action type

--- vendor/github.com/mattn/user_activity_logger.go
+++ vendor/github.com/mattn/user_activity_logger.go
@@ -3,90 +3,48 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"time"
 )
 
-type ActivityType string
-
-const (
-	Login      ActivityType = "LOGIN"
-	Logout     ActivityType = "LOGOUT"
-	ViewPage   ActivityType = "VIEW_PAGE"
-	UpdateData ActivityType = "UPDATE_DATA"
-)
-
-type UserActivity struct {
-	UserID    string       `json:"user_id"`
-	Action    ActivityType `json:"action"`
-	Timestamp time.Time    `json:"timestamp"`
-	Details   string       `json:"details,omitempty"`
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Resource  string    `json:"resource"`
 }
 
-func LogActivity(userID string, action ActivityType, details string) UserActivity {
-	activity := UserActivity{
+func logActivity(userID, action, resource string) {
+	activity := ActivityLog{
+		Timestamp: time.Now().UTC(),
 		UserID:    userID,
 		Action:    action,
-		Timestamp: time.Now().UTC(),
-		Details:   details,
+		Resource:  resource,
 	}
-	return activity
-}
 
-func SaveActivityToFile(activity UserActivity, filename string) error {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logEntry, err := json.Marshal(activity)
 	if err != nil {
-		return err
+		log.Printf("Failed to marshal activity: %v", err)
+		return
+	}
+
+	file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("Failed to open log file: %v", err)
+		return
 	}
 	defer file.Close()
 
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	return encoder.Encode(activity)
+	if _, err := file.Write(append(logEntry, '\n')); err != nil {
+		log.Printf("Failed to write log entry: %v", err)
+	}
 }
 
 func main() {
-	activities := []UserActivity{
-		LogActivity("user123", Login, "Successful authentication"),
-		LogActivity("user123", ViewPage, "/dashboard"),
-		LogActivity("user456", UpdateData, "Updated profile information"),
-		LogActivity("user123", Logout, "Session terminated"),
-	}
+	logActivity("user123", "CREATE", "/api/document")
+	logActivity("user456", "READ", "/api/report")
+	logActivity("user789", "UPDATE", "/api/profile")
 
-	for _, activity := range activities {
-		err := SaveActivityToFile(activity, "user_activities.json")
-		if err != nil {
-			fmt.Printf("Failed to save activity: %v\n", err)
-		} else {
-			fmt.Printf("Logged: %s - %s\n", activity.UserID, activity.Action)
-		}
-	}
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | RemoteAddr: %s",
-		r.Method,
-		r.URL.Path,
-		duration,
-		r.RemoteAddr,
-	)
+	fmt.Println("Activity logging completed")
 }
