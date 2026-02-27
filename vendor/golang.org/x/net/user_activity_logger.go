5978ca2924aa30e32b9d3b794a458926b081fba8
FelixMakes <felix.makes@hotmail.com>
2026-02-27 15:04:25 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/golang.org/x/net/user_activity_logger.go
+++ vendor/golang.org/x/net/user_activity_logger.go
@@ -16,42 +16,30 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+
+	al.handler.ServeHTTP(recorder, r)
+
 	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
+	log.Printf(
+		"%s %s %d %s %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
+		recorder.statusCode,
 		duration,
+		r.RemoteAddr,
 	)
 }
-package middleware
 
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	userAgent := r.UserAgent()
-	clientIP := r.RemoteAddr
-	requestPath := r.URL.Path
-
-	al.handler.ServeHTTP(w, r)
-
-	duration := time.Since(start)
-	log.Printf("Activity: %s | %s | %s | %v", clientIP, userAgent, requestPath, duration)
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
