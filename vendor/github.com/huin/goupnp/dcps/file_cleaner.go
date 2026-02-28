39f5d3c5b134c7161ab3a41b86abb2ba625e9fb6
VioletInquiry <violet.inquiry@outlook.com>
2026-02-28 01:35:08 +0000

    Add utility to remove temporary files older than 7 days

--- vendor/github.com/huin/goupnp/dcps/file_cleaner.go
+++ vendor/github.com/huin/goupnp/dcps/file_cleaner.go
@@ -33,4 +33,55 @@ func main() {
 	}
 
 	fmt.Printf("Cleaned %d temporary files older than 7 days\n", removedCount)
+}package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_cache"
+	maxAgeHours  = 168 // 7 days
+)
+
+func main() {
+	err := cleanOldFiles(tempDir)
+	if err != nil {
+		fmt.Printf("Error cleaning files: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string) error {
+	cutoffTime := time.Now().Add(-time.Hour * maxAgeHours)
+
+	return filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			err = os.Remove(path)
+			if err != nil {
+				fmt.Printf("Failed to remove %s: %v\n", path, err)
+			} else {
+				fmt.Printf("Removed old file: %s\n", path)
+			}
+		}
+		return nil
+	})
 }
