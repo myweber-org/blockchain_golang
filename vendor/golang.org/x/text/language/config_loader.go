dbcfa404adcb6311e8f77e30b810459e55aa3cd6
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-28 23:36:38 +0000

    Add configuration loader with YAML parsing support

--- vendor/golang.org/x/text/language/config_loader.go
+++ vendor/golang.org/x/text/language/config_loader.go
@@ -1,65 +1,22 @@
 package config
 
 import (
-    "os"
-    "strconv"
-    "strings"
-)
-
-type Config struct {
-    ServerPort int
-    DatabaseURL string
-    EnableDebug bool
-    AllowedOrigins []string
-}
-
-func Load() (*Config, error) {
-    cfg := &Config{}
-    
-    portStr := getEnv("SERVER_PORT", "8080")
-    port, err := strconv.Atoi(portStr)
-    if err != nil {
-        return nil, err
-    }
-    cfg.ServerPort = port
-    
-    cfg.DatabaseURL = getEnv("DATABASE_URL", "postgres://localhost:5432/app")
-    
-    debugStr := getEnv("ENABLE_DEBUG", "false")
-    cfg.EnableDebug = strings.ToLower(debugStr) == "true"
-    
-    originsStr := getEnv("ALLOWED_ORIGINS", "http://localhost:3000")
-    cfg.AllowedOrigins = strings.Split(originsStr, ",")
-    
-    return cfg, nil
-}
-
-func getEnv(key, defaultValue string) string {
-    value := os.Getenv(key)
-    if value == "" {
-        return defaultValue
-    }
-    return value
-}package config
-
-import (
-	"errors"
 	"io/ioutil"
-	"path/filepath"
+	"log"
 
 	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
+type AppConfig struct {
 	Server struct {
-		Host string `yaml:"host"`
 		Port int    `yaml:"port"`
+		Host string `yaml:"host"`
 	} `yaml:"server"`
 	Database struct {
-		Driver   string `yaml:"driver"`
-		Host     string `yaml:"host"`
 		Username string `yaml:"username"`
 		Password string `yaml:"password"`
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
 		Name     string `yaml:"name"`
 	} `yaml:"database"`
 	Logging struct {
@@ -68,39 +25,28 @@ type Config struct {
 	} `yaml:"logging"`
 }
 
-func LoadConfig(filePath string) (*Config, error) {
-	if filePath == "" {
-		return nil, errors.New("config file path cannot be empty")
-	}
-
-	absPath, err := filepath.Abs(filePath)
+func LoadConfig(filePath string) (*AppConfig, error) {
+	configData, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 
-	data, err := ioutil.ReadFile(absPath)
+	var config AppConfig
+	err = yaml.Unmarshal(configData, &config)
 	if err != nil {
 		return nil, err
 	}
 
-	var config Config
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		return nil, err
-	}
-
+	log.Printf("Configuration loaded successfully from %s", filePath)
 	return &config, nil
 }
 
-func (c *Config) Validate() error {
-	if c.Server.Host == "" {
-		return errors.New("server host cannot be empty")
+func ValidateConfig(config *AppConfig) bool {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		return false
 	}
-	if c.Server.Port <= 0 || c.Server.Port > 65535 {
-		return errors.New("server port must be between 1 and 65535")
+	if config.Database.Port <= 0 || config.Database.Port > 65535 {
+		return false
 	}
-	if c.Database.Driver == "" {
-		return errors.New("database driver cannot be empty")
-	}
-	return nil
+	return true
 }
