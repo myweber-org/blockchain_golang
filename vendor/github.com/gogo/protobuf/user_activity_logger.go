35b50bdf90356ff5ede395dde6c789d87929603a
VioletInquiry <violet.inquiry@outlook.com>
2026-02-28 23:18:33 +0000

    Add user activity logging middleware to track API usage

--- vendor/github.com/gogo/protobuf/user_activity_logger.go
+++ vendor/github.com/gogo/protobuf/user_activity_logger.go
@@ -7,35 +7,30 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
+	handler http.Handler
 }
 
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"%s %s %d %s %s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-		)
-	})
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
+	duration := time.Since(start)
+	log.Printf("%s %s %d %s %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		r.RemoteAddr,
+		duration,
+	)
 }
 
 type responseRecorder struct {
@@ -46,60 +41,4 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
-}package main
-
-import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "os"
-    "time"
-)
-
-type UserActivity struct {
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Timestamp time.Time `json:"timestamp"`
-    Details   string    `json:"details,omitempty"`
-}
-
-func logActivity(userID, action, details string) error {
-    activity := UserActivity{
-        UserID:    userID,
-        Action:    action,
-        Timestamp: time.Now().UTC(),
-        Details:   details,
-    }
-
-    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    encoder := json.NewEncoder(file)
-    encoder.SetIndent("", "  ")
-    if err := encoder.Encode(activity); err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func main() {
-    activities := []struct {
-        userID, action, details string
-    }{
-        {"user_001", "login", "Successful authentication"},
-        {"user_002", "purchase", "Order #789 completed"},
-        {"user_001", "logout", "Session terminated"},
-    }
-
-    for _, act := range activities {
-        if err := logActivity(act.userID, act.action, act.details); err != nil {
-            log.Printf("Failed to log activity: %v", err)
-        } else {
-            fmt.Printf("Logged: %s - %s\n", act.userID, act.action)
-        }
-    }
 }
