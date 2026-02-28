54c9305ae5c7504734cd914c271b99e4a485a1c2
ElaraWrites <elara.writes@outlook.com>
2026-02-28 23:55:22 +0000

    Add configuration loader with YAML parsing support

--- vendor/github.com/spacemonkeygo/config_loader.go
+++ vendor/github.com/spacemonkeygo/config_loader.go
@@ -129,4 +129,55 @@ func (c *Config) Validate() error {
         return fmt.Errorf("server port cannot be zero")
     }
     return nil
+}package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+	Server struct {
+		Port int    `yaml:"port"`
+		Host string `yaml:"host"`
+	} `yaml:"server"`
+	Database struct {
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(filename string) (*AppConfig, error) {
+	configData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config AppConfig
+	err = yaml.Unmarshal(configData, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Configuration loaded successfully from %s", filename)
+	return &config, nil
+}
+
+func ValidateConfig(config *AppConfig) bool {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		return false
+	}
+	if config.Database.Port <= 0 || config.Database.Port > 65535 {
+		return false
+	}
+	return true
 }
