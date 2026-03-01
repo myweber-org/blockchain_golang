10bc4069501e7ab136560fbda58293ae9c4018b7
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-01 19:52:06 +0000

    Add configuration file parsing utility with YAML support

--- vendor/github.com/go-stack/config_loader.go
+++ vendor/github.com/go-stack/config_loader.go
@@ -1,77 +1,56 @@
 package config
 
 import (
-	"os"
-	"path/filepath"
+	"io/ioutil"
+	"log"
 
-	"gopkg.in/yaml.v3"
+	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
 	Server struct {
-		Host string `yaml:"host" env:"SERVER_HOST"`
-		Port int    `yaml:"port" env:"SERVER_PORT"`
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
 	} `yaml:"server"`
 	Database struct {
-		Host     string `yaml:"host" env:"DB_HOST"`
-		Port     int    `yaml:"port" env:"DB_PORT"`
-		Name     string `yaml:"name" env:"DB_NAME"`
-		User     string `yaml:"user" env:"DB_USER"`
-		Password string `yaml:"password" env:"DB_PASSWORD"`
+		Host     string `yaml:"host"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
 	} `yaml:"database"`
 	Logging struct {
-		Level  string `yaml:"level" env:"LOG_LEVEL"`
-		Output string `yaml:"output" env:"LOG_OUTPUT"`
+		Level string `yaml:"level"`
+		File  string `yaml:"file"`
 	} `yaml:"logging"`
 }
 
-func LoadConfig(configPath string) (*Config, error) {
-	absPath, err := filepath.Abs(configPath)
+func LoadConfig(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	data, err := os.ReadFile(absPath)
+	var config Config
+	err = yaml.Unmarshal(data, &config)
 	if err != nil {
 		return nil, err
 	}
 
-	var cfg Config
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		return nil, err
-	}
-
-	overrideFromEnv(&cfg)
-	return &cfg, nil
+	return &config, nil
 }
 
-func overrideFromEnv(cfg *Config) {
-	cfg.Server.Host = getEnvOrDefault("SERVER_HOST", cfg.Server.Host)
-	cfg.Server.Port = getEnvIntOrDefault("SERVER_PORT", cfg.Server.Port)
-
-	cfg.Database.Host = getEnvOrDefault("DB_HOST", cfg.Database.Host)
-	cfg.Database.Port = getEnvIntOrDefault("DB_PORT", cfg.Database.Port)
-	cfg.Database.Name = getEnvOrDefault("DB_NAME", cfg.Database.Name)
-	cfg.Database.User = getEnvOrDefault("DB_USER", cfg.Database.User)
-	cfg.Database.Password = getEnvOrDefault("DB_PASSWORD", cfg.Database.Password)
-
-	cfg.Logging.Level = getEnvOrDefault("LOG_LEVEL", cfg.Logging.Level)
-	cfg.Logging.Output = getEnvOrDefault("LOG_OUTPUT", cfg.Logging.Output)
-}
-
-func getEnvOrDefault(key, defaultValue string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
+func ValidateConfig(config *Config) bool {
+	if config.Server.Host == "" {
+		log.Println("Server host is required")
+		return false
 	}
-	return defaultValue
-}
-
-func getEnvIntOrDefault(key string, defaultValue int) int {
-	if value := os.Getenv(key); value != "" {
-		var result int
-		if _, err := fmt.Sscanf(value, "%d", &result); err == nil {
-			return result
-		}
+	if config.Server.Port <= 0 {
+		log.Println("Server port must be positive")
+		return false
 	}
-	return defaultValue
+	if config.Database.Host == "" {
+		log.Println("Database host is required")
+		return false
+	}
+	return true
 }
