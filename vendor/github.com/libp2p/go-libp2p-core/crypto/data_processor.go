11df069ec1653c9f8b8e0b6972b0b3ed3c752477
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-01 23:07:26 +0000

    Add utility function to filter and transform numeric slices

--- vendor/github.com/libp2p/go-libp2p-core/crypto/data_processor.go
+++ vendor/github.com/libp2p/go-libp2p-core/crypto/data_processor.go
@@ -86,3 +86,23 @@ func CalculateTotal(records []Record) float64 {
 	}
 	return total
 }
+package main
+
+import "fmt"
+
+func FilterAndDoubleEvenNumbers(numbers []int) []int {
+    var result []int
+    for _, num := range numbers {
+        if num%2 == 0 {
+            result = append(result, num*2)
+        }
+    }
+    return result
+}
+
+func main() {
+    input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    output := FilterAndDoubleEvenNumbers(input)
+    fmt.Println("Original:", input)
+    fmt.Println("Filtered and doubled evens:", output)
+}
