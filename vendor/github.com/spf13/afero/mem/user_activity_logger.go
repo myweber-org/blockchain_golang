4a33dd8813c2ae237ec654df7a822495aa023ba0
heyQuim <hey.quim@hotmail.com>
2026-03-01 00:53:07 +0000

    Add user activity logging middleware with request details and timestamp

--- vendor/github.com/spf13/afero/mem/user_activity_logger.go
+++ vendor/github.com/spf13/afero/mem/user_activity_logger.go
@@ -94,4 +94,39 @@ func main() {
 	logActivity("user123", "logout", "Session ended after 30 minutes")
 
 	fmt.Println("Activity logging completed. Check activity.log for details.")
+}package middleware
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+type responseWriter struct {
+    http.ResponseWriter
+    statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+    rw.statusCode = code
+    rw.ResponseWriter.WriteHeader(code)
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+        next.ServeHTTP(rw, r)
+
+        duration := time.Since(start)
+        log.Printf(
+            "%s %s %d %s %s",
+            r.Method,
+            r.URL.Path,
+            rw.statusCode,
+            duration,
+            r.RemoteAddr,
+        )
+    })
 }
