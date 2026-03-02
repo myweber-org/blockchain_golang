747435f6c7b289171ffaf093dbff793637d5a3d2
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-02 17:14:32 +0000

    Add session cleanup cron job to remove expired sessions daily

--- vendor/github.com/huin/goupnp/ssdp/session_cleanup.go
+++ vendor/github.com/huin/goupnp/ssdp/session_cleanup.go
@@ -1,51 +1,52 @@
-
 package main
 
 import (
-	"context"
-	"log"
-	"time"
-
-	"yourproject/internal/database"
+    "log"
+    "time"
+    "database/sql"
+    _ "github.com/lib/pq"
 )
 
-const cleanupInterval = 24 * time.Hour
-
-func main() {
-	db, err := database.NewConnection()
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer db.Close()
-
-	ticker := time.NewTicker(cleanupInterval)
-	defer ticker.Stop()
-
-	ctx := context.Background()
-	for {
-		select {
-		case <-ticker.C:
-			if err := cleanupExpiredSessions(ctx, db); err != nil {
-				log.Printf("Session cleanup failed: %v", err)
-			} else {
-				log.Println("Session cleanup completed successfully")
-			}
-		}
-	}
+func cleanupExpiredSessions(db *sql.DB) error {
+    query := `DELETE FROM user_sessions WHERE expires_at < $1`
+    result, err := db.Exec(query, time.Now())
+    if err != nil {
+        return err
+    }
+    
+    rowsAffected, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+    
+    log.Printf("Cleaned up %d expired sessions", rowsAffected)
+    return nil
 }
 
-func cleanupExpiredSessions(ctx context.Context, db *database.DB) error {
-	query := `DELETE FROM user_sessions WHERE expires_at < NOW()`
-	result, err := db.ExecContext(ctx, query)
-	if err != nil {
-		return err
-	}
+func scheduleSessionCleanup(db *sql.DB) {
+    ticker := time.NewTicker(24 * time.Hour)
+    defer ticker.Stop()
+    
+    for {
+        select {
+        case <-ticker.C:
+            if err := cleanupExpiredSessions(db); err != nil {
+                log.Printf("Session cleanup failed: %v", err)
+            }
+        }
+    }
+}
 
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Cleaned up %d expired sessions", rowsAffected)
-	return nil
+func main() {
+    db, err := sql.Open("postgres", "host=localhost port=5432 user=app dbname=appdb sslmode=disable")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+    
+    if err := db.Ping(); err != nil {
+        log.Fatal(err)
+    }
+    
+    scheduleSessionCleanup(db)
 }
