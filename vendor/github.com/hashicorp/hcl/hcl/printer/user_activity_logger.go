9d866e7e21c924bc039eade81fb9805bd0763188
InkspillVerse <inkspill.verse@outlook.com>
2026-03-02 19:56:09 +0000

    Add user activity logging middleware for tracking API usage

--- vendor/github.com/hashicorp/hcl/hcl/printer/user_activity_logger.go
+++ vendor/github.com/hashicorp/hcl/hcl/printer/user_activity_logger.go
@@ -28,63 +28,13 @@ func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
 		duration := time.Since(start)
 		
 		al.Logger.Printf(
-			"%s %s %d %s %s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration.String(),
-			r.RemoteAddr,
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		userID := r.Header.Get("X-User-ID")
-		if userID == "" {
-			userID = "anonymous"
-		}
-
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-
-		next.ServeHTTP(recorder, r)
-
-		duration := time.Since(start)
-		al.Logger.Printf(
-			"User %s | Method %s | Path %s | Status %d | Duration %v",
-			userID,
+			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
 			r.Method,
 			r.URL.Path,
 			recorder.statusCode,
 			duration,
+			r.RemoteAddr,
+			r.UserAgent(),
 		)
 	})
 }
