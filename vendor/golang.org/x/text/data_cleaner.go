a2a1295d4cd8d807aaf9ad962fec8b2e44b34be2
heyQuim <hey.quim@hotmail.com>
2026-03-03 01:05:41 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- vendor/golang.org/x/text/data_cleaner.go
+++ vendor/golang.org/x/text/data_cleaner.go
@@ -12,4 +12,64 @@ func RemoveDuplicates[T comparable](slice []T) []T {
 	}
 
 	return result
+}package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func removeDuplicates(inputPath, outputPath string) error {
+	inFile, err := os.Open(inputPath)
+	if err != nil {
+		return fmt.Errorf("failed to open input file: %w", err)
+	}
+	defer inFile.Close()
+
+	reader := csv.NewReader(inFile)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return fmt.Errorf("failed to read CSV: %w", err)
+	}
+
+	seen := make(map[string]bool)
+	var uniqueRecords [][]string
+
+	for _, record := range records {
+		if len(record) == 0 {
+			continue
+		}
+		key := record[0]
+		if !seen[key] {
+			seen[key] = true
+			uniqueRecords = append(uniqueRecords, record)
+		}
+	}
+
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer outFile.Close()
+
+	writer := csv.NewWriter(outFile)
+	defer writer.Flush()
+
+	return writer.WriteAll(uniqueRecords)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	err := removeDuplicates(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Duplicate removal completed successfully")
 }
