5557a6fbe45ab75c396d0c97dffd91ecdbb3593a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-05 00:13:34 +0000

    Add configuration loader with YAML parsing support

--- vendor/github.com/spaolacci/murmur3/config_loader.go
+++ vendor/github.com/spaolacci/murmur3/config_loader.go
@@ -70,4 +70,55 @@ func overrideInt(field *int, envVar string) {
 			*field = intVal
 		}
 	}
+}package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+    Host     string `yaml:"host"`
+    Port     int    `yaml:"port"`
+    Username string `yaml:"username"`
+    Password string `yaml:"password"`
+    Name     string `yaml:"name"`
+}
+
+type ServerConfig struct {
+    Port int    `yaml:"port"`
+    Env  string `yaml:"env"`
+}
+
+type AppConfig struct {
+    Database DatabaseConfig `yaml:"database"`
+    Server   ServerConfig   `yaml:"server"`
+}
+
+func LoadConfig(path string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    return &config, nil
+}
+
+func (c *AppConfig) Validate() error {
+    if c.Database.Host == "" {
+        return fmt.Errorf("database host is required")
+    }
+    if c.Database.Port <= 0 {
+        return fmt.Errorf("database port must be positive")
+    }
+    if c.Server.Port <= 0 {
+        return fmt.Errorf("server port must be positive")
+    }
+    return nil
 }
