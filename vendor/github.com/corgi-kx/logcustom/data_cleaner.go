02232271cbec22f335f72203f5beb8f292fae766
heyQuim <hey.quim@hotmail.com>
2026-03-05 14:36:33 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- vendor/github.com/corgi-kx/logcustom/data_cleaner.go
+++ vendor/github.com/corgi-kx/logcustom/data_cleaner.go
@@ -21,3 +21,54 @@ func main() {
 	fmt.Printf("Original: %v\n", data)
 	fmt.Printf("Cleaned: %v\n", cleaned)
 }
+package datautils
+
+import "sort"
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	seen := make(map[T]bool)
+	result := make([]T, 0, len(slice))
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
+
+func RemoveDuplicatesSorted[T comparable](slice []T) []T {
+	if len(slice) == 0 {
+		return slice
+	}
+
+	sort.Slice(slice, func(i, j int) bool {
+		switch v := any(slice).(type) {
+		case []string:
+			return v[i] < v[j]
+		case []int:
+			return v[i] < v[j]
+		case []float64:
+			return v[i] < v[j]
+		default:
+			return false
+		}
+	})
+
+	result := make([]T, 0, len(slice))
+	result = append(result, slice[0])
+
+	for i := 1; i < len(slice); i++ {
+		if slice[i] != slice[i-1] {
+			result = append(result, slice[i])
+		}
+	}
+
+	return result
+}
