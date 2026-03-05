5471455d82baa7a396184dce3aaff328b6cc0a77
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-05 15:37:48 +0000

    Add configuration loader with YAML support and environment variable overrides

--- vendor/github.com/whyrusleeping/config_loader.go
+++ vendor/github.com/whyrusleeping/config_loader.go
@@ -67,4 +67,88 @@ func getEnvBool(key string, defaultValue bool) bool {
         return lowerValue == "true" || lowerValue == "1" || lowerValue == "yes"
     }
     return defaultValue
+}package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Format string `yaml:"format" env:"LOG_FORMAT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(&cfg)
+
+	return &cfg, nil
+}
+
+func overrideFromEnv(cfg *Config) {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		cfg.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		port := 0
+		fmt.Sscanf(val, "%d", &port)
+		if port > 0 {
+			cfg.Server.Port = port
+		}
+	}
+	if val := os.Getenv("DB_HOST"); val != "" {
+		cfg.Database.Host = val
+	}
+	if val := os.Getenv("DB_PORT"); val != "" {
+		port := 0
+		fmt.Sscanf(val, "%d", &port)
+		if port > 0 {
+			cfg.Database.Port = port
+		}
+	}
+	if val := os.Getenv("DB_NAME"); val != "" {
+		cfg.Database.Name = val
+	}
+	if val := os.Getenv("DB_USER"); val != "" {
+		cfg.Database.User = val
+	}
+	if val := os.Getenv("DB_PASSWORD"); val != "" {
+		cfg.Database.Password = val
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		cfg.Logging.Level = val
+	}
+	if val := os.Getenv("LOG_FORMAT"); val != "" {
+		cfg.Logging.Format = val
+	}
 }
