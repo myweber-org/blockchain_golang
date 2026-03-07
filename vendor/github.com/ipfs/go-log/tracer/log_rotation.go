426b25b39f2172ca963a2ee66e3496e744debfea
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-07 01:17:01 +0000

    Add log rotation functionality with file size limit and backup retention

--- vendor/github.com/ipfs/go-log/tracer/log_rotation.go
+++ vendor/github.com/ipfs/go-log/tracer/log_rotation.go
@@ -104,3 +104,124 @@ func main() {
 		time.Sleep(100 * time.Millisecond)
 	}
 }
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	maxFileSize = 10 * 1024 * 1024 // 10MB
+	maxBackups  = 5
+	logFileName = "app.log"
+)
+
+type RotatingLogger struct {
+	currentSize int64
+	backupCount int
+}
+
+func NewRotatingLogger() *RotatingLogger {
+	return &RotatingLogger{}
+}
+
+func (rl *RotatingLogger) Write(p []byte) (n int, err error) {
+	rl.checkRotation()
+	file, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	n, err = file.Write(p)
+	if err == nil {
+		rl.currentSize += int64(n)
+	}
+	return n, err
+}
+
+func (rl *RotatingLogger) checkRotation() {
+	info, err := os.Stat(logFileName)
+	if os.IsNotExist(err) {
+		rl.currentSize = 0
+		return
+	}
+	if err != nil {
+		log.Printf("Error checking log file: %v", err)
+		return
+	}
+
+	rl.currentSize = info.Size()
+	if rl.currentSize >= maxFileSize {
+		rl.rotate()
+	}
+}
+
+func (rl *RotatingLogger) rotate() {
+	timestamp := time.Now().Format("20060102_150405")
+	backupName := logFileName + "." + timestamp
+
+	err := os.Rename(logFileName, backupName)
+	if err != nil {
+		log.Printf("Failed to rotate log: %v", err)
+		return
+	}
+
+	rl.currentSize = 0
+	rl.cleanupOldBackups()
+	rl.backupCount++
+}
+
+func (rl *RotatingLogger) cleanupOldBackups() {
+	pattern := logFileName + ".*"
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return
+	}
+
+	if len(matches) <= maxBackups {
+		return
+	}
+
+	backups := make([]backupInfo, 0, len(matches))
+	for _, match := range matches {
+		parts := strings.Split(match, ".")
+		if len(parts) < 2 {
+			continue
+		}
+		timestamp := parts[len(parts)-1]
+		t, err := time.Parse("20060102_150405", timestamp)
+		if err != nil {
+			continue
+		}
+		backups = append(backups, backupInfo{path: match, time: t})
+	}
+
+	if len(backups) <= maxBackups {
+		return
+	}
+
+	for i := 0; i < len(backups)-maxBackups; i++ {
+		os.Remove(backups[i].path)
+	}
+}
+
+type backupInfo struct {
+	path string
+	time time.Time
+}
+
+func main() {
+	logger := NewRotatingLogger()
+	log.SetOutput(logger)
+
+	for i := 0; i < 1000; i++ {
+		log.Printf("Log entry %d: %s", i, strconv.FormatInt(time.Now().UnixNano(), 10))
+		time.Sleep(10 * time.Millisecond)
+	}
+}
