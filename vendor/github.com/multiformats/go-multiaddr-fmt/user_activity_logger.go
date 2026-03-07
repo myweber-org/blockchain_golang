1ec88c2205d7a1297d6efe5413a188be0405a7ff
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-07 19:54:01 +0000

    Add user activity logging middleware for API endpoints

--- vendor/github.com/multiformats/go-multiaddr-fmt/user_activity_logger.go
+++ vendor/github.com/multiformats/go-multiaddr-fmt/user_activity_logger.go
@@ -16,70 +16,30 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
 
+	al.handler.ServeHTTP(recorder, r)
+
+	duration := time.Since(start)
 	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | RemoteAddr: %s",
+		"Method: %s | Path: %s | Status: %d | Duration: %v | RemoteAddr: %s",
 		r.Method,
 		r.URL.Path,
+		recorder.statusCode,
 		duration,
 		r.RemoteAddr,
 	)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf(
-		"Activity: %s %s from %s completed in %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }
