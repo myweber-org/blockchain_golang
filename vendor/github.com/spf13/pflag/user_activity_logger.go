0ae4d4f8f269fc3dadc955c9a5d3592410c6622b
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-09 15:47:56 +0000

    Add user activity logging middleware for HTTP requests

--- vendor/github.com/spf13/pflag/user_activity_logger.go
+++ vendor/github.com/spf13/pflag/user_activity_logger.go
@@ -1,70 +1,28 @@
-package main
+package middleware
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "os"
-    "time"
+	"log"
+	"net/http"
+	"time"
 )
 
-type UserActivity struct {
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Timestamp time.Time `json:"timestamp"`
-    Details   string    `json:"details,omitempty"`
-}
-
 type ActivityLogger struct {
-    logFile *os.File
+	handler http.Handler
 }
 
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return nil, err
-    }
-    return &ActivityLogger{logFile: file}, nil
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (l *ActivityLogger) LogActivity(userID, action, details string) error {
-    activity := UserActivity{
-        UserID:    userID,
-        Action:    action,
-        Timestamp: time.Now().UTC(),
-        Details:   details,
-    }
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-    data, err := json.Marshal(activity)
-    if err != nil {
-        return err
-    }
-
-    data = append(data, '\n')
-    _, err = l.logFile.Write(data)
-    return err
-}
-
-func (l *ActivityLogger) Close() error {
-    return l.logFile.Close()
-}
-
-func main() {
-    logger, err := NewActivityLogger("user_activities.log")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer logger.Close()
-
-    err = logger.LogActivity("user123", "login", "Successful authentication")
-    if err != nil {
-        log.Printf("Failed to log activity: %v", err)
-    }
-
-    err = logger.LogActivity("user123", "file_upload", "uploaded profile.jpg")
-    if err != nil {
-        log.Printf("Failed to log activity: %v", err)
-    }
-
-    fmt.Println("User activities logged successfully")
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
