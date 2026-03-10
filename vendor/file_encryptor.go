0f45063f8eebf57e90583b1583922ae4c55ee928
FelixMakes <felix.makes@hotmail.com>
2026-03-10 03:14:13 +0000

    Add file encryption utility with AES-256-GCM support

--- vendor/file_encryptor.go
+++ vendor/file_encryptor.go
@@ -105,4 +105,102 @@ func main() {
 	os.Remove(inputFile)
 	os.Remove(encryptedFile)
 	os.Remove(decryptedFile)
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Encryptor struct {
+	key []byte
+}
+
+func NewEncryptor(key string) (*Encryptor, error) {
+	if len(key) != 32 {
+		return nil, errors.New("encryption key must be 32 bytes")
+	}
+	return &Encryptor{key: []byte(key)}, nil
+}
+
+func (e *Encryptor) Encrypt(plaintext string) (string, error) {
+	block, err := aes.NewCipher(e.key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, []byte(plaintext), nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func (e *Encryptor) Decrypt(encrypted string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(e.key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return "", errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+func main() {
+	key := "thisis32bitlongpassphraseimusing!"
+	encryptor, err := NewEncryptor(key)
+	if err != nil {
+		fmt.Printf("Error creating encryptor: %v\n", err)
+		os.Exit(1)
+	}
+
+	secret := "confidential data"
+	encrypted, err := encryptor.Encrypt(secret)
+	if err != nil {
+		fmt.Printf("Encryption failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Encrypted: %s\n", encrypted)
+
+	decrypted, err := encryptor.Decrypt(encrypted)
+	if err != nil {
+		fmt.Printf("Decryption failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Decrypted: %s\n", decrypted)
 }
