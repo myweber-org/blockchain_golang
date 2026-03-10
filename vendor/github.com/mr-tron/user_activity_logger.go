d75ed079e38327d72728c36227962083b7637587
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-10 14:58:15 +0000

    Add user activity logging middleware with request details and timestamp

--- vendor/github.com/mr-tron/user_activity_logger.go
+++ vendor/github.com/mr-tron/user_activity_logger.go
@@ -6,58 +6,31 @@ import (
 	"time"
 )
 
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	recorder := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
-	duration := time.Since(start)
-	log.Printf("%s %s %d %v", r.Method, r.URL.Path, recorder.statusCode, duration)
-}
-
-type responseRecorder struct {
+type responseWriter struct {
 	http.ResponseWriter
 	statusCode int
 }
 
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf(
-		"Method: %s, Path: %s, Duration: %v, Timestamp: %s",
-		r.Method,
-		r.URL.Path,
-		duration,
-		time.Now().Format(time.RFC3339),
-	)
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		
+		next.ServeHTTP(rw, r)
+		
+		duration := time.Since(start)
+		log.Printf(
+			"%s %s %d %s %s",
+			r.Method,
+			r.URL.Path,
+			rw.statusCode,
+			duration,
+			r.RemoteAddr,
+		)
+	})
 }
