75a9107045938e52d075aa9b9211f545a21b6567
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-10 18:45:58 +0000

    Add user activity logging middleware to track API usage

--- vendor/github.com/whyrusleeping/go-logging/user_activity_logger.go
+++ vendor/github.com/whyrusleeping/go-logging/user_activity_logger.go
@@ -1,75 +1,28 @@
-package main
+package middleware
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"time"
 )
 
-type ActivityEvent struct {
-	UserID    string    `json:"user_id"`
-	EventType string    `json:"event_type"`
-	Timestamp time.Time `json:"timestamp"`
-	Details   string    `json:"details,omitempty"`
-}
-
 type ActivityLogger struct {
-	logFile *os.File
+	handler http.Handler
 }
 
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, err
-	}
-	return &ActivityLogger{logFile: file}, nil
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func (al *ActivityLogger) LogActivity(userID, eventType, details string) error {
-	event := ActivityEvent{
-		UserID:    userID,
-		EventType: eventType,
-		Timestamp: time.Now(),
-		Details:   details,
-	}
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-	eventJSON, err := json.Marshal(event)
-	if err != nil {
-		return err
-	}
-
-	eventJSON = append(eventJSON, '\n')
-	_, err = al.logFile.Write(eventJSON)
-	return err
-}
-
-func (al *ActivityLogger) Close() error {
-	return al.logFile.Close()
-}
-
-func main() {
-	logger, err := NewActivityLogger("activity.log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logger.Close()
-
-	err = logger.LogActivity("user123", "login", "User logged in from web browser")
-	if err != nil {
-		log.Printf("Failed to log activity: %v", err)
-	}
-
-	err = logger.LogActivity("user123", "view_page", "Viewed dashboard")
-	if err != nil {
-		log.Printf("Failed to log activity: %v", err)
-	}
-
-	err = logger.LogActivity("user456", "logout", "Session expired")
-	if err != nil {
-		log.Printf("Failed to log activity: %v", err)
-	}
-
-	fmt.Println("Activity logging completed")
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }
