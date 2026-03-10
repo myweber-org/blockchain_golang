727987704362858ea97dd8c0d8338d5dc822879f
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-10 14:19:20 +0000

    Add user activity logging middleware to track API usage

--- vendor/github.com/spf13/cast/user_activity_logger.go
+++ vendor/github.com/spf13/cast/user_activity_logger.go
@@ -7,42 +7,6 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		userAgent := r.UserAgent()
-		clientIP := r.RemoteAddr
-		method := r.Method
-		path := r.URL.Path
-
-		next.ServeHTTP(w, r)
-
-		duration := time.Since(start)
-		al.Logger.Printf(
-			"Activity: %s %s | IP: %s | Agent: %s | Duration: %v",
-			method,
-			path,
-			clientIP,
-			userAgent,
-			duration,
-		)
-	})
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
 	handler http.Handler
 }
 
