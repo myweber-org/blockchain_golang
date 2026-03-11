491e174fd7cacc76494188c2b3950fb870abd428
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-11 19:50:50 +0000

    Add data validation and sanitization functions for user input

--- vendor/github.com/whyrusleeping/go-logging/data_processor.go
+++ vendor/github.com/whyrusleeping/go-logging/data_processor.go
@@ -285,4 +285,25 @@ func ProcessUserInput(rawUsername, rawEmail string, age int) (UserData, error) {
 	}
 
 	return userData, nil
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeUsername(input string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+	sanitized := re.ReplaceAllString(input, "")
+	return strings.TrimSpace(sanitized)
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(pattern)
+	return re.MatchString(email)
+}
+
+func TrimAndLower(input string) string {
+	return strings.ToLower(strings.TrimSpace(input))
 }
