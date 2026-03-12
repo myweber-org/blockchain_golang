8726b994b602bfab447cb42e64cb33d3287ccebf
InkspillVerse <inkspill.verse@outlook.com>
2026-03-12 14:16:00 +0000

    Add configuration loader with YAML support and environment variable overrides

--- vendor/github.com/libp2p/go-libp2p/p2p/host/config_loader.go
+++ vendor/github.com/libp2p/go-libp2p/p2p/host/config_loader.go
@@ -52,4 +52,82 @@ func getEnvAsSlice(key string, defaultValue []string, sep string) []string {
         return defaultValue
     }
     return strings.Split(valueStr, sep)
+}package config
+
+import (
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+		SSLMode  string `yaml:"ssl_mode" env:"DB_SSL_MODE"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Format string `yaml:"format" env:"LOG_FORMAT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	overrideWithEnvVars(config)
+
+	return config, nil
+}
+
+func overrideWithEnvVars(config *Config) {
+	overrideString(&config.Server.Host, "SERVER_HOST")
+	overrideInt(&config.Server.Port, "SERVER_PORT")
+	overrideString(&config.Database.Host, "DB_HOST")
+	overrideInt(&config.Database.Port, "DB_PORT")
+	overrideString(&config.Database.Name, "DB_NAME")
+	overrideString(&config.Database.User, "DB_USER")
+	overrideString(&config.Database.Password, "DB_PASSWORD")
+	overrideString(&config.Database.SSLMode, "DB_SSL_MODE")
+	overrideString(&config.Logging.Level, "LOG_LEVEL")
+	overrideString(&config.Logging.Format, "LOG_FORMAT")
+}
+
+func overrideString(field *string, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		*field = val
+	}
+}
+
+func overrideInt(field *int, envVar string) {
+	if val := os.Getenv(envVar); val != "" {
+		if intVal, err := parseInt(val); err == nil {
+			*field = intVal
+		}
+	}
+}
+
+func parseInt(s string) (int, error) {
+	var result int
+	_, err := fmt.Sscanf(s, "%d", &result)
+	return result, err
 }
