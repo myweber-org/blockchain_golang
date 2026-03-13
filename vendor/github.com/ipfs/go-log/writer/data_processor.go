b3cbf1b25244b7c9f7f575f37f558613c5d00c04
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-13 23:44:05 +0000

    Add utility function to calculate moving average of a slice

--- vendor/github.com/ipfs/go-log/writer/data_processor.go
+++ vendor/github.com/ipfs/go-log/writer/data_processor.go
@@ -235,3 +235,28 @@ func main() {
 		os.Exit(1)
 	}
 }
+package main
+
+import "fmt"
+
+func movingAverage(data []float64, windowSize int) []float64 {
+    if windowSize <= 0 || len(data) == 0 {
+        return []float64{}
+    }
+
+    result := make([]float64, 0, len(data)-windowSize+1)
+    for i := 0; i <= len(data)-windowSize; i++ {
+        sum := 0.0
+        for j := i; j < i+windowSize; j++ {
+            sum += data[j]
+        }
+        result = append(result, sum/float64(windowSize))
+    }
+    return result
+}
+
+func main() {
+    sampleData := []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}
+    averaged := movingAverage(sampleData, 3)
+    fmt.Printf("Moving average result: %v\n", averaged)
+}
