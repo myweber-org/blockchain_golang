9d175ec1f5a27f6036386a0010032adac6c060f1
InkspillVerse <inkspill.verse@outlook.com>
2026-03-13 02:20:15 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- vendor/github.com/libp2p/go-libp2p-core/crypto/pb/data_cleaner.go
+++ vendor/github.com/libp2p/go-libp2p-core/crypto/pb/data_cleaner.go
@@ -3,67 +3,22 @@ package main
 
 import "fmt"
 
-func RemoveDuplicates(input []int) []int {
+func RemoveDuplicates(nums []int) []int {
 	seen := make(map[int]bool)
 	result := []int{}
 
-	for _, value := range input {
-		if !seen[value] {
-			seen[value] = true
-			result = append(result, value)
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
 		}
 	}
 	return result
 }
 
 func main() {
-	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
-	cleaned := RemoveDuplicates(data)
-	fmt.Println("Original:", data)
-	fmt.Println("Cleaned:", cleaned)
-}package main
-
-import "fmt"
-
-func RemoveDuplicates(input []int) []int {
-	seen := make(map[int]bool)
-	result := []int{}
-
-	for _, value := range input {
-		if !seen[value] {
-			seen[value] = true
-			result = append(result, value)
-		}
-	}
-	return result
-}
-
-func main() {
-	data := []int{1, 2, 2, 3, 4, 4, 5}
-	cleaned := RemoveDuplicates(data)
-	fmt.Printf("Original: %v\n", data)
+	input := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(input)
+	fmt.Printf("Original: %v\n", input)
 	fmt.Printf("Cleaned: %v\n", cleaned)
-}package main
-
-import (
-	"regexp"
-	"strings"
-)
-
-func SanitizeCSVField(input string) string {
-	if input == "" {
-		return input
-	}
-
-	input = strings.TrimSpace(input)
-
-	re := regexp.MustCompile(`[\r\n]+`)
-	input = re.ReplaceAllString(input, " ")
-
-	re = regexp.MustCompile(`[,"]`)
-	if re.MatchString(input) {
-		input = `"` + strings.ReplaceAll(input, `"`, `""`) + `"`
-	}
-
-	return input
 }
