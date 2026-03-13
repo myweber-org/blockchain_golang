4003c83a30460a0000b8d7019c0220772513fed4
VioletInquiry <violet.inquiry@outlook.com>
2026-03-13 16:48:08 +0000

    Add utility to remove duplicate lines from a text file

--- vendor/github.com/libp2p/go-libp2p-core/sec/insecure/pb/file_cleaner.go
+++ vendor/github.com/libp2p/go-libp2p-core/sec/insecure/pb/file_cleaner.go
@@ -22,4 +22,63 @@ func main() {
         }
         return nil
     })
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_cleaner <input_file>")
+		return
+	}
+
+	inputFile := os.Args[1]
+	outputFile := inputFile + ".cleaned"
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	seen := make(map[string]bool)
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !seen[line] {
+			seen[line] = true
+			lines = append(lines, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	out, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		return
+	}
+	defer out.Close()
+
+	writer := bufio.NewWriter(out)
+	for _, line := range lines {
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			fmt.Printf("Error writing to file: %v\n", err)
+			return
+		}
+	}
+	writer.Flush()
+
+	fmt.Printf("Duplicate lines removed. Cleaned file saved as: %s\n", outputFile)
 }
