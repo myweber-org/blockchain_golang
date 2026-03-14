d2d90c012d0caa96efa83eadb1f1e56c52fbfa57
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-14 00:04:34 +0000

    Add currency conversion utility with USD to EUR rate

--- vendor/github.com/libp2p/go-libp2p-peerstore/currency_converter.go
+++ vendor/github.com/libp2p/go-libp2p-peerstore/currency_converter.go
@@ -85,3 +85,19 @@ func main() {
 
 	fmt.Printf("%.2f %s = %.2f %s\n", amount, from, result, to)
 }
+package main
+
+import (
+	"fmt"
+)
+
+func ConvertUSDToEUR(amount float64) float64 {
+	const exchangeRate = 0.85
+	return amount * exchangeRate
+}
+
+func main() {
+	usdAmount := 100.0
+	eurAmount := ConvertUSDToEUR(usdAmount)
+	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
+}
